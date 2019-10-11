93c45922f6fd554d329af5c5657cfe8cf56a8e75
Mauricio Pena <sistemas5@arios-ing.com>
2019-10-11 11:13:41 -0500

    EVA-2 feat: Se cambió plural y singular relacionado en clase Meta, adicionalmente se modifica función __str__

--- Administracion/models/models.py
+++ Administracion/models/models.py
@@ -44,47 +44,48 @@ class TipoIdentificacion(models.Model):
         return self.nombre
 
     class Meta:
-        verbose_name = 'Tipo Identificación'
-        verbose_name_plural = 'Tipos Identificaciones'
+        verbose_name = 'Tipo de Identificación'
+        verbose_name_plural = 'Tipos de Identificaciones'
 
 
 class TipoContrato(models.Model):
     nombre = models.CharField(max_length=100, verbose_name='Nombre', null=False, blank=False)
     descripcion = models.TextField(max_length=300, verbose_name='Descripción', null=False, blank=False)
     estado = models.BooleanField(verbose_name='Estado', null=False, blank=False)
+    laboral = models.BooleanField(verbose_name='Estado', null=False, blank=False)
 
     def __str__(self):
         return self.nombre
 
     class Meta:
-        verbose_name = 'Tipo Contrato'
-        verbose_name_plural = 'Tipos Contratos'
+        verbose_name = 'Tipo de Contrato'
+        verbose_name_plural = 'Tipos de Contratos'
 
 
 class Persona(models.Model):
     identificacion = models.CharField(max_length=20, verbose_name='Identificación', null=False, blank=False,
-                                      unique=True)
+                                          unique=True)
     fecha_expedicion = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Expedición', null=False,
                                           blank=False)
-    fecha_nacimiento = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Nacimiento', null=False,
+    fecha_nacimiento = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Nacimiento', null=True,
                                           blank=False)
-    telefono = models.TextField(max_length=300, verbose_name='Teléfono', null=False, blank=False)
+    telefono = models.TextField(max_length=15, verbose_name='Teléfono', null=False, blank=False)
     fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación', null=False,
-                                            blank=False)
+                                          blank=False)
     fecha_modificacion = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Modificación', null=False,
-                                            blank=False)
-    genero = models.TextField(max_length=300, verbose_name='Género', null=False, blank=False)
+                                          blank=False)
+    genero = models.TextField(max_length=10, verbose_name='Género', null=False, blank=False)
     tipo_identificacion = models.ForeignKey(TipoIdentificacion, on_delete=models.DO_NOTHING,
-                                            verbose_name='Tipo de identificación', null=True, blank=False)
+                                          verbose_name='Tipo de identificación', null=True, blank=False)
     usuario = models.OneToOneField(User, on_delete=models.DO_NOTHING, verbose_name="Usuario", null=True,
-                                blank=False, related_name='UserAccess')
+                                          blank=False, related_name='UserAccess')
     usuario_crea = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="Usuario Crea", null=False,
-                                     blank=False, related_name='UserCrea')
-    usuario_actualiza = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="Usuario Actualiza", null=False,
-                                         blank=False, related_name='UserModifica')
+                                          blank=False, related_name='UserCrea')
+    usuario_actualiza = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="Usuario Actualiza",
+                                          null=False, blank=False, related_name='UserModifica')
 
     def __str__(self):
-        return self
+        return self.usuario.first_name
 
     class Meta:
         abstract = True
