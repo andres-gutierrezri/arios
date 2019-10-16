e6cd037654e4afe20af1f88abaae38b7902171b7
Mauricio Pena <sistemas5@arios-ing.com>
2019-10-16 11:09:59 -0500

    EVA-3 feat: Se agregan URL para selectores.

--- Administracion/urls.py
+++ Administracion/urls.py
@@ -15,15 +15,19 @@ Including another URLconf
 """
 
 from django.urls import path
-from Administracion.views import tercero
+from Administracion.views import tercero, views
 
 app_name = 'Administracion'
 
 urlpatterns = [
+    path('principal/', tercero.principal_view, name='principal'),
+    path('terceros/', tercero.tercero_view, name='terceros'),
+    path('terceros/add', tercero.TerceroCrearView.as_view(), name='terceros-crear'),
+    path('terceros/<int:id>/', tercero.TerceroEditarView.as_view(), name='terceros-editar'),
+    path('terceros/<int:id>/delete', tercero.TerceroEliminarView.as_view(), name='terceros-eliminar'),
+    path('departamentos/<int:id>/municipios/json', views.CargarMunicipiosSelectJsonView.as_view(),
+        name='municipios-json'),
+    path('municipios/<int:id>/centro-poblado/json', views.CargarCentroPobladoSelectJsonView.as_view(),
+        name='centro-poblado-json'),
 
-
-    path('tercero/', tercero.tercero_view, name='tercero'),
-    path('tercero/add', tercero.TerceroCrearView.as_view(), name='tercero-crear'),
-    path('tercero/<int:id>/', tercero.TerceroEditarView.as_view(), name='tercero-editar'),
-    path('tercero/<int:id>/delete', tercero.TerceroEliminarView.as_view(), name='tercero-eliminar'),
 ]
