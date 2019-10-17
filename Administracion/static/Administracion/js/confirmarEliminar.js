eca49194711ebbb0544e4aaad67c1d15e4d518a8
Mauricio Pena <sistemas5@arios-ing.com>
2019-10-17 16:16:01 -0500

    EVA-3 feat: Se crea archivo js para sweet alert de eliminar Tercero.

--- Administracion/static/Administracion/js/confirmarEliminar.js
+++ Administracion/static/Administracion/js/confirmarEliminar.js
@@ -2,46 +2,62 @@ var URLDomain = document.location.origin+"/";
 var idBorrar = 0;
 var urlFinal;
 var rutaBorrado = $('#rutaBorrado').val();
-$(document).ready(function() {
-
-    $(".confirmarEliminarSi").click(function () {
-        if ($('#id_eliminar').val() === 'medidor'){
-
-            urlFinal = URLDomain + "recuperacion-perdidas/" + rutaBorrado + "/" + idBorrar + "/delete"
-        }else{
-            urlFinal = URLDomain + rutaBorrado + "/" + idBorrar + "/delete"
-        }
-        console.log(urlFinal);
-        $.ajax({
-            url: urlFinal,
-            type: 'POST',
-            context: document.body,
-            success: function (data) {
-                cerrarModalEliminar();
-                if(data.Mensaje === "OK") {
-                    location.reload();
-                }
-                else{
-                    alert(data.Mensaje);
-                }
-            },
-            failure: function (errMsg) {
-                alert('Se presentó un error. No se pudo eliminar.');
-                cerrarModalEliminar();
-                location.reload();
-            }
-        });
-    });
-});
-
-function cerrarModalEliminar() {
-    $("#confirmarEliminar").modal('hide');
-    $(".modal-backdrop").remove();
-}
 
 function fConfirmarEliminar(idElemento) {
-    $("#confirmarEliminar").modal('show');
+    // $("#confirmarEliminar").modal('show');
+     fSweetAlert();
     idBorrar = idElemento;
 }
+
+function fSweetAlert() {
+    Swal.fire({
+                        title: 'Quiere eliminar este campo?',
+                        text: "Esta acción no se podrá revertir",
+                        type: 'warning',
+                        showCancelButton: true,
+                        //confirmButtonColor: '#d33',
+                        confirmButtonText: 'Sí, eliminarlo!',
+                        cancelButtonText: 'Cancelar'
+                    }).then(result => {
+                        if (result.value) {
+
+                            $.ajax({
+                                    url: URLDomain + rutaBorrado + "/" + idBorrar + "/delete",
+                                    type: 'POST',
+                                    context: document.body,
+                                    success: function (data) {
+                                        if(data.Mensaje === "OK") {
+                                            Swal.fire({
+                                                title: "Eliminado!",
+                                                text: "El campo ha sido eliminado.",
+                                                //type: "error",
+                                                confirmButtonText: "OK",
+                                        preConfirm: function () {
+                                            return new Promise(function (resolve) {
+                                                resolve();
+                    });
+                }
+                }).then(function () {
+                    location.reload();
+                });
+                    }
+                    else{
+                        Swal.fire("Error!", "Error");
+                    }
+
+
+                                         //Swal.fire("Eliminado!", "El campo ha sido eliminado.");
+                                    },
+                                    failure: function (errMsg) {
+                                        location.reload();
+                                        Swal.fire("Error!", "Error");
+                                    }
+                            });
+                        }
+                    });
+
+                }
+
+
 
 
