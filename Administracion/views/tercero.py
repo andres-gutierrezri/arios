26c2ad6348bc1a80f6773acca9dbddf1834ecefb
Mauricio Pena <sistemas5@arios-ing.com>
2019-10-19 12:45:49 -0500

    EVA-3 fix: Se envía nombre de tercero para usar en el toast y se y se elimina código comentado.

--- Administracion/views/tercero.py
+++ Administracion/views/tercero.py
@@ -9,7 +9,8 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 
-from Administracion.models import Tercero, TipoIdentificacion, TipoTercero, CentroPoblado, Empresa, Departamento
+from Administracion.models import Tercero, TipoIdentificacion, TipoTercero, CentroPoblado, Empresa, Departamento, \
+    Municipio
 
 
 def tercero_view(request):
@@ -19,7 +20,7 @@ def tercero_view(request):
 
 
 def principal_view(request):
-    return render(request, 'Administracion/Tercero/principal.html')
+    return render(request, 'Administracion/index.html')
 
 
 class TerceroCrearView(View):
@@ -39,16 +40,14 @@ class TerceroCrearView(View):
         identificacion = request.POST.get('identificacion', '')
         tipo_identificacion = request.POST.get('tipo_identificacion_id', '')
         empresa = request.POST.get('empresa_id', '')
-        # fecha_creacion = request.POST.get('fecha_creacion', '')
-        # fecha_modificacion = request.POST.get('fecha_modificacion', '')
         tipo_tercero = request.POST.get('tipo_tercero_id', '')
         centro_poblado = request.POST.get('centro_poblado_id', '')
         tercero = Tercero(nombre=nombre, identificacion=identificacion, tipo_identificacion_id=tipo_identificacion,
                           estado=True, empresa_id=empresa,
                           tipo_tercero_id=tipo_tercero, centro_poblado_id=centro_poblado)
-        # fecha = datetime.today()
+
         tercero.save()
-        messages.success(request, 'Se ha creado un Tercero')
+        messages.success(request, nombre)
 
         return redirect(reverse('Administracion:terceros'))
 
@@ -60,22 +59,25 @@ class TerceroEditarView(View):
         tipo_identificaciones = TipoIdentificacion.objects.filter(estado=True).order_by('nombre')
         tipo_terceros = TipoTercero.objects.filter(estado=True).order_by('nombre')
         departamentos = Departamento.objects.all().order_by('nombre')
+        municipios = Municipio.objects.all().order_by('nombre')
+        c_poblados = CentroPoblado.objects.all().order_by('nombre')
         return render(request, 'Administracion/Tercero/editar.html', {'tercero': tercero, 'empresas': empresas,
                                                        'tipo_identificaciones': tipo_identificaciones,
                                                        'tipo_terceros': tipo_terceros,
-                                                       'departamentos': departamentos})
+                                                       'departamentos': departamentos,
+                                                       'municipios': municipios, 'c_poblados': c_poblados})
 
     def post(self, request, id):
         update_fields = ['nombre', 'identificacion', 'tipo_identificacion_id', 'estado', 'empresa_id',
                          'fecha_modificacion', 'tipo_tercero_id', 'centro_poblado_id']
+
         tercero = Tercero(id=id)
         tercero.nombre = request.POST.get('nombre', '')
         tercero.identificacion = request.POST.get('identificacion', '')
         tercero.tipo_identificacion_id = request.POST.get('tipo_identificacion_id', '')
         tercero.estado = request.POST.get('estado', 'False') == 'True'
         tercero.empresa_id = request.POST.get('empresa_id', '')
-        # tercero.fecha_creacion = request.POST.get('fecha_creacion', '')
-        tercero.fecha_modificacion = datetime.today()
+        tercero.fecha_modificacion = datetime.now()
         tercero.tipo_tercero_id = request.POST.get('tipo_tercero_id', '')
         tercero.centro_poblado_id = request.POST.get('centro_poblado_id', '')
         tercero.save(update_fields=update_fields)
