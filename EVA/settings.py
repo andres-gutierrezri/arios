1809b8a910543610c3f76efa4c1e884aa0d9ab17
Mauricio Pena <sistemas5@arios-ing.com>
2019-10-24 11:56:33 -0500

    EVA-7 feat: Se agrega la aplicación proyectos al archivo settings.py.

--- EVA/settings.py
+++ EVA/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Administracion.apps.AdministracionConfig',
+    'Proyectos.apps.ProyectosConfig',
 ]
 
 MIDDLEWARE = [
