9de3e372cef7961cec7f700cafad17ad48725250
Mauricio Pena <sistemas5@arios-ing.com>
2019-10-24 11:57:29 -0500

    EVA-7 feat: Se incluye la URL de la aplicación proyectos.

--- EVA/urls.py
+++ EVA/urls.py
@@ -21,5 +21,6 @@ from EVA.views import index_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view),
-    path('administracion/', include('Administracion.urls'))
+    path('administracion/', include('Administracion.urls')),
+    path('proyectos/', include('Proyectos.urls')),
 ]
