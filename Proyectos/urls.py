ee78b78334e5d0f5a01c595c2303d92cd4f98598
Mauricio Pena <sistemas5@arios-ing.com>
2019-10-25 08:20:10 -0500

    EVA-8 feat: Se crean URL para el index y la opción crear de contratos.

--- Proyectos/urls.py
+++ Proyectos/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 
 from django.urls import path
-from Proyectos.views import proyectos
+from Proyectos.views import proyectos, contratos
 
 app_name = 'Proyectos'
 
 urlpatterns = [
     path('index/', proyectos.index, name='index'),
-
+    path('contratos/', contratos.ContratoView.as_view(), name='contratos'),
+    path('contratos/add', contratos.ContratoCrearView.as_view(), name='contratos-crear'),
 ]
