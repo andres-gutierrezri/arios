7b98d42208c65194be59f67b2050a5dc6c0be9b5
Alfonso Ardila <asesorsistemas@arios-ing.com>
2019-11-08 20:18:00 -0500

    EVA-16 refactor: Se minimiza el codigo duplicado en la vistas asociadas a Contratos.
    Adiconal a esto se agrega que se ejecute lña valicación del modelo con el método full_clean(),  para tener también validación de los datos desde el lado del servidor.

--- Proyectos/views/contratos.py
+++ Proyectos/views/contratos.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from django.shortcuts import render, redirect, reverse
 from django.views import View
@@ -18,168 +20,80 @@ class ContratoView(View):
 
 
 class ContratoCrearView(View):
-    def get(self, request):
-        tipo_contratos = TipoContrato.objects \
-            .filter(laboral=False)\
-            .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-        empresas = Empresa.objects.filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre'))\
-            .order_by('nombre')
-        terceros = Tercero.objects.filter(tipo_tercero_id=TipoTercero.CLIENTE)\
-            .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-        rango_anho = [{'campo_valor': anho, 'campo_texto': str(anho)}for anho in range(2000, 2051)]
-        opcion = 'crear'
+    def __init__(self):
+        self.opcion = 'crear'
+        super().__init__()
 
-        return render(request, 'Proyectos/Contrato/crear-editar.html', {'tipo_contratos': tipo_contratos,
-                                                                        'empresas': empresas, 'terceros': terceros,
-                                                                        'rango_anho': rango_anho, 'opcion': opcion})
+    def get(self, request):
+        return render(request, 'Proyectos/Contrato/crear-editar.html', datos_xa_render(self.opcion))
 
     def post(self, request):
 
-        numero_contrato = request.POST.get('numero_contrato', '')
-        cliente = int((request.POST.get('cliente_id', '0')))
-        anho = int(request.POST.get('rango_anho', '0'))
-        supervisor_nombre = request.POST.get('supervisor_nombre', '')
-        supervisor_telefono = request.POST.get('supervisor_telefono', '')
-        supervisor_correo = request.POST.get('supervisor_correo', '')
-        residente = request.POST.get('residente', '')
-        fecha_inicio = datetime.strptime(request.POST.get('fecha_inicio', ''), "%Y-%m-%d")
-        fecha_terminacion = datetime.strptime(request.POST.get('fecha_terminacion', ''), "%Y-%m-%d")
-        valor = request.POST.get('valor', '')
-        periodicidad_informes = request.POST.get('periodicidad_informes', '')
-        tiempo = request.POST.get('tiempo', '')
-        tipo_contrato = int((request.POST.get('tipo_contrato_id', '0')))
-        empresa = int((request.POST.get('empresa_id', '0')))
+        contrato = Contrato.from_dictionary(request.POST)
+        try:
+            contrato.full_clean()
+        except ValidationError as errores:
+            datos = datos_xa_render(self.opcion, contrato)
+            datos['contrato'] = contrato
+            datos['errores'] = errores
+            return render(request, 'Proyectos/Contrato/crear-editar.html', datos)
 
-        contrato = Contrato(numero_contrato=numero_contrato, cliente_id=cliente, anho=anho,
-                            supervisor_nombre=supervisor_nombre, supervisor_telefono=supervisor_telefono,
-                            supervisor_correo=supervisor_correo, residente=residente, fecha_inicio=fecha_inicio,
-                            fecha_terminacion=fecha_terminacion, valor=valor,
-                            periodicidad_informes=periodicidad_informes, tiempo=tiempo, tipo_contrato_id=tipo_contrato,
-                            empresa_id=empresa)
+        if Contrato.objects.filter(numero_contrato=contrato.numero_contrato).exists():
+            messages.warning(request, 'Ya existe un contrato con número {0}'.format(contrato.numero_contrato))
+            datos = datos_xa_render(self.opcion, contrato)
+            return render(request, 'Proyectos/Contrato/crear-editar.html', datos)
 
-        if Contrato.objects.filter(numero_contrato=numero_contrato):
-            messages.warning(request, 'Ya existe un contrato con número {0}'.format(numero_contrato))
-            tipo_contratos = TipoContrato.objects \
-                .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            empresas = Empresa.objects \
-                .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            terceros = Tercero.objects.filter(tipo_tercero_id=TipoTercero.CLIENTE) \
-                .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            rango_anho = [{'campo_valor': anho, 'campo_texto': str(anho)} for anho in range(2000, 2051)]
-            opcion = 'crear'
-            return render(request, 'Proyectos/Contrato/crear-editar.html', {'contrato': contrato,
-                                                                            'tipo_contratos': tipo_contratos,
-                                                                            'empresas': empresas,
-                                                                            'terceros': terceros,
-                                                                            'rango_anho': rango_anho,
-                                                                            'opcion': opcion})
+        if Contrato.objects.filter(numero_contrato=contrato.numero_contrato):
+            messages.warning(request, 'Ya existe un contrato con número {0}'.format(contrato.numero_contrato))
+            return render(request, 'Proyectos/Contrato/crear-editar.html', datos_xa_render(self.opcion, contrato))
 
         if contrato.fecha_inicio > contrato.fecha_terminacion:
             messages.warning(request, 'La fecha de inicio debe ser menor o igual a la fecha de terminación')
-            tipo_contratos = TipoContrato.objects \
-                .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            empresas = Empresa.objects \
-                .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            terceros = Tercero.objects.filter(tipo_tercero_id=TipoTercero.CLIENTE) \
-                .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            rango_anho = [{'campo_valor': anho, 'campo_texto': str(anho)} for anho in range(2000, 2051)]
-            opcion = 'crear'
-            return render(request, 'Proyectos/Contrato/crear-editar.html', {'contrato': contrato,
-                                                                            'tipo_contratos': tipo_contratos,
-                                                                            'empresas': empresas,
-                                                                            'terceros': terceros,
-                                                                            'rango_anho': rango_anho, 'opcion': opcion})
+            return render(request, 'Proyectos/Contrato/crear-editar.html', datos_xa_render(self.opcion, contrato))
+
         contrato.save()
-        messages.success(request, 'Se ha agregado el contrato número {0}'.format(numero_contrato))
+        messages.success(request, 'Se ha agregado el contrato número {0}'.format(contrato.numero_contrato))
         return redirect(reverse('proyectos:contratos'))
 
 
 class ContratoEditarView(View):
+    def __init__(self):
+        self.opcion = 'editar'
+        super().__init__()
+
     def get(self, request, id):
         contrato = Contrato.objects.get(id=id)
-        tipo_contratos = TipoContrato.objects \
-            .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-        empresas = Empresa.objects \
-            .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-        terceros = Tercero.objects.filter(tipo_tercero_id=TipoTercero.CLIENTE) \
-            .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-        rango_anho = [{'campo_valor': anho, 'campo_texto': str(anho)} for anho in range(2000, 2051)]
-        opcion = 'editar'
-        return render(request, 'Proyectos/Contrato/crear-editar.html', {'contrato': contrato,
-                                                                        'tipo_contratos': tipo_contratos,
-                                                                        'empresas': empresas,
-                                                                        'terceros': terceros,
-                                                                        'rango_anho': rango_anho, 'opcion': opcion})
+        return render(request, 'Proyectos/Contrato/crear-editar.html', datos_xa_render(self.opcion, contrato))
 
     def post(self, request, id):
         update_fields = ['numero_contrato', 'cliente_id', 'anho', 'supervisor_nombre', 'supervisor_correo',
                          'supervisor_telefono', 'residente', 'fecha_inicio', 'fecha_terminacion', 'valor',
                          'periodicidad_informes', 'tiempo', 'tipo_contrato_id', 'empresa_id']
 
-        contrato = Contrato(id=id)
-        contrato.numero_contrato = request.POST.get('numero_contrato', '')
-        contrato.cliente_id = int(request.POST.get('cliente_id', ''))
-        contrato.anho = int(request.POST.get('rango_anho', ''))
-        contrato.supervisor_nombre = request.POST.get('supervisor_nombre', '')
-        contrato.supervisor_correo = request.POST.get('supervisor_correo', '')
-        contrato.supervisor_telefono = request.POST.get('supervisor_telefono', '')
-        contrato.residente = request.POST.get('residente', '')
-        contrato.fecha_inicio = datetime.strptime(request.POST.get('fecha_inicio', ''), "%Y-%m-%d")
-        contrato.fecha_terminacion = datetime.strptime(request.POST.get('fecha_terminacion', ''), "%Y-%m-%d")
-        contrato.valor = request.POST.get('valor', '')
-        contrato.periodicidad_informes = request.POST.get('periodicidad_informes', '')
-        contrato.tiempo = request.POST.get('tiempo', '')
-        contrato.tipo_contrato_id = int(request.POST.get('tipo_contrato_id', ''))
-        contrato.empresa_id = int(request.POST.get('empresa_id', ''))
+        contrato = Contrato.from_dictionary(request.POST)
+        contrato.id = int(id)
 
-        if Contrato.objects.filter(numero_contrato=contrato.numero_contrato).exclude(id=id):
+        try:
+            contrato.full_clean(validate_unique=False)
+        except ValidationError as errores:
+            datos = datos_xa_render(self.opcion, contrato)
+            datos['contrato'] = contrato
+            datos['errores'] = errores
+            return render(request, 'Proyectos/Contrato/crear-editar.html', datos)
+
+        if Contrato.objects.filter(numero_contrato=contrato.numero_contrato).exclude(id=id).exists():
             messages.warning(request, 'Ya existe un contrato con número {0}'.format(contrato.numero_contrato))
-            tipo_contratos = TipoContrato.objects \
-                .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            empresas = Empresa.objects \
-                .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            terceros = Tercero.objects.filter(tipo_tercero_id=TipoTercero.CLIENTE) \
-                .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            rango_anho = [{'campo_valor': anho, 'campo_texto': str(anho)} for anho in range(2000, 2051)]
-            opcion = 'editar'
-            return render(request, 'Proyectos/Contrato/crear-editar.html',
-                          {'contrato': contrato,
-                           'tipo_contratos': tipo_contratos,
-                           'empresas': empresas,
-                           'terceros': terceros,
-                           'rango_anho': rango_anho, 'opcion': opcion})
+            return render(request, 'Proyectos/Contrato/crear-editar.html', datos_xa_render(self.opcion, contrato))
 
-        elif contrato.fecha_inicio > contrato.fecha_terminacion:
+        if contrato.fecha_inicio > contrato.fecha_terminacion:
             messages.warning(request, 'La fecha de inicio debe ser menor o igual a la fecha de terminación')
-            tipo_contratos = TipoContrato.objects \
-                .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            empresas = Empresa.objects \
-                .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            terceros = Tercero.objects.filter(tipo_tercero_id=TipoTercero.CLIENTE) \
-                .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-            rango_anho = [{'campo_valor': anho, 'campo_texto': str(anho)} for anho in range(2000, 2051)]
-            opcion = 'editar'
-            return render(request, 'Proyectos/Contrato/crear-editar.html',
-                          {'contrato': contrato,
-                           'tipo_contratos': tipo_contratos,
-                           'empresas': empresas,
-                           'terceros': terceros,
-                           'rango_anho': rango_anho, 'opcion': opcion})
+            return render(request, 'Proyectos/Contrato/crear-editar.html', datos_xa_render(self.opcion, contrato))
 
-        elif Contrato.objects.filter(numero_contrato=contrato.numero_contrato, cliente=contrato.cliente_id,
-                                     anho=contrato.anho, supervisor_nombre=contrato.supervisor_nombre,
-                                     supervisor_correo=contrato.supervisor_correo,
-                                     supervisor_telefono=contrato.supervisor_telefono, residente=contrato.residente,
-                                     fecha_inicio__range=(contrato.fecha_inicio, contrato.fecha_inicio),
-                                     fecha_terminacion__range=(contrato.fecha_terminacion, contrato.fecha_terminacion),
-                                     valor=contrato.valor,
-                                     periodicidad_informes=contrato.periodicidad_informes, tiempo=contrato.tiempo,
-                                     tipo_contrato_id=contrato.tipo_contrato_id,
-                                     empresa_id=contrato.empresa_id):
+        contrato_db = Contrato.objects.get(id=id)
+        if contrato_db.comparar(contrato):
             messages.success(request, 'No se hicieron cambios en el contrato número {0}'
                              .format(contrato.numero_contrato))
             return redirect(reverse('Proyectos:contratos'))
-
         else:
             contrato.save(update_fields=update_fields)
             messages.success(request, 'Se ha actualizado el contrato número {0}'.format(contrato.numero_contrato))
@@ -196,3 +110,32 @@ class ContratoEliminarView(View):
 
         except IntegrityError:
             return JsonResponse({"Mensaje": "No se puede eliminar"})
+
+
+# region Métodos de ayuda
+def datos_xa_render(opcion: str, contrato: Contrato = None) -> dict:
+    """
+    Datos necesarios para la creación de los html de Contratos.
+    :param opcion: valor de la acción a realizar 'crea' o 'editar'
+    :param contrato: Es opcional si se requiere pre cargar datos.
+    :return: Un diccionario con los datos.
+    """
+
+    tipo_contratos = TipoContrato.objects.tipos_comerciales(True)\
+        .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
+    empresas = Empresa.objects.filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')) \
+        .order_by('nombre')
+    terceros = Tercero.objects.filter(tipo_tercero_id=TipoTercero.CLIENTE) \
+        .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
+    rango_anho = [{'campo_valor': anho, 'campo_texto': str(anho)} for anho in range(2000, 2051)]
+
+    datos = {'tipo_contratos': tipo_contratos,
+             'empresas': empresas,
+             'terceros': terceros,
+             'rango_anho': rango_anho,
+             'opcion': opcion}
+    if contrato:
+        datos['contrato'] = contrato
+
+    return datos
+# endregion
