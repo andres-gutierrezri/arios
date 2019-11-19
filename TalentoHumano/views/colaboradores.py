6109113abd66ffd075b9b34b72ab5ac06f0a8e03
mauricio.quimbayo <sistemas5@arios-ing.com>
2019-11-19 00:30:54 -0500

    EVA-20 refactor: Se complementan los métodos de ayuda en la vista de colaboradores.

--- TalentoHumano/views/colaboradores.py
+++ TalentoHumano/views/colaboradores.py
@@ -6,9 +6,10 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from django.db.models import F
 
-from Administracion.models import Cargo, Proceso, TipoContrato, CentroPoblado, Rango
+from Administracion.models import Cargo, Proceso, TipoContrato, CentroPoblado, Rango, Municipio, Departamento
 from Proyectos.models import Contrato
-from TalentoHumano.models import Colaboradores, EntidadesCAFE
+from TalentoHumano.models import Colaboradores, EntidadesCAFE, TipoEntidadesCAFE
+
 
 # Create your views here.
 
@@ -60,21 +61,44 @@ def datos_xa_render(opcion: str, colaborador: Colaboradores = None) -> dict:
     :return: Un diccionario con los datos.
     """
 
-    entidad_cafe = EntidadesCAFE.objects.get_xa_select_activos()
+    eps = EntidadesCAFE.objects.filter(tipo_entidad=TipoEntidadesCAFE.EPS)\
+        .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
+    arl = EntidadesCAFE.objects.filter(tipo_entidad=TipoEntidadesCAFE.ARL) \
+        .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
+    afp = EntidadesCAFE.objects.filter(tipo_entidad=TipoEntidadesCAFE.AFP) \
+        .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
+    caja_compensacion = EntidadesCAFE.objects.filter(tipo_entidad=TipoEntidadesCAFE.CAJA_COMPENSACION) \
+        .values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
     jefe_inmediato = Colaboradores.objects \
-        .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('nombre')).order_by('nombre')
-    contrato = Contrato.objects.get_xa_select_activos()
+        .filter(estado=True).values(campo_valor=F('id'), campo_texto=F('identificacion')).order_by('identificacion')
+    contrato = Contrato.objects \
+        .filter(tipo_contrato__laboral=True).values(campo_valor=F('id'), campo_texto=F('numero_contrato'))\
+        .order_by('numero_contrato')
     cargo = Cargo.objects.get_xa_select_activos()
     proceso = Proceso.objects.get_xa_select_activos()
-    tipo_contrato = TipoContrato.objects.get_xa_select_activos()
-    lugar_nacimiento = CentroPoblado.objects.get_xa_select_activos()
+    tipo_contratos = TipoContrato.objects.tipos_laborares(True, True)
+    departamentos = Departamento.objects.get_xa_select_activos()
     rango = Rango.objects.get_xa_select_activos()
+    talla_camisa = [{'campo_valor': talla_camisa, 'campo_texto': str(talla_camisa)} for talla_camisa in
+                    ['S', 'M', 'L', 'XL']]
+    talla_pantalon = [{'campo_valor': talla_pantalon, 'campo_texto': str(talla_pantalon)} for talla_pantalon in
+                      range(6, 45)]
+    talla_zapatos = [{'campo_valor': talla_zapatos, 'campo_texto': str(talla_zapatos)} for talla_zapatos in
+                     range(20, 47)]
 
-    datos = {'entidad_cafe': entidad_cafe, 'jefe_inmediato': jefe_inmediato,
-             'contrato': contrato, 'cargo': cargo, 'proceso': proceso, 'tipo_contrato': tipo_contrato,
-             'lugar_nacimiento': lugar_nacimiento, 'rango': rango, 'opcion': opcion}
+    datos = {'arl': arl, 'eps': eps, 'afp': afp, 'caja_compensacion': caja_compensacion,
+             'jefe_inmediato': jefe_inmediato, 'contrato': contrato, 'cargo': cargo, 'proceso': proceso,
+             'tipo_contrato': tipo_contratos, 'rango': rango, 'departamentos': departamentos,
+             'talla_camisa': talla_camisa, 'talla_zapatos': talla_zapatos, 'talla_pantalon': talla_pantalon,
+             'opcion': opcion}
 
     if colaborador:
+        municipios = Municipio.objects.get_xa_select_activos() \
+            .filter(departamento_id=colaborador.lugar_nacimiento.municipio.departamento_id)
+        lugar_nacimiento = CentroPoblado.objects.get_xa_select_activos() \
+            .filter(municipio_id=colaborador.lugar_nacimiento.municipio_id)
+        datos['municipios'] = municipios
+        datos['lugar_nacimiento'] = lugar_nacimiento
         datos['colaborador'] = colaborador
 
     return datos
