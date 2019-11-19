987246e59c60a1fdfbd6e73f14c6b5ede2ffa156
mauricio.quimbayo <sistemas5@arios-ing.com>
2019-11-19 00:28:36 -0500

    EVA-20 feat: Se incluye datepicker en el módulo de colaboradores

---
+++ TalentoHumano/static/TalentoHumano/js/date_picker.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let controls = {
+	leftArrow: '<i class="fal fa-angle-left" style="font-size: 1.25rem"></i>',
+	rightArrow: '<i class="fal fa-angle-right" style="font-size: 1.25rem"></i>'
+};
+
+let runDatePicker = function () {
+
+	// minimum setup
+	$('#fecha_ingreso_id').datepicker({
+		todayHighlight: true,
+		orientation: "bottom left",
+		templates: controls,
+		format: 'yyyy-mm-dd',
+		autoclose: true
+	});
+
+	$('#fecha_examen_id').datepicker({
+		todayHighlight: true,
+		orientation: "bottom left",
+		templates: controls,
+		format: 'yyyy-mm-dd',
+		autoclose: true
+	});
+
+	$('#fecha_dotacion_id').datepicker({
+		todayHighlight: true,
+		orientation: "bottom left",
+		templates: controls,
+		format: 'yyyy-mm-dd',
+		autoclose: true
+	});
+};
+
+$(document).ready(function () {
+	runDatePicker();
+});
+
