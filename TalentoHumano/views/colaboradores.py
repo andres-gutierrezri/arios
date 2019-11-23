f54647129fb73a44a5fd1d6eaf8a710bed1462ce
Mauricio Pena <sistemas5@arios-ing.com>
2019-11-23 12:32:34 -0500

    EVA-20 refactor: Se modifica nombre del modelo a Colaborador.

--- TalentoHumano/views/colaboradores.py
+++ TalentoHumano/views/colaboradores.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, redirect, reverse
 from Administracion.models import Cargo, Proceso, TipoContrato, CentroPoblado, Rango, Municipio, Departamento, \
     TipoIdentificacion
 from Proyectos.models import Contrato
-from TalentoHumano.models import Colaboradores, EntidadesCAFE
+from TalentoHumano.models import Colaborador, EntidadesCAFE
 
 
 # Create your views here.
@@ -17,9 +17,7 @@ class ColaboradoresIndexView(View):
 
     def get(self, request):
 
-        colaboradores = Colaboradores.objects.all()
-        # for colaborador in colaboradores:
-        #     print(colaborador.usuario.first_name)
+        colaboradores = Colaborador.objects.all()
         fecha = datetime.now()
         print(colaboradores)
         return render(request, 'TalentoHumano/Colaboradores/index.html', {'fecha': fecha,
@@ -33,7 +31,7 @@ class ColaboradoresCrearView(View):
         return render(request, 'TalentoHumano/Colaboradores/crear-editar.html', datos_xa_render(self.OPCION))
 
     def post(self, request):
-        colaborador = Colaboradores.from_dictionary(request.POST)
+        colaborador = Colaborador.from_dictionary(request.POST)
         try:
             # Se excluye el usuario debido a que el id no es asginado sino hasta después de ser guardado en la BD.
             colaborador.full_clean(exclude=['usuario'])
@@ -42,27 +40,25 @@ class ColaboradoresCrearView(View):
             datos['errores'] = errores.message_dict
             return render(request, 'TalentoHumano/Colaboradores/crear-editar.html', datos)
 
-        if Colaboradores.objects.filter(identificacion=colaborador.identificacion).exists():
+        if Colaborador.objects.filter(identificacion=colaborador.identificacion).exists():
             messages.warning(request, 'Ya existe un colaborador con identificación {0}'
                              .format(colaborador.identificacion))
             return render(request, 'TalentoHumano/Colaboradores/crear-editar.html',
                           datos_xa_render(self.OPCION, colaborador))
 
-        nombre = colaborador.usuario.first_name
-        apellido = colaborador.usuario.last_name
         colaborador.usuario.save()
+        # Se realiza esto ya que el campo usuario_id del modelo no es asignado automáticamente despues de guardar el ususario en la BD.
+        colaborador.usuario_id = colaborador.usuario.id
         colaborador.save()
-        print(nombre)
 
-        messages.success(request, 'Se ha agregado el colaborador  {0}'.format(nombre) + ' ' +
-                         '{0}'.format(apellido))
+        messages.success(request, 'Se ha agregado el colaborador  {0}'.format(colaborador.nombre_completo))
         return redirect(reverse('TalentoHumano:colaboradores-index'))
 
 
 # region Métodos de ayuda
 
 
-def datos_xa_render(opcion: str, colaborador: Colaboradores = None) -> dict:
+def datos_xa_render(opcion: str, colaborador: Colaborador = None) -> dict:
     """
     Datos necesarios para la creación de los html de Colaboradores.
     :param opcion: valor de la acción a realizar 'crear' o 'editar'
@@ -74,7 +70,7 @@ def datos_xa_render(opcion: str, colaborador: Colaboradores = None) -> dict:
     arl = EntidadesCAFE.objects.arl_xa_select()
     afp = EntidadesCAFE.objects.afp_xa_select()
     caja_compensacion = EntidadesCAFE.objects.caja_compensacion_xa_select()
-    jefe_inmediato = Colaboradores.objects.get_xa_select()
+    jefe_inmediato = Colaborador.objects.get_xa_select()
     contrato = Contrato.objects.get_xa_select()
     cargo = Cargo.objects.get_xa_select()
     proceso = Proceso.objects.get_xa_select()
