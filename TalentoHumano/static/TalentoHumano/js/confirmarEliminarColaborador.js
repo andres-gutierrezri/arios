818bc1be9b6a508469cb59c949ff047bb77f544e
mauricio.quimbayo <sistemas5@arios-ing.com>
2019-11-24 22:28:30 -0500

    EVA-20 feat: Se adiciona archivo JS para eliminar colaboradores.

---
+++ TalentoHumano/static/TalentoHumano/js/confirmarEliminarColaborador.js
@@ -0,0 +1,46 @@
+let URLDomain = document.location.origin+"/";
+let idBorrar = 0;
+let urlFinal;
+let rutaBorrado = $('#rutaBorrado').val();
+
+function fConfirmarEliminarColaborador(idElemento) {
+    fSweetAlert();
+    idBorrar = idElemento;
+}
+
+function fSweetAlert() {
+    Swal.fire({
+        title: '¿Está seguro de eliminar este colaborador?',
+        text: "Esta acción no se podrá revertir",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: '¡Sí, eliminarlo!',
+        cancelButtonText: 'Cancelar'
+    }).then(result => {
+
+        if (result.value) {
+            $.ajax({
+                url: URLDomain + rutaBorrado + "/" + idBorrar + "/delete",
+                type: 'POST',
+                context: document.body,
+                success: function (data) {
+                    if(data.Mensaje === "OK") {
+                        location.reload();
+                    }
+                    else {
+                        location.reload();
+                    }
+                },
+                failure: function (errMsg) {
+                    location.reload();
+                }
+            });
+        }
+    });
+}
+
+
+
+
+
+
