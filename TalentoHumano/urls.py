f8b99c2a0792798cf0e01ca7e1067cdfe80f752e
mauricio.quimbayo <sistemas5@arios-ing.com>
2019-11-24 22:35:34 -0500

    EVA-20 feat: Se crea las url para eliminar colaboradores y para visualizar el perfil de estos.

--- TalentoHumano/urls.py
+++ TalentoHumano/urls.py
@@ -27,5 +27,9 @@ urlpatterns = [
     path('entidades/<int:id>/delete', entidades_cafe.EntidadCAFEEliminarView.as_view(), name='entidades-eliminar'),
     path('colaboradores-index/', colaboradores.ColaboradoresIndexView.as_view(), name='colaboradores-index'),
     path('colaboradores/add', colaboradores.ColaboradoresCrearView.as_view(), name='colaboradores-crear'),
-    path('colaboradores/<int:id>/', colaboradores.ColaboradorEditarView.as_view(), name='colaboradores-editar')
+    path('colaboradores/<int:id>/', colaboradores.ColaboradorEditarView.as_view(), name='colaboradores-editar'),
+    path('colaboradores/<int:id>/delete', colaboradores.ColaboradorEliminarView.as_view(),
+         name='colaboradores-eliminar'),
+    path('colaboradores-perfil/<int:id>/', colaboradores.ColaboradoresPerfilView.as_view(),
+         name='colaboradores-perfil'),
 ]
