73b4808b206d00c58d52c5de537f7a243ee0553d
Mauricio Pena <sistemas5@arios-ing.com>
2019-11-25 18:39:00 -0500

    EVA-20 feat: Se crea templatetag para entradas tipo file.

--- EVA/templatetags/etiquetas_generales.py
+++ EVA/templatetags/etiquetas_generales.py
@@ -62,14 +62,15 @@ def input_email_tag(nombre, texto_label, **kwargs):
 
     return arma_input_general_tag(nombre, **kwargs)
 
+
 @register.inclusion_tag('EVA/_general_tags/_input_general_tag.html')
-def input_image_tag(nombre, texto_label, **kwargs):
+def input_file_tag(nombre, texto_label, **kwargs):
 
     kwargs.pop('texto_label', None)
     kwargs.pop('type', None)
 
     kwargs['texto_label'] = texto_label
-    kwargs['type'] = u'image'
+    kwargs['type'] = u'file'
 
     return arma_input_general_tag(nombre, **kwargs)
 
