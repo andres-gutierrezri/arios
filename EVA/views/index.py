3a249f977a5f7eead049b328d335dfa677904b42
Mauricio Pena <sistemas5@arios-ing.com>
2019-12-03 18:33:32 -0500

    EVA-22 feat: Se renombra vista y se crea clase abstracta con el fin de garantizar que el usuario haya iniciado sesión antes de hacer uso de cualquier aplicación

--- EVA/views/index.py
+++ EVA/views/index.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+
+from django.shortcuts import redirect, render, reverse
+from django.views import View
 
 
-def index_view(request):
-    return render(request, 'EVA/index.html')
+class AbstractEvaLoggedView(View):
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return super(AbstractEvaLoggedView, self).dispatch(*args, **kwargs)
+        else:
+            return redirect(reverse('administracion:iniciar-sesion'))
+
+
+class IndexView(AbstractEvaLoggedView):
+    def get(self, request):
+        return render(request, 'EVA/index.html')
