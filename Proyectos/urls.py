947edabcedaed69cb448c2a4fa69d9c0627877c7
Mauricio Pena <sistemas5@arios-ing.com>
2019-12-03 18:35:21 -0500

    EVA-22 refactor: Se renombra vista en la url del index

--- Proyectos/urls.py
+++ Proyectos/urls.py
@@ -20,7 +20,7 @@ from Proyectos.views import proyectos, contratos
 app_name = 'Proyectos'
 
 urlpatterns = [
-    path('index/', proyectos.index, name='index'),
+    path('index/', proyectos.Index.as_view(), name='index'),
     path('contratos/', contratos.ContratoView.as_view(), name='contratos'),
     path('contratos/add', contratos.ContratoCrearView.as_view(), name='contratos-crear'),
     path('contratos/<int:id>/', contratos.ContratoEditarView.as_view(), name='contratos-editar'),
