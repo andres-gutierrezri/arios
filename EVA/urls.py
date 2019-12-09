c988ac129b2194daf926c9dfdd644e0330656f47
Mauricio Pena <sistemas5@arios-ing.com>
2019-12-09 18:21:23 -0500

    EVA-24 feat: Se agregan url para enviar el correo

--- EVA/urls.py
+++ EVA/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from EVA.views.index import IndexView
 
@@ -26,4 +27,24 @@ urlpatterns = [
     path('administracion/', include('Administracion.urls', namespace='administracion')),
     path('proyectos/', include('Proyectos.urls', namespace='proyectos')),
     path('talento-humano/', include('TalentoHumano.urls', namespace='talento-humano')),
+
+    path('correo/', include('django.contrib.auth.urls')),
+    path('password-reset', auth_views.PasswordResetView.as_view(
+            template_name='TalentoHumano/correo/password_reset.html',
+            html_email_template_name="accounts/password_reset_complete.html",
+            email_template_name='accounts/password_reset_email.html',
+            subject_template_name="accounts/password_reset_subject.txt",
+            ), name="password_reset"),
+    path('password-reset/done',
+         auth_views.PasswordResetDoneView.as_view(template_name='TalentoHumano/correo/password_reset_done.html'),
+         name="password_reset_done"),
+
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='TalentoHumano/correo/password_reset_complete.html')
+         , name="password_reset_complete"),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='TalentoHumano/correo/password_reset_confirm.html'),
+         name="password_reset_confirm"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
