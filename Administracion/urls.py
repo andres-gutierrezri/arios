90006826725b549ad7fe2017415598757b3657aa
Mauricio Pena <sistemas5@arios-ing.com>
2019-12-10 18:17:27 -0500

    EVA-24 feat: Se crea url terminar-sesion para cerrar la sesión que se encuentre actualmente abierta

--- Administracion/urls.py
+++ Administracion/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
          name='centros-poblados-json'),
     path('iniciar-sesion', inicio_sesion.IniciarSesionView.as_view(), name='iniciar-sesion'),
     path('cerrar-sesion', inicio_sesion.CerrarSesion.as_view(), name='cerrar-sesion'),
+    path('terminar-sesion', inicio_sesion.TerminarSesion.as_view(), name='terminar-sesion'),
 ]
