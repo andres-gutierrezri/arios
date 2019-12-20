d8218bdf1a00a27891036989d6d2210d151d52f2
Mauricio Pena <sistemas5@arios-ing.com>
2019-12-20 18:28:07 -0500

    EVA-26 feat: Se crea función en JS para cargar el modal para seleccionar la empresa

---
+++ Administracion/static/Administracion/js/seleccionEmpresa.js
@@ -0,0 +1,45 @@
+let URLDomainEmpresa = document.location.origin+"/";
+
+$(document).ready(function () {
+
+    $('.seleccionEmpresaGlobal').click(function () {
+
+        $.ajax({
+            url: URLDomainEmpresa + "administracion/empresas/modal-seleccion",
+            context: document.body
+        }).done(function (response) {
+            let mdEmpresa = $("#selectEmpresaGlobal");
+            mdEmpresa.html(response);
+            mdEmpresa.modal('show');
+        });
+    });
+});
+
+function cerrarModalEmpresa() {
+    $("#selectEmpresaGlobal").modal('hide');
+    $(".modal-backdrop").remove();
+}
+
+function clicEmpresa(idEmpresa) {
+
+    let jsonEmpresa = {"idEmpresa":idEmpresa};
+
+    $.ajax({
+        url: URLDomainEmpresa + "administracion/empresas/modal-seleccion",
+        data: JSON.stringify(jsonEmpresa),
+        type: 'POST',
+        contentType: "application/json; charset=utf-8;",
+        dataType: "json",
+        success: function (data) {
+            cerrarModalEmpresa();
+            if(data.Mensaje === "OK") {
+                location.reload();
+            }
+        },
+        failure: function (errMsg) {
+            alert('Se presentó un error. No se pudó seleccionar la empresa.');
+            cerrarModalEmpresa();
+            location.reload();
+        }
+    });
+}
