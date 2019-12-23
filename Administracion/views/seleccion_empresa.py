b317295b47adaa8a47058d2c57ff0eaea443a00f
Mauricio Pena <sistemas5@arios-ing.com>
2019-12-23 11:15:04 -0500

    EVA-26 feat: Se realiza la selección de la empresa y al iniciar sesión se carga el logo de la última emrpesa a la cual accedió el colaborador

--- Administracion/views/seleccion_empresa.py
+++ Administracion/views/seleccion_empresa.py
@@ -13,15 +13,20 @@ from TalentoHumano.models.colaboradores import ColaboradorContrato, Colaborador
 class SeleccionEmpresaModalView(AbstractEvaLoggedView):
     def get(self, request):
         empresas = ColaboradorContrato.objects.filter(colaborador__usuario=request.user).distinct('contrato__empresa')
+        colaborador = Colaborador.objects.get(usuario=request.user)
 
-        return render(request, 'Administracion/_common/_modal_seleccion_empresa.html', {'empresas': empresas})
+        return render(request, 'Administracion/_common/_modal_seleccion_empresa.html', {'empresas': empresas,
+                                                                                        'colaborador': colaborador})
 
     def post(self, request):
         try:
             body_unicode = request.body.decode('utf-8')
             body = json.loads(body_unicode)
-            request.session['empresa'] = Empresa.objects.get(id=int(body['idEmpresa'])).empresa_to_json()
-
+            empresa = Empresa.objects.get(id=int(body['idEmpresa']))
+            request.session['empresa'] = empresa.empresa_to_json()
+            colaborador = Colaborador.objects.get(usuario=request.user)
+            colaborador.empresa_sesion_id = int(body['idEmpresa'])
+            colaborador.save(update_fields=['empresa_sesion_id'])
             return JsonResponse({"Mensaje": "OK"})
         except:
             return JsonResponse({"Mensaje": "Falló"})
