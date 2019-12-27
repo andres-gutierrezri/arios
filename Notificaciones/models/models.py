f8f3ef75f2cbd4f02d756ae8c0c1095e794d0061
Miguel Alejandro Cepeda <sistemas@arios-ing.com>
2019-12-27 10:16:20 -0500

    EVA-30 Refactor: Se agregan dos constantes al modelo de Evento Desencadenador

--- Notificaciones/models/models.py
+++ Notificaciones/models/models.py
@@ -47,10 +47,12 @@ class EventoDesencadenador(models.Model):
         verbose_name_plural = 'Eventos Desencadenadores'
 
     # region Constantes Estados
+    BIENVENIDA = 1
     CONTRATO = 1
     TERCERO = 2
     EMPRESAS = 3
     ENTIDADES_CAFE = 4
+    COLABORADOR = 5
     # endregion
 
 
