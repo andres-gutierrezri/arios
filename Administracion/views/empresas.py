3eeb847236680011e156cc07080be65b25f65a58
Mauricio Pena <sistemas5@arios-ing.com>
2020-01-04 13:24:12 -0500

    EVA-33 feat: Se crea la vista para visualizar subempresas.

--- Administracion/views/empresas.py
+++ Administracion/views/empresas.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.http import JsonResponse
 from django.urls import reverse
 
+from Administracion.utils import get_id_empresa_global
 from EVA.views.index import AbstractEvaLoggedView
 from datetime import datetime
 from django.shortcuts import render, redirect
@@ -14,7 +15,7 @@ from Administracion.models import Empresa
 
 class EmpresaView(AbstractEvaLoggedView):
     def get(self, request):
-        empresas = Empresa.objects.all()
+        empresas = Empresa.objects.filter(subempresa=False)
         fecha = datetime.now()
         return render(request, 'Administracion/Empresas/index.html', {'empresas': empresas, 'fecha': fecha})
 
@@ -100,6 +101,38 @@ class EmpresaEliminarView(AbstractEvaLoggedView):
             messages.warning(request, 'No se puede eliminar la empresa {0}'.format(empresa.nombre) +
                              ' porque ya se encuentra asociado a otros módulos')
             return JsonResponse({"Mensaje": "No se puede eliminar"})
+
+
+class SubempresasView(AbstractEvaLoggedView):
+    def get(self, request):
+        subempresas = Empresa.objects.filter(subempresa=True, empresa_ppal_id=get_id_empresa_global(request))
+        return render(request, 'Administracion/Subempresas/index.html', {'subempresas': subempresas,
+                                                                         'fecha': datetime.now()})
+
+
+class SubempresaCrearView(AbstractEvaLoggedView):
+    def get(self, request):
+        OPCION = 'crear'
+        return render(request, 'Administracion/Subempresas/crear-editar.html', datos_xa_render(OPCION))
+
+    def post(self, request):
+
+        nombre = request.POST.get('nombre', '')
+        nit = request.POST.get('nit', '')
+        logo = request.FILES.get('logo', None)
+        empresa_ppal_id = get_id_empresa_global(request)
+
+        if Empresa.objects.filter(nit=nit):
+            messages.warning(request, 'No se puede eliminar la empresa {0}'.format(empresa.nombre) +
+                             ' porque ya se encuentra asociado a otros módulos')
+            error = 'Ya existe una empresa con ese número de NIT'
+            return render(request, 'Inventario/Subempresas/crear-editar.html', {'error': error})
+
+        subempresa = Empresa(nombre=nombre, nit=nit, logo=logo, estado=True,
+                             subempresa=True, empresa_ppal_id=empresa_ppal_id)
+        subempresa.save()
+
+        return redirect(reverse('inventario:subempresas'))
 
 
 def datos_xa_render(opcion: str, empresa: Empresa = None) -> dict:
