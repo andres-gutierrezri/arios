08aaf6d7e23f954b6e2686b1f1416a0aaf46f29b
Mauricio Pena <sistemas5@arios-ing.com>
2020-01-18 12:15:47 -0500

    EVA-36 feat: Se crea la vista para controlar el index de SGI

---
+++ SGI/views/sgi.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from EVA.views.index import AbstractEvaLoggedView
+
+
+class Index(AbstractEvaLoggedView):
+    def get(self, request):
+        return render(request, 'SGI/index.html')
