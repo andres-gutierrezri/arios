877a17378590d3afece9a50a34cf712aa67ee5fc
Alfonso Ardila <asesorsistemas@arios-ing.com>
2020-01-18 04:44:40 -0500

    EVA-38 feat: Se agrega a la administración de Django Impuestos y Resolución de facturación.

--- Administracion/admin.py
+++ Administracion/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from Administracion.models import Impuesto
+
+
+class ImpuestoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'porcentaje', 'aplica_item', 'aplica_global', 'estado', 'usuario_crea')
+    list_display_links = ('id', 'nombre')
+    search_fields = ('id', 'nombre', 'porcentaje')
+
+
+admin.site.register(Impuesto, ImpuestoAdmin)
