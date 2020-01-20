13bb600c592e0af3caf20ad03ece03ce1398b87a
Mauricio Pena <sistemas5@arios-ing.com>
2020-01-20 18:42:55 -0500

    EVA-36 feat: Se define el proceso en el que se encuentra el usuario.

--- SGI/views/documentos.py
+++ SGI/views/documentos.py
@@ -10,7 +10,8 @@ class IndexView(AbstractEvaLoggedView):
     def get(self, request, id):
         documentos = Documento.objects.filter(grupo_documento_id=id)
         procesos = Proceso.objects.all()
+        proceso = Proceso.objects.get(id=id)
         grupo_documentos = GrupoDocumento.objects.all()
         return render(request, 'SGI/documentos/index.html', {'documentos': documentos, 'procesos': procesos,
-                                                             'grupo_documentos': grupo_documentos})
+                                                             'grupo_documentos': grupo_documentos, 'proceso': proceso})
 
