9b840fa5a8388c4ed02c4e49f199b98e00421a79
Alfonso Ardila <asesorsistemas@arios-ing.com>
2020-02-06 22:43:47 -0500

    EVA-38 test: Se realizan ajuste de los test del modelo de factura.
    En tre estos que la llave unica sea compuesta por la empresa y el númeor de factura encabezado.

--- Financiero/test/test_facturacion_models.py
+++ Financiero/test/test_facturacion_models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal, ROUND_DOWN, ROUND_05UP, localcontext
 from unittest import TestCase
 
 from Administracion.models import Impuesto
@@ -7,7 +8,7 @@ from Financiero.models import FacturaEncabezado
 class TestFacturaEncabezado(TestCase):
     factura_encabezado_1 = FacturaEncabezado(porcentaje_administracion=22.00, porcentaje_imprevistos=3.00,
                                              porcentaje_utilidad=10.00, subtotal=22712038.00)
-    factura_encabezado_2 = FacturaEncabezado(impuesto=Impuesto(id=1), valor_impuesto=1730964.67, subtotal=9110340.36)
+    factura_encabezado_2 = FacturaEncabezado(valor_impuesto=1730964.67, subtotal=9110340.36)
     factura_encabezado_3 = FacturaEncabezado(amortizacion=41331.84, subtotal=103329.60)
 
     def setUp(self):
@@ -24,19 +25,19 @@ class TestFacturaEncabezado(TestCase):
         self.assertEqual(self.factura_encabezado_3.is_amortizacion, True)
 
     def test_total_administracion(self):
-        self.assertEqual(self.factura_encabezado_1.total_administracion, 4996648.36)
-        self.assertEqual(self.factura_encabezado_2.total_administracion, 0.00)
-        self.assertEqual(self.factura_encabezado_3.total_administracion, 0.00)
+        self.assertEqual(self.factura_encabezado_1.total_administracion, Decimal('4996648.36'))
+        self.assertEqual(self.factura_encabezado_2.total_administracion, Decimal('0.00'))
+        self.assertEqual(self.factura_encabezado_3.total_administracion, Decimal('0.00'))
 
     def test_total_imprevistos(self):
-        self.assertEqual(self.factura_encabezado_1.total_imprevistos, 681361.14)
-        self.assertEqual(self.factura_encabezado_2.total_imprevistos, 0.00)
-        self.assertEqual(self.factura_encabezado_3.total_imprevistos, 0.00)
+        self.assertEqual(self.factura_encabezado_1.total_imprevistos, Decimal('681361.14'))
+        self.assertEqual(self.factura_encabezado_2.total_imprevistos, Decimal('0.00'))
+        self.assertEqual(self.factura_encabezado_3.total_imprevistos, Decimal('0.00'))
 
     def test_total_utilidad(self):
-        self.assertEqual(self.factura_encabezado_1.total_utilidad, 2271203.80)
-        self.assertEqual(self.factura_encabezado_2.total_utilidad, 0.00)
-        self.assertEqual(self.factura_encabezado_3.total_utilidad, 0.00)
+        self.assertEqual(self.factura_encabezado_1.total_utilidad, Decimal('2271203.80'))
+        self.assertEqual(self.factura_encabezado_2.total_utilidad, Decimal('0.00'))
+        self.assertEqual(self.factura_encabezado_3.total_utilidad, Decimal('0.00'))
 
     def test_total_amortizacion(self):
         self.assertEqual(self.factura_encabezado_1.total_amortizacion, 0.00)
@@ -49,6 +50,7 @@ class TestFacturaEncabezado(TestCase):
         self.assertEqual(self.factura_encabezado_3.total_impuesto, 0.00)
 
     def test_total_factura(self):
-        self.assertEqual(self.factura_encabezado_1.total_factura, 30661251.30)
-        self.assertEqual(self.factura_encabezado_2.total_factura, 10841305.03)
-        self.assertEqual(self.factura_encabezado_3.total_factura, 61997.76)
+        self.assertEqual(self.factura_encabezado_1.total_factura, Decimal('30661251.30'))
+        self.assertEqual(self.factura_encabezado_2.total_factura, Decimal('10841305.03'))
+        self.assertEqual(self.factura_encabezado_3.total_factura, Decimal('61997.76'))
+
