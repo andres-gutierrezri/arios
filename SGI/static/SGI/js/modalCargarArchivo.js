55b261d201b72dbcc9ff83278422f7863e861c79
Alejandro Cepeda <sistemas@arios-ing.com>
2020-03-12 10:25:50 -0500

    EVA-45 Refactor: Se pasa la funcion que carga el modal desde el template a el js

--- SGI/static/SGI/js/modalCargarArchivo.js
+++ SGI/static/SGI/js/modalCargarArchivo.js
@@ -1,3 +1,16 @@
 $('#archivo_id').change(function(e){
     $('.custom-file-label').html(e.target.files[0].name);
 });
+
+function abrir_modal_cargar(url) {
+    $('#cargar').load(url, function () {
+        $(this).modal('show');
+        $('#fecha_id').datepicker({
+            todayHighlight: true,
+            orientation: "bottom left",
+            templates: controls,
+            format: 'yyyy-mm-dd',
+            autoclose: true
+        });
+    });
+}
