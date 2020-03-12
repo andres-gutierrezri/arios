4e501e190e3bb633b90a56c0b1b362db5f4cc323
Alejandro Cepeda <sistemas@arios-ing.com>
2020-03-12 17:20:52 -0500

    EVA-46 Fix: Se soluciona el error al seleccionar la fecha en cargar documento Se deja var y no let, porque aun colocandole el 'use strict' se detecta que la variable ya ha sido declarada en otro documento

--- SGI/static/SGI/js/modalCargarArchivo.js
+++ SGI/static/SGI/js/modalCargarArchivo.js
@@ -2,10 +2,15 @@ $('#archivo_id').change(function(e){
     $('.custom-file-label').html(e.target.files[0].name);
 });
 
+var controls = {
+	leftArrow: '<i class="fal fa-angle-left" style="font-size: 1.25rem"></i>',
+	rightArrow: '<i class="fal fa-angle-right" style="font-size: 1.25rem"></i>'
+};
+
 function abrir_modal_cargar(url) {
     $('#cargar').load(url, function () {
         $(this).modal('show');
-        $('#fecha_id').datepicker({
+        $('#fecha_documento_id').datepicker({
             todayHighlight: true,
             orientation: "bottom left",
             templates: controls,
