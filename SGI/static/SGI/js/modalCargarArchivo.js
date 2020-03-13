7754a5502fe1e29bc36d9075ea89509903de2d7b
Alfonso Ardila <asesorsistemas@arios-ing.com>
2020-03-12 22:41:43 -0500

    EVA-46 feat: Se agrega validación el lado del cliente al formulario de cargar archivo a un documento en la aplicación SGI.

--- SGI/static/SGI/js/modalCargarArchivo.js
+++ SGI/static/SGI/js/modalCargarArchivo.js
@@ -1,7 +1,3 @@
-$('#archivo_id').change(function(e){
-    $('.custom-file-label').html(e.target.files[0].name);
-});
-
 var controls = {
 	leftArrow: '<i class="fal fa-angle-left" style="font-size: 1.25rem"></i>',
 	rightArrow: '<i class="fal fa-angle-right" style="font-size: 1.25rem"></i>'
@@ -17,5 +13,9 @@ function abrir_modal_cargar(url) {
             format: 'yyyy-mm-dd',
             autoclose: true
         });
+        $('#archivo_id').change(function(e){
+            $('.custom-file-label').html(e.target.files[0].name);
+        });
+        agregarValidacionFormularios();
     });
 }
