d0295da7eb5aaa9844d58b23c28bf5c7711cffcc
Alejandro Cepeda <sistemas@arios-ing.com>
2020-03-14 11:43:04 -0500

    EVA-47 Feat: Se agrega la validacion de permisos en las vistas de terceros

--- Administracion/views/terceros.py
+++ Administracion/views/terceros.py
@@ -16,104 +16,124 @@ from EVA.views.index import AbstractEvaLoggedView
 
 class TerceroView(AbstractEvaLoggedView):
     def get(self, request):
-        terceros = Tercero.objects.all()
-        fecha = datetime.now()
-        return render(request, 'Administracion/Tercero/index.html', {'terceros': terceros, 'fecha': fecha})
+        if not request.user.has_perms(['Administracion.view_tercero', 'TalentoHumano.can_menu_administracion']):
+            return redirect(reverse('eva-index'))
+        else:
+            terceros = Tercero.objects.all()
+            fecha = datetime.now()
+            return render(request, 'Administracion/Tercero/index.html', {'terceros': terceros, 'fecha': fecha})
 
 
 class TerceroCrearView(AbstractEvaLoggedView):
     OPCION = 'crear'
 
     def get(self, request):
-        return render(request, 'Administracion/Tercero/crear-editar.html', datos_xa_render(self.OPCION))
+        if not request.user.has_perms(['Administracion.add_tercero', 'TalentoHumano.can_menu_administracion']):
+            return redirect(reverse('eva-index'))
+        else:
+            return render(request, 'Administracion/Tercero/crear-editar.html', datos_xa_render(self.OPCION))
 
     def post(self, request):
-        tercero = Tercero.from_dictionary(request.POST)
-        tercero.empresa_id = get_id_empresa_global(request)
-        tercero.estado = True
-        try:
-            tercero.full_clean()
-        except ValidationError as errores:
-            datos = datos_xa_render(self.OPCION, tercero)
-            datos['errores'] = errores.message_dict
-            if 'identificacion' in errores.message_dict:
-                for mensaje in errores.message_dict['identificacion']:
-                    if mensaje.startswith('Ya existe'):
-                        messages.warning(request,
-                                         'Ya existe un tercero con identificación {0}'.format(tercero.identificacion))
-                        break
-            return render(request, 'Administracion/Tercero/crear-editar.html', datos)
+        if not request.user.has_perms(['Administracion.add_tercero', 'TalentoHumano.can_menu_administracion']):
+            return redirect(reverse('eva-index'))
+        else:
+            tercero = Tercero.from_dictionary(request.POST)
+            tercero.empresa_id = get_id_empresa_global(request)
+            tercero.estado = True
+            try:
+                tercero.full_clean()
+            except ValidationError as errores:
+                datos = datos_xa_render(self.OPCION, tercero)
+                datos['errores'] = errores.message_dict
+                if 'identificacion' in errores.message_dict:
+                    for mensaje in errores.message_dict['identificacion']:
+                        if mensaje.startswith('Ya existe'):
+                            messages.warning(request,
+                                             'Ya existe un tercero con identificación {0}'.format(tercero.identificacion))
+                            break
+                return render(request, 'Administracion/Tercero/crear-editar.html', datos)
 
-        tercero.save()
-        messages.success(request, 'Se ha agregado el tercero {0}'.format(tercero.nombre))
-        return redirect(reverse('Administracion:terceros'))
+            tercero.save()
+            messages.success(request, 'Se ha agregado el tercero {0}'.format(tercero.nombre))
+            return redirect(reverse('Administracion:terceros'))
 
 
 class TerceroEditarView(AbstractEvaLoggedView):
     OPCION = 'editar'
 
     def get(self, request, id):
-        tercero = Tercero.objects.get(id=id)
-        return render(request, 'Administracion/Tercero/crear-editar.html', datos_xa_render(self.OPCION, tercero))
-
-    def post(self, request, id):
-        update_fields = ['nombre', 'identificacion', 'tipo_identificacion_id', 'estado',
-                         'fecha_modificacion', 'tipo_tercero_id', 'centro_poblado_id', 'telefono', 'fax', 'direccion']
-
-        tercero = Tercero.from_dictionary(request.POST)
-        tercero.empresa_id = get_id_empresa_global(request)
-        tercero.id = int(id)
-
-        try:
-            tercero.full_clean(validate_unique=False)
-        except ValidationError as errores:
-            datos = datos_xa_render(self.OPCION, tercero)
-            datos['errores'] = errores.message_dict
-            return render(request, 'Administracion/Tercero/crear-editar.html', datos)
-
-        if Tercero.objects.filter(identificacion=tercero.identificacion).exclude(id=id).exists():
-            messages.warning(request, 'Ya existe un tercero con identificación {0}'.format(tercero.identificacion))
+        if not request.user.has_perms(['Administracion.change_tercero', 'TalentoHumano.can_menu_administracion']):
+            return redirect(reverse('eva-index'))
+        else:
+            tercero = Tercero.objects.get(id=id)
             return render(request, 'Administracion/Tercero/crear-editar.html', datos_xa_render(self.OPCION, tercero))
 
-        tercero_db = Tercero.objects.get(id=id)
-        if tercero_db.comparar(tercero):
-            messages.success(request, 'No se hicieron cambios en el tercero {0}'.format(tercero.nombre))
-            return redirect(reverse('Administracion:terceros'))
-
+    def post(self, request, id):
+        if not request.user.has_perms(['Administracion.change_tercero', 'TalentoHumano.can_menu_administracion']):
+            return redirect(reverse('eva-index'))
         else:
+            update_fields = ['nombre', 'identificacion', 'tipo_identificacion_id', 'estado',
+                             'fecha_modificacion', 'tipo_tercero_id', 'centro_poblado_id', 'telefono', 'fax', 'direccion']
 
-            tercero.save(update_fields=update_fields)
-            messages.success(request, 'Se ha actualizado el tercero {0}'.format(tercero.nombre)
-                             + ' con identificación {0}'.format(tercero.identificacion))
+            tercero = Tercero.from_dictionary(request.POST)
+            tercero.empresa_id = get_id_empresa_global(request)
+            tercero.id = int(id)
 
-            return redirect(reverse('Administracion:terceros'))
+            try:
+                tercero.full_clean(validate_unique=False)
+            except ValidationError as errores:
+                datos = datos_xa_render(self.OPCION, tercero)
+                datos['errores'] = errores.message_dict
+                return render(request, 'Administracion/Tercero/crear-editar.html', datos)
+
+            if Tercero.objects.filter(identificacion=tercero.identificacion).exclude(id=id).exists():
+                messages.warning(request, 'Ya existe un tercero con identificación {0}'.format(tercero.identificacion))
+                return render(request, 'Administracion/Tercero/crear-editar.html', datos_xa_render(self.OPCION, tercero))
+
+            tercero_db = Tercero.objects.get(id=id)
+            if tercero_db.comparar(tercero):
+                messages.success(request, 'No se hicieron cambios en el tercero {0}'.format(tercero.nombre))
+                return redirect(reverse('Administracion:terceros'))
+
+            else:
+
+                tercero.save(update_fields=update_fields)
+                messages.success(request, 'Se ha actualizado el tercero {0}'.format(tercero.nombre)
+                                 + ' con identificación {0}'.format(tercero.identificacion))
+
+                return redirect(reverse('Administracion:terceros'))
 
 
 class TerceroEliminarView(AbstractEvaLoggedView):
     def post(self, request, id):
-        try:
-            tercero = Tercero.objects.get(id=id)
-            tercero.delete()
-            messages.success(request, 'Se ha eliminado el tercero {0}'.format(tercero.nombre))
-            return JsonResponse({"Mensaje": "OK"})
+        if not request.user.has_perms(['Administracion.delete_tercero', 'TalentoHumano.can_menu_administracion']):
+            return redirect(reverse('eva-index'))
+        else:
+            try:
+                tercero = Tercero.objects.get(id=id)
+                tercero.delete()
+                messages.success(request, 'Se ha eliminado el tercero {0}'.format(tercero.nombre))
+                return JsonResponse({"Mensaje": "OK"})
 
-        except IntegrityError:
-            tercero = Tercero.objects.get(id=id)
-            messages.warning(request, 'No se puede eliminar el tercero {0}'.format(tercero.nombre) +
-                             ' porque ya se encuentra asociado a otros módulos')
-            return JsonResponse({"Mensaje": "No se puede eliminar"})
+            except IntegrityError:
+                tercero = Tercero.objects.get(id=id)
+                messages.warning(request, 'No se puede eliminar el tercero {0}'.format(tercero.nombre) +
+                                 ' porque ya se encuentra asociado a otros módulos')
+                return JsonResponse({"Mensaje": "No se puede eliminar"})
 
 
 class TerceroDetalleView(AbstractEvaLoggedView):
     def get(self, request, id):
-
-        try:
-            tercero = Tercero.objects.get(id=id)
-            return JsonResponse({'estado': 'OK', 'datos': tercero.to_dict(campos=['id', 'identificacion',
-                                                                                  'direccion', 'telefono',
-                                                                                  'fax', 'correo'])})
-        except Tercero.DoesNotExist:
-            return JsonResponse({"estado": "error", "mensaje": 'El cliente seleccionado no existe.'})
+        if not request.user.has_perms(['Administracion.view_tercero', 'TalentoHumano.can_menu_administracion']):
+            return redirect(reverse('eva-index'))
+        else:
+            try:
+                tercero = Tercero.objects.get(id=id)
+                return JsonResponse({'estado': 'OK', 'datos': tercero.to_dict(campos=['id', 'identificacion',
+                                                                                      'direccion', 'telefono',
+                                                                                      'fax', 'correo'])})
+            except Tercero.DoesNotExist:
+                return JsonResponse({"estado": "error", "mensaje": 'El cliente seleccionado no existe.'})
 
 
 # region Métodos de ayuda
