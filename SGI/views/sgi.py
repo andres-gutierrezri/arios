646961f3a2f3d6b279ef0c7ba99f782a6a07cea7
Alejandro Cepeda <sistemas@arios-ing.com>
2020-03-17 09:30:44 -0500

    EVA-47 Feat: Se agrega la validación de permisos en la vista index de SGI

--- SGI/views/sgi.py
+++ SGI/views/sgi.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from Administracion.models import Proceso
+from EVA.General.validacionpermisos import tiene_permisos
+from EVA.views.index import AbstractEvaLoggedView
 
 # Create your views here.
-from Administracion.models import Proceso
-from EVA.views.index import AbstractEvaLoggedView
 
 
 class Index(AbstractEvaLoggedView):
-    def get(self, request):
-        procesos = Proceso.objects.all()
-        return render(request, 'SGI/index.html', {'procesos': procesos})
+        def get(self, request):
+            if not tiene_permisos(request, 'SGI', None, None):
+                return redirect(reverse('eva-index'))
+            else:
+                procesos = Proceso.objects.all()
+                return render(request, 'SGI/index.html', {'procesos': procesos})
