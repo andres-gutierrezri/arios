0f1f2c7cb36bd57981d3b9e840fc56d8e5d717ee
Alejandro Cepeda <sistemas@arios-ing.com>
2020-03-19 10:15:15 -0500

    EVA-54 Refactor: Se implementa la validación de permisos nueva en SGI Se quita la validación de permisos anterior Se agregan los permisos necesarios al archivo de permisos de SGI

--- SGI/permisos.py
+++ SGI/permisos.py
@@ -3,4 +3,10 @@ from EVA.permisos import Permiso
 app_name = 'SGI'
 
 permisos = [Permiso('index', None),
-            Permiso('documentos-index', ['SGI.view_documento'])]
+            Permiso('documentos-index', ['SGI.view_documento']),
+            Permiso('documentos-crear', ['SGI.add_documento']),
+            Permiso('documentos-editar', ['SGI.change_documento']),
+            Permiso('documentos-eliminar', ['SGI.delete_documento']),
+            Permiso('documentos-cargar', ['SGI.add_archivo']),
+            Permiso('documentos-ver', ['SGI.view_archivo']),
+            ]
