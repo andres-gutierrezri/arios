a9b71ff58e4177134b9becec469a84a4fbaa614f
Alejandro Cepeda <sistemas@arios-ing.com>
2020-03-20 18:15:20 -0500

    EVA-55 Fix: Se filtra por la empresa seleccionara los procesos en SGI

--- SGI/views/sgi.py
+++ SGI/views/sgi.py
@@ -5,5 +5,5 @@ from EVA.views.index import AbstractEvaLoggedView
 
 class Index(AbstractEvaLoggedView):
         def get(self, request):
-            procesos = Proceso.objects.all()
+            procesos = Proceso.objects.filter(empresa_id=request.session['empresa']['id'])
             return render(request, 'SGI/index.html', {'procesos': procesos})
