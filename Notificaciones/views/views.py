5fd80c5fedff125b4569f5418cc748c66818d08a
Alejandro Cepeda <sistemas@arios-ing.com>
2020-03-25 18:44:00 -0500

    EVA-57 Feat: Se crea la vista para ver notificaciones

--- Notificaciones/views/views.py
+++ Notificaciones/views/views.py
@@ -1,5 +1,10 @@
 import datetime
 
+from django.db import IntegrityError
+from django.http import JsonResponse
+
+from EVA.General.conversiones import distancia_entre_fechas_a_texto
+from EVA.views.index import AbstractEvaLoggedView
 from Notificaciones.models.models import TextoNotificacionDelSistema, Notificacion, TipoNotificacion, \
     SeleccionDeNotificacionARecibir, DestinatarioNotificacion, EventoDesencadenador
 
@@ -40,3 +45,45 @@ def crear_destinatarios(notificacion, lista_usuarios):
                                                     envio_email_exitoso=False,
                                                     notificacion=notificacion,
                                                     usuario_id=seleccion)
+
+
+class NotificacionesView(AbstractEvaLoggedView):
+    def get(self, request):
+        try:
+            contador = 0
+            limite_notificaciones = 10
+            destinatarios = DestinatarioNotificacion.objects.filter(usuario=request.user)\
+                .order_by('-notificacion__fecha_creacion')
+            lista_notificaciones = []
+            for destinatario in destinatarios:
+                if destinatario.notificacion.fecha_creacion.date() <= datetime.date.today():
+                    fecha = distancia_entre_fechas_a_texto(destinatario.notificacion.fecha_creacion)
+                    contador += 1
+
+                    if destinatario.notificacion.tipo_notificacion_id == TipoNotificacion.EVENTO_DEL_SISTEMA:
+                        url = destinatario.notificacion.evento_desencadenador.ruta
+                        evento = destinatario.notificacion.id_evento
+                    else:
+                        url = '/notificaciones/detalle-info-oblig'
+                        evento = destinatario.notificacion.id
+
+                    lista_notificaciones.append(
+                        {"id": destinatario.notificacion.id,
+                         "titulo": destinatario.notificacion.titulo,
+                         "mensaje": destinatario.notificacion.mensaje,
+                         "fecha": fecha,
+                         "visto": destinatario.visto,
+                         "id_evento": evento,
+                         "url": url})
+                    if contador > limite_notificaciones:
+                        break
+            contador_notificaciones = 0
+            for dest in destinatarios:
+                if not dest.visto and dest.notificacion.fecha_creacion.date() <= datetime.date.today():
+                    contador_notificaciones += 1
+            return JsonResponse({"Mensaje": contador_notificaciones,
+                                 "Notificaciones": lista_notificaciones})
+
+        except IntegrityError:
+
+            return JsonResponse({"Mensaje": "Error interno del servidor", "Notificaciones": []})
