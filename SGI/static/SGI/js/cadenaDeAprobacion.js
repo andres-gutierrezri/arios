aca0ecda147d0157f189d38d26a3a14865ec2b22
Alejandro Cepeda <sistemas@arios-ing.com>
2020-03-31 20:02:41 -0500

    EVA-50 Feat: Se edita el js que controla los select de cadena de aprobación para poder editar Se realizan modificaciones para que la funcion se ejecute correctamente al editar y al crear

--- SGI/static/SGI/js/cadenaDeAprobacion.js
+++ SGI/static/SGI/js/cadenaDeAprobacion.js
@@ -6,12 +6,34 @@ var usuarios_seleccionados = $('#usuarios_seleccionados');
 
 $(document).ready(function () {
     var temp  = contador;
-
-    while(temp > 0){
-        contenedor.append(crearSelectores(temp, true));
+    if(valores_selectores.opcion === 'editar'){
+        var orden = 1;
+        while (orden <= temp){
+            $("#agregar_" + (orden-1)).hide();
+            $("#eliminar_" + (orden-1)).hide();
+            valores_selectores.selecciones.forEach(function (item) {
+                if (item.orden === orden){
+                    contenedor.append(crearSelectores(item.orden, item.usuario_id));
+                    selecciones.push(item.usuario_id);
+                }
+                if (orden === 1){
+                     $("#eliminar_" + (orden)).hide();
+                }
+            });
+            orden += 1
+        }
+        orden_anteior = orden - 1;
+        if(orden > valores_selectores.colaboradores.length){
+             $("#agregar_" + (orden_anteior)).hide();
+        }
+         $('#colaborador_id_' + orden_anteior).removeAttr('disabled', true);
+    }else{
+        while(temp > 0){
+        contenedor.append(crearSelectores(temp));
         temp -= 1;
     }
     $("#eliminar_1").hide();
+    }
 });
 
 function agregar() {
@@ -42,14 +64,18 @@ function eliminar(id) {
     usuarios_seleccionados.val(selecciones);
 }
 
-function crearSelectores(posicion) {
+function crearSelectores(posicion, usuario_id) {
     var valor;
-    var opciones_colaborador = crearOpciones();
+    var opciones_colaborador = crearOpciones(usuario_id);
+    var desactivar = '';
+    if (usuario_id){
+        desactivar = 'disabled="disabled"'
+    }
 
     valor = '<div class="form-group" id="elemento_'+ posicion +'"><div class="form-row">' +
             '<div class="col-md-11">' +
             '<label for="colaborador_id_'+ posicion +'">Usuario '+ posicion +' </label>'+
-            '<select class="select2 form-control" name="ultimo_usuario" id="colaborador_id_'+ posicion +'">'+ opciones_colaborador +'</select></div>'+
+            '<select class="select2 form-control" '+ desactivar +' name="ultimo_usuario" id="colaborador_id_'+ posicion +'">'+ opciones_colaborador +'</select></div>'+
             '<div class="col-md-1" style="padding-top:30px" id="botones_'+ posicion +'">'+
             '<a class="far fa-2x far fa-plus" id="agregar_'+ posicion +'" href="#" onclick="agregar()" title="" data-original-title="Agregar Usuario"></a>'+
             '<a class="far fa-2x far fa-times" id="eliminar_'+ posicion +'" href="#" onclick="eliminar('+posicion+')" title="" data-original-title="Eliminar Usuario"></a>'+
@@ -57,16 +83,20 @@ function crearSelectores(posicion) {
     return valor;
 }
 
-function crearOpciones() {
+function crearOpciones(usuario_id) {
     opciones_colaborador = [];
     valores_selectores['colaboradores'].forEach(function(usuario) {
         var existe = false;
         selecciones.forEach(function (id) {
-            if (usuario['campo_valor'].toString() === id){
+            if (usuario['campo_valor'].toString() === id.toString()){
                 existe = true}
         });
         if (!existe){
-            opciones_colaborador += ('<option value="'+ usuario['campo_valor']+ '">'+ usuario['campo_texto']+ '</option>')}
+            var seleccionar = '';
+            if (usuario_id === usuario.campo_valor ){
+                seleccionar = 'selected ';
+            }
+            opciones_colaborador += ('<option '+ seleccionar +'value="'+ usuario.campo_valor + '">'+ usuario.campo_texto + '</option>')}
     });
     return opciones_colaborador;
 }
