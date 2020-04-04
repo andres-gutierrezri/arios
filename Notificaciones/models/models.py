7e1365b8aac61396cc4e351459d82dcf701f975e
Alfonso Ardila <asesorsistemas@arios-ing.com>
2020-04-04 16:21:22 -0500

    app notificaciones  fix: Se corriguen otografía en los modelos de notifiaciones y en la migración con la que se crean datos en al BD.

--- Notificaciones/models/models.py
+++ Notificaciones/models/models.py
@@ -15,10 +15,10 @@ class TipoNotificacion(models.Model):
         return self.nombre
 
     class Meta:
-        verbose_name = 'Tipo de Notificacion'
+        verbose_name = 'Tipo de Notificación'
         verbose_name_plural = 'Tipos de Notificaciones'
 
-    # region Constantes Tipos de Notificacion
+    # region Constantes Tipos de Notificación
     EVENTO_DEL_SISTEMA = 1
     OBLIGATORIA = 2
     INFORMATIVA = 3
@@ -51,12 +51,12 @@ class EventoDesencadenador(models.Model):
 
 
 class Notificacion(models.Model):
-    titulo = models.CharField(max_length=100, verbose_name='Titulo', null=False, blank=False)
+    titulo = models.CharField(max_length=100, verbose_name='Título', null=False, blank=False)
     mensaje = models.TextField(max_length=300, verbose_name='Mensaje', null=False, blank=False)
     fecha_creacion = models.DateTimeField(verbose_name='Fecha de Creación', null=False,
                                           blank=False, )
     tipo_notificacion = models.ForeignKey(TipoNotificacion, on_delete=models.DO_NOTHING,
-                                          verbose_name='Tipo Notificacion', null=True, blank=False)
+                                          verbose_name='Tipo Notificación', null=True, blank=False)
     evento_desencadenador = models.ForeignKey(EventoDesencadenador, on_delete=models.DO_NOTHING,
                                               verbose_name='Evento Desencadenador', null=True, blank=False)
     id_evento = models.IntegerField(verbose_name='Id Evento', null=True, blank=False)
@@ -68,7 +68,7 @@ class Notificacion(models.Model):
         return self.titulo
 
     class Meta:
-        verbose_name = 'Notificacion'
+        verbose_name = 'Notificación'
         verbose_name_plural = 'Notificaciones'
 
 
@@ -76,34 +76,34 @@ class DestinatarioNotificacion(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="Usuario", null=False, blank=False)
     visto = models.BooleanField(verbose_name='Visto', null=False, blank=False)
     notificacion = models.ForeignKey(Notificacion, on_delete=models.DO_NOTHING,
-                                     verbose_name='Tipo Notificacion', null=True, blank=False)
-    envio_email_exitoso = models.BooleanField(verbose_name='Envio Email Exitoso', null=False, blank=False)
+                                     verbose_name='Tipo Notificación', null=True, blank=False)
+    envio_email_exitoso = models.BooleanField(verbose_name='Envío Email Exitoso', null=False, blank=False)
 
     def __str__(self):
         return self.usuario.first_name
 
     class Meta:
-        verbose_name = 'Destinatario Notificacion'
+        verbose_name = 'Destinatario Notificación'
         verbose_name_plural = 'Destinatarios de Notificaciones'
 
 
 class SeleccionDeNotificacionARecibir(models.Model):
     evento_desencadenador = models.ForeignKey(EventoDesencadenador, on_delete=models.DO_NOTHING,
                                               verbose_name='Evento Desencadenador', null=True, blank=False)
-    envio_x_email = models.BooleanField(verbose_name='Envio Por Email', null=False, blank=False)
+    envio_x_email = models.BooleanField(verbose_name='Envío por Email', null=False, blank=False)
     usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="Usuario", null=False, blank=False)
     estado = models.BooleanField(verbose_name='Estado', null=False, blank=False)
 
     def __str__(self):
-        return '{0} (Seleccion: {1})'.format(self.evento_desencadenador.nombre, self.usuario)
+        return '{0} (Selección: {1})'.format(self.evento_desencadenador.nombre, self.usuario)
 
     class Meta:
-        verbose_name = 'Seleccion de Notificacion A Recibir'
-        verbose_name_plural = 'Selecciones de Notificaciones A Recibir'
+        verbose_name = 'Selección de Notificación a Recibir'
+        verbose_name_plural = 'Selecciones de Notificaciones a Recibir'
 
 
 class TextoNotificacionDelSistema(models.Model):
-    titulo = models.CharField(max_length=100, verbose_name='Titulo', null=False, blank=False)
+    titulo = models.CharField(max_length=100, verbose_name='Título', null=False, blank=False)
     mensaje = models.TextField(max_length=300, verbose_name='Mensaje', null=False, blank=False)
     fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación', null=False,
                                           blank=False)
@@ -116,5 +116,5 @@ class TextoNotificacionDelSistema(models.Model):
         return self.titulo
 
     class Meta:
-        verbose_name = 'Texto de Notificacion del Sistema'
+        verbose_name = 'Texto de Notificación del Sistema'
         verbose_name_plural = 'Textos de Notificaciones del Sistema'
