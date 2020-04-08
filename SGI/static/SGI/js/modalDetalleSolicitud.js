2cae21b2f776e68e72731748876ad9ab5f56b64b
Alejandro Cepeda <sistemas@arios-ing.com>
2020-04-08 15:33:51 -0500

    EVA-62 Feat: Se crea el template y el JS para detalle de solicitud de aprobación

---
+++ SGI/static/SGI/js/modalDetalleSolicitud.js
@@ -0,0 +1,18 @@
+function abrir_modal_detalle_solicitud(url) {
+    $('#detalle_solicitud').load(url, function (responseText) {
+        try {
+            if(responseText.includes("<!DOCTYPE html>")){
+                EVANotificacion.toast.error('No tiene permisos para acceder a esta funcionalidad');
+                return false;
+            }
+        $(this).modal('show');
+        }
+    catch(err) {
+            console.log(err);
+        EVANotificacion.toast.error('Ha ocurrido un al ver el detalle');
+    }
+    });
+}
+$(document).ready(function() {
+    iniciarTablaExportar([0, 1, 2, 3]);
+});
