3be43ebe3ff49aca3c695c031ba0c9bb1b266daa
Alejandro Cepeda <sistemas@arios-ing.com>
2020-04-11 11:19:45 -0500

    EVA-64 Fix: Se corrige la ortografía en el js confirmar eliminar

--- SGI/static/SGI/js/confirmarEliminarSGI.js
+++ SGI/static/SGI/js/confirmarEliminarSGI.js
@@ -11,7 +11,7 @@ function fConfirmarEliminar(idElemento) {
 
 function fSweetAlert() {
     Swal.fire({
-        title: '¿Está seguro de eliminar este Item?',
+        title: '¿Está seguro de eliminar este ítem?',
         text: "Esta acción no se podrá revertir",
         type: 'warning',
         showCancelButton: true,
@@ -38,7 +38,7 @@ function fSweetAlert() {
                     location.reload();
                     Swal.fire({
                             title: "¡Error!",
-                            text: "Ha ocurrido un error eliminando el item",
+                            text: "Ha ocurrido un error eliminando el ítem",
                             type: 'error',
                         });
                 }
