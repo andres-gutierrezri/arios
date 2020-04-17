29d0f9e3794624a8140ac620f8a4f9557910b827
Alejandro Cepeda <sistemas@arios-ing.com>
2020-04-17 11:30:46 -0500

    EVA-67 Feat: Se modifican la vista de asignacion de  notifcaciones para guarde las selecciones

--- Notificaciones/views/asignacion.py
+++ Notificaciones/views/asignacion.py
@@ -1,17 +1,33 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from EVA.views.index import AbstractEvaLoggedView
-from Notificaciones.models.models import EventoDesencadenador
+from Notificaciones.models.models import EventoDesencadenador, SeleccionDeNotificacionARecibir
 
 
 class AsignacionView(AbstractEvaLoggedView):
     def get(self, request, id):
-        desencadenadores = EventoDesencadenador.objects.all()
+        selecciones = SeleccionDeNotificacionARecibir.objects.filter(usuario_id=id)
+        desencadenadores = EventoDesencadenador.objects.exclude(id=EventoDesencadenador.BIENVENIDA)\
+            .exclude(permiso=None)
+        lista_desencadenador = []
+        for desencadenador in desencadenadores:
+            if User.objects.get(id=id).has_perms([desencadenador.permiso]):
+                lista_desencadenador.append({'id': desencadenador.id,
+                                             'nombre': desencadenador.nombre,
+                                             'descripcion': desencadenador.descripcion})
+
         return render(request, 'Notificaciones/AsignacionNotificaciones/asignacion.html',
-                      {"desencadenadores": desencadenadores,
+                      {"desencadenadores": lista_desencadenador,
+                       "selecciones": selecciones,
                        "colaborador": id})
 
     def post(self, request, id):
         selecciones = request.POST.getlist("desencadenadores", [])
+        SeleccionDeNotificacionARecibir.objects.filter(usuario_id=id).delete()
+        for seleccion in selecciones:
+            SeleccionDeNotificacionARecibir.objects.create(usuario_id=id, estado=True, envio_x_email=False,
+                                                           evento_desencadenador_id=seleccion)
+
         return redirect(reverse('TalentoHumano:colaboradores-index', args=[0]))
