e751f328211b711ba5c1f6a185f456bb3b018555
Alejandro Cepeda <sistemas@arios-ing.com>
2020-04-29 14:45:47 -0500

    EVA-75 Refactor: Se cambia la condicion del filtro que guarda las selecciones en editar cadena de aprobación Porque en ocasiones no estaba eliminando todas las cadenas de aprobación, haciendo que se repitieran registros

--- SGI/views/cadena_aprobacion.py
+++ SGI/views/cadena_aprobacion.py
@@ -100,10 +100,10 @@ class CadenaAprobacionEditarView(AbstractEvaLoggedView):
         if not Archivo.objects.filter(cadena_aprobacion_id=id) and \
                 not Documento.objects.filter(cadena_aprobacion_id=id):
             if usuarios_seleccionados:
-                CadenaAprobacionDetalle.objects.filter(cadena_aprobacion=cadena).delete()
+                CadenaAprobacionDetalle.objects.filter(cadena_aprobacion_id=id).delete()
             orden = 1
             for usuarios in selecciones:
-                CadenaAprobacionDetalle.objects.create(cadena_aprobacion=cadena, usuario_id=usuarios, orden=orden)
+                CadenaAprobacionDetalle.objects.create(cadena_aprobacion_id=id, usuario_id=usuarios, orden=orden)
                 orden += 1
 
         cadena.save(update_fields=['nombre', 'estado'])
