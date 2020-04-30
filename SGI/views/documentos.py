5d0d4522dd0596f57804a719085f0427df8b7859
Alejandro Cepeda <sistemas@arios-ing.com>
2020-04-30 18:34:55 -0500

    EVA-76 Fix: Se corrige el al guardar un archivo de tipo enlace No estaba pasando la validación de tener un formato valido

--- SGI/views/documentos.py
+++ SGI/views/documentos.py
@@ -184,7 +184,7 @@ class ArchivoCargarView(AbstractEvaLoggedView):
         elif tipo_archivo == 'enlace':
             archivo.enlace = request.POST.get('enlace', '')
             archivo.archivo = None
-            if archivo.enlace.find('http://') < 0 or archivo.enlace.find('https://') < 0:
+            if archivo.enlace.find('http://') < 0 and archivo.enlace.find('https://') < 0:
                 messages.error(request, 'El enlace que ha ingresado, no tiene el formato correcto. <br>'
                                         'Ejemplo: https://www.arios-ing.com')
                 return redirect(reverse('SGI:documentos-index', args=[id_proceso]))
