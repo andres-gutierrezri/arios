bf40089293e0bf5f4703d40c7e17ac789a99d9ef
Alejandro Cepeda <sistemas@arios-ing.com>
2020-04-30 10:42:00 -0500

    EVA-77 Feat: Se agrega una modificación para que al hacer clic en los botones de historial, cambie de color

--- SGI/static/SGI/js/arbolDocumentos.js
+++ SGI/static/SGI/js/arbolDocumentos.js
@@ -1,4 +1,12 @@
-function arbolDocumentos(id) {
+function arbolDocumentos(id, id_accionador) {
+    let accionador = $('#'+ id_accionador);
     let children = $("#"+id);
-    (children.is(':visible') ? children.hide('fast') : children.show('fast'));
+
+    if(children.is(':visible')) {
+        children.hide('fast');
+        accionador.removeClass('color-primary-50')
+    }else{
+        children.show('fast');
+        accionador.addClass('color-primary-50')
+    }
 }
