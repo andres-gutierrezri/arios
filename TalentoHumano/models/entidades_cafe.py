7db6495f17117701b9fa96a99e4d77cee7a8292f
Alejandro Cepeda <sistemas@arios-ing.com>
2020-05-04 19:37:51 -0500

    EVA-78 Fix: Se corrige el error en el ingreso de telefono de contacto en entidades CAFE

--- TalentoHumano/models/entidades_cafe.py
+++ TalentoHumano/models/entidades_cafe.py
@@ -58,7 +58,7 @@ class EntidadesCAFE(models.Model, ModelDjangoExtensiones):
     nombre = models.CharField(max_length=100, verbose_name='Nombre', null=False, blank=False)
     direccion = models.CharField(max_length=100, verbose_name='Dirección', null=False, blank=False)
     nombre_contacto = models.CharField(max_length=100, verbose_name='Nombre de contacto', null=False, blank=False)
-    telefono_contacto = models.IntegerField(verbose_name='Teléfono de contacto', null=False, blank=False)
+    telefono_contacto = models.CharField(max_length=30, verbose_name='Teléfono de contacto', null=False, blank=False)
     correo = models.EmailField(max_length=100, verbose_name='Correo del supervisor', null=False, blank=False,
                                error_messages={'invalid': "Ingrese una dirección de correo válida"})
     direccion_web = models.CharField(max_length=100, verbose_name='Dirección web', null=False, blank=False)
