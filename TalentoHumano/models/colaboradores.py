c09ee02bf92b0b53bd25a18ac8bdb74b5097e967
Alejandro Cepeda <sistemas@arios-ing.com>
2020-05-06 18:10:19 -0500

    EVA-79 Feat: Se crea el icono en el upbar para gestión documental, Tambien se crea el permiso para el botón

--- TalentoHumano/models/colaboradores.py
+++ TalentoHumano/models/colaboradores.py
@@ -76,6 +76,7 @@ class Colaborador(Persona, ModelDjangoExtensiones):
                        ("can_menu_talentohumano", "Can access menu talento humano"),
                        ("can_menu_sgi", "Can access menu sgi"),
                        ("can_menu_financiero", "Can access menu financiero"),
+                       ("can_menu_gestiondocumental", "Can access menu gestion documental"),
                        ("view_historial", "Can view historial"),
                        ("view_archivo_historial", "Can view archivo historial"),
                        )
