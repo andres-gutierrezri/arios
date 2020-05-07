963908e6a33c0a0824f0540ed0688074de53627c
Alejandro Cepeda <sistemas@arios-ing.com>
2020-05-07 11:41:22 -0500

    EVA-79 Feat: Se crea el archivo de permisos para la aplicación Gestión Documental

--- GestionDocumental/apps.py
+++ GestionDocumental/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from EVA.permisos import incluir_permisos_app
 
 
 class GestiondocumentalConfig(AppConfig):
     name = 'GestionDocumental'
+    incluir_permisos_app('GestionDocumental.permisos')
