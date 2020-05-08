7a83baf327b912dae43a7565a4c9f33f63a01018
Alejandro Cepeda <sistemas@arios-ing.com>
2020-05-07 20:25:30 -0500

    EVA-80 Feat: Se agrega el auto_now_add a la fecha del modelo de Consecutivo Documento

--- GestionDocumental/models/models.py
+++ GestionDocumental/models/models.py
@@ -8,7 +8,7 @@ from Proyectos.models import Contrato
 class ConsecutivoDocumento(models.Model):
     objects = ManagerGeneral()
     consecutivo = models.CharField(max_length=50, verbose_name='Consecutivo', null=False, blank=False)
-    fecha = models.DateTimeField(verbose_name='Fecha', null=False, blank=False)
+    fecha = models.DateTimeField(auto_now_add=True, verbose_name='Fecha', null=False, blank=False)
     contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE, verbose_name='Contrato', null=True, blank=True)
     detalle = models.CharField(max_length=100, verbose_name='Detalle', null=False, blank=False)
     destinatario = models.CharField(max_length=100, verbose_name='Destinatario', null=False, blank=False)
@@ -21,3 +21,17 @@ class ConsecutivoDocumento(models.Model):
     class Meta:
         verbose_name = 'Consecutivo Documento'
         verbose_name_plural = 'Consecutivos Documentos'
+
+    @staticmethod
+    def from_dictionary(datos: dict) -> 'ConsecutivoDocumento':
+        """
+        Crea una instancia de ConsecutivoDocumento con los datos pasados en el diccionario.
+        :param datos: Diccionario con los datos para crear el Consecutivo de Documento.
+        :return: Instacia de consecutivo de documento con la información especificada en el diccionario.
+        """
+        consecutivo = ConsecutivoDocumento()
+        consecutivo.contrato_id = datos.get('contrato_id', None)
+        consecutivo.detalle = datos.get('detalle', '')
+        consecutivo.destinatario = datos.get('destinatario', '')
+
+        return consecutivo
