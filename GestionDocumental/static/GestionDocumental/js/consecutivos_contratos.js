19fcd1a11138756c35ced56073aa6945bd1a2d77
Alejandro Cepeda <sistemas@arios-ing.com>
2020-06-08 16:50:16 -0500

    EVA-89 Refactor: Se modifica el js de consecutivos de contrato para controlar los inputs con el tipo de contrato Se controla el colaborador, tercero y la fecha de fin, dependiendo del tipo de contrato.

--- GestionDocumental/static/GestionDocumental/js/consecutivos_contratos.js
+++ GestionDocumental/static/GestionDocumental/js/consecutivos_contratos.js
@@ -1,65 +1,42 @@
 'use strict';
-
+let extra_tipos_contrato = $('#extra_tipos_contrato');
+let fechaInicioID = $('#fecha_inicio_id');
+let fechaFinalID = $('#fecha_final_id');
 let colaboradorSelect = $('#colaborador_mostrar_id');
 let terceroSelect = $('#tercero_mostrar_id');
 let fechaFinal = $('#fecha_final_mostrar');
-let fechaInicioID = $('#fecha_inicio_id');
-let fechaFinalID = $('#fecha_final_id');
-let tipoTerminacionSelect = $("#tipo_terminacion_select_id");
-let colaboradorSelectID = $('#colaborador_select_id');
-let terceroSelectID = $('#tercero_select_id');
 
 $(document).ready(function () {
     $('.select2').select2();
-
-    mostrarXTipoContrato();
-
-    tipoTerminacionSelect.change(function () {
-        mostrarXTipoContrato()
+    $('#tipo_contrato_select_id').change(function () {
+        let actual = this.value;
+        $.each(jQuery.parseJSON(extra_tipos_contrato.val()), function(key, value) {
+            if(actual == value.id){
+              if (value.laboral){
+                  colaboradorSelect.show();
+                  terceroSelect.hide();
+              }else{
+                  colaboradorSelect.hide();
+                  terceroSelect.show();
+              }
+              if(value.fecha_fin){
+                  fechaFinal.show();
+              }else{
+                  fechaFinal.hide();
+              }
+            }
+        });
+        if (actual === ""){
+             colaboradorSelect.show();
+             terceroSelect.hide();
+             fechaFinal.show();
+        }
     });
 
     $('#tipo_consecutivos_select_id').change(function () {
         window.location = '/gestion-documental/consecutivo-contratos/' + this.value + '/index';
     });
 });
-
-function mostrarXTipoContrato() {
-    if(tipoTerminacionSelect.val() === '1'){
-        mostrarOcultarColaboradorTercero('colaborador');
-        fechaFinal.show();
-        fechaFinalID.attr("required", true);
-    }else if(tipoTerminacionSelect.val() === '2'){
-        mostrarOcultarColaboradorTercero('colaborador');
-        fechaFinal.hide();
-        fechaFinalID.removeAttr('required', true);
-    }else if(tipoTerminacionSelect.val() === '3'){
-        mostrarOcultarColaboradorTercero('tercero');
-        fechaFinal.show();
-        fechaFinalID.attr("required", true);
-    }else if(tipoTerminacionSelect.val() === '4'){
-        mostrarOcultarColaboradorTercero('tercero');
-        fechaFinal.hide();
-        fechaFinalID.removeAttr('required', true);
-    }else{
-        mostrarOcultarColaboradorTercero('ninguno');
-        fechaFinal.show();
-        fechaFinalID.attr("required", true);
-    }
-}
-
-function mostrarOcultarColaboradorTercero(tipo) {
-    if (tipo === 'tercero'){
-        terceroSelect.show();
-        terceroSelectID.attr("required", true);
-        colaboradorSelect.hide();
-        colaboradorSelectID.removeAttr('required', true);
-    }else{
-        colaboradorSelect.show();
-        colaboradorSelectID.attr("required", true);
-        terceroSelect.hide();
-        terceroSelectID.removeAttr('required', true);
-    }
-}
 
 fechaInicioID.change(function () {
     if (new Date(fechaInicioID.val()) > new Date(fechaFinalID.val())) {
