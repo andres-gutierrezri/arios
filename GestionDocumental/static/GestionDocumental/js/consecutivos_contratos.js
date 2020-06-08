71a517342c5d1beb9fcf1cd1d2eca23e9fca46b2
Alejandro Cepeda <sistemas@arios-ing.com>
2020-06-08 17:12:42 -0500

    EVA-89 Refactor: Se agregan condicionales para agregar y quitar los required de las fechas y los selects

--- GestionDocumental/static/GestionDocumental/js/consecutivos_contratos.js
+++ GestionDocumental/static/GestionDocumental/js/consecutivos_contratos.js
@@ -4,6 +4,9 @@ let fechaInicioID = $('#fecha_inicio_id');
 let fechaFinalID = $('#fecha_final_id');
 let colaboradorSelect = $('#colaborador_mostrar_id');
 let terceroSelect = $('#tercero_mostrar_id');
+let colaboradorSelectID = $('#colaborador_select_id');
+let terceroSelectID = $('#tercero_select_id');
+
 let fechaFinal = $('#fecha_final_mostrar');
 
 $(document).ready(function () {
@@ -13,23 +16,23 @@ $(document).ready(function () {
         $.each(jQuery.parseJSON(extra_tipos_contrato.val()), function(key, value) {
             if(actual == value.id){
               if (value.laboral){
-                  colaboradorSelect.show();
-                  terceroSelect.hide();
+                  mostrarOcultarColaboradorTercero("laboral")
               }else{
-                  colaboradorSelect.hide();
-                  terceroSelect.show();
+                  mostrarOcultarColaboradorTercero("no_laboral")
               }
               if(value.fecha_fin){
                   fechaFinal.show();
+                  fechaFinalID.attr("required", true);
               }else{
                   fechaFinal.hide();
+                  fechaFinalID.removeAttr("required", true);
               }
             }
         });
         if (actual === ""){
-             colaboradorSelect.show();
-             terceroSelect.hide();
              fechaFinal.show();
+             fechaFinalID.attr("required", true);
+             mostrarOcultarColaboradorTercero("ninguno")
         }
     });
 
@@ -51,4 +54,18 @@ fechaFinalID.change(function () {
         EVANotificacion.toast.advertencia('La fecha final no puede ser menor a la fecha inicial');
     }
 });
+
+function mostrarOcultarColaboradorTercero(tipo) {
+    if (tipo === 'no_laboral'){
+        terceroSelect.show();
+        terceroSelectID.attr("required", true);
+        colaboradorSelect.hide();
+        colaboradorSelectID.removeAttr('required', true);
+    }else{
+        colaboradorSelect.show();
+        colaboradorSelectID.attr("required", true);
+        terceroSelect.hide();
+        terceroSelectID.removeAttr('required', true);
+    }
+}
 
