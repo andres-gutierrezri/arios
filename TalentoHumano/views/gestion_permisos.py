3f93ef00f2255f1615a6e5cd413a23c3513d9414
Alejandro Cepeda <sistemas@arios-ing.com>
2020-06-17 12:05:24 -0500

    EVA-91 Feat: Se optimiza la funcion que agrega los permisos al usuario

--- TalentoHumano/views/gestion_permisos.py
+++ TalentoHumano/views/gestion_permisos.py
@@ -8,11 +8,15 @@ from django.urls import reverse
 from EVA.views.index import AbstractEvaLoggedView
 from TalentoHumano.models import PermisosFuncionalidad, Colaborador
 
+VER = 1
+CREAR = 2
+EDITAR = 3
+ELIMINAR = 4
+
 
 class AsignacionPermisosView(AbstractEvaLoggedView):
     def get(self, request, id):
         colaborador = Colaborador.objects.get(id=id)
-        user_permisos = obtener_permisos(User.objects.get(id=request.user.id))
         permisos = PermisosFuncionalidad.objects.all()
         permisos_json = []
         for prm in permisos:
@@ -23,34 +27,29 @@ class AsignacionPermisosView(AbstractEvaLoggedView):
                       {'permisos': permisos,
                        'id_permisos': json.dumps(permisos_json),
                        'colaborador': colaborador,
-                       'acciones_permisos': acciones_permisos,
-                       'user_permisos': user_permisos})
+                       'acciones_permisos': acciones_permisos})
 
     def post(self, request, id):
         datos_permisos = json.loads(request.POST.get('valores_permisos', ''))
+        usuario = User.objects.get(id=id)
+        if datos_permisos:
+            funcionalidades = PermisosFuncionalidad.objects.filter(estado=True)
+            limpiar_permisos(usuario)
 
-        funcionalidades = PermisosFuncionalidad.objects.filter(estado=True)
-
-        for func in funcionalidades:
-            for datos in datos_permisos:
-                if func.content_type_id == datos['funcionalidad']:
-                    for perm in datos['permiso']:
-                        if perm == 1:
-                            permiso = Permission.objects.get(content_type_id=func.content_type_id,
-                                                             codename='view_' + func.content_type.model)
-                            User.objects.get(id=id).user_permissions.add(permiso)
-                        elif perm == 2:
-                            permiso = Permission.objects.get(content_type_id=func.content_type_id,
-                                                             codename='add_' + func.content_type.model)
-                            User.objects.get(id=id).user_permissions.add(permiso)
-                        elif perm == 3:
-                            permiso = Permission.objects.get(content_type_id=func.content_type_id,
-                                                             codename='change_' + func.content_type.model)
-                            User.objects.get(id=id).user_permissions.add(permiso)
-                        elif perm == 4:
-                            permiso = Permission.objects.get(content_type_id=func.content_type_id,
-                                                             codename='delete_' + func.content_type.model)
-                            User.objects.get(id=id).user_permissions.add(permiso)
+            for func in funcionalidades:
+                for datos in datos_permisos:
+                    if func.content_type_id == datos['funcionalidad']:
+                        for perm in datos['permiso']:
+                            if perm == VER:
+                                usuario.user_permissions.add(consultar_permiso(func, 'view'))
+                            elif perm == CREAR:
+                                usuario.user_permissions.add(consultar_permiso(func, 'add'))
+                            elif perm == EDITAR:
+                                usuario.user_permissions.add(consultar_permiso(func, 'change'))
+                            elif perm == ELIMINAR:
+                                usuario.user_permissions.add(consultar_permiso(func, 'delete'))
+        else:
+            limpiar_permisos(usuario)
 
         messages.success(request, 'Se han guardado las selecciones correctamente')
         return redirect(reverse('TalentoHumano:colaboradores-index', args=[0]))
@@ -63,3 +62,13 @@ def obtener_permisos(user, obj=None):
         if hasattr(backend, name):
             permissions.update(getattr(backend, name)(user, obj))
     return permissions
+
+
+def limpiar_permisos(usuario):
+    for perm in usuario.user_permissions.all():
+        usuario.user_permissions.remove(perm)
+
+
+def consultar_permiso(funcionalidad, accion):
+    return Permission.objects.get(content_type_id=funcionalidad.content_type_id,
+                                  codename=accion + '_' + funcionalidad.content_type.model)
