3b2802d245b0846e44032c54c32b333e45392806
Alejandro Cepeda <sistemas@arios-ing.com>
2020-06-17 10:01:44 -0500

    EVA-91 Feat: Se configura la gestión del modelo de permisos de funcionalidad en el admin de TalentoHumano

--- TalentoHumano/admin.py
+++ TalentoHumano/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from TalentoHumano.models import PermisosFuncionalidad
+
+
+class PermisosFuncionalidadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion', 'content_type', 'estado')
+    list_display_links = ('id', 'nombre')
+    search_fields = ('id', 'nombre', 'aplicacion')
+
+
+admin.site.register(PermisosFuncionalidad, PermisosFuncionalidadAdmin)
