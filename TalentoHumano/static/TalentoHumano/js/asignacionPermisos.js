13910d3ebd42e20b39a7f764a75209421db4b93a
Alejandro Cepeda <sistemas@arios-ing.com>
2020-06-17 09:57:06 -0500

    EVA-91 Feat: Se agrega la configuración al js de asignación de permisos, para enviar los datos al servidor.

--- TalentoHumano/static/TalentoHumano/js/asignacionPermisos.js
+++ TalentoHumano/static/TalentoHumano/js/asignacionPermisos.js
@@ -1,3 +1,46 @@
 function guardarAsignacionesPermisos() {
-    $( "#asignacion-permisos-form" ).submit();
+    let valoresPermisos = $('#valores_permisos');
+    let listaPermisos = [];
+
+    let idPermisos = JSON.parse($('#id_permisos').val());
+    idPermisos.forEach( function(valor) {
+        let idFuncionalidad = 'permiso_'+ valor.id + '_id';
+
+        if ($('#' + idFuncionalidad).prop('checked')){
+            let permisos = [];
+            let pos = 1;
+
+            while (pos <= 4) {
+                if ($('#accion_' + pos + '_' + idFuncionalidad).prop('checked')) {
+                    permisos.push(pos)
+                }
+                pos += 1;
+            }
+            listaPermisos.push({'funcionalidad': valor.id, 'permiso': permisos});
+        }
+    });
+    valoresPermisos.val(JSON.stringify(listaPermisos));
+    $("#asignacion-permisos-form").submit();
+}
+
+function activarFuncionalidad(id) {
+    let funcionalidad = $('#' + id);
+
+    if (funcionalidad.prop('checked')){
+        let x = 1;
+        while (x <= 4) {
+            let item = $('#accion_' + x + '_' + id);
+            item.prop("checked", true);
+            item.prop("disabled", false);
+            x += 1;
+        }
+    }else {
+        let x = 1;
+        while (x <= 4) {
+            let item = $('#accion_' + x + '_' + id);
+            item.prop("checked", false);
+            item.prop("disabled", true);
+            x += 1;
+        }
+    }
 }
