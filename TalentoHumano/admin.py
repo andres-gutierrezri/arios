3e18d276e1d701a4a1882558f4d83fa2569df73a
Alejandro Cepeda <sistemas@arios-ing.com>
2020-06-20 13:49:53 -0500

    EVA-91 Feat: Se agrega el solo admin al index de permisos de funcionalidades en el admin

--- TalentoHumano/admin.py
+++ TalentoHumano/admin.py
@@ -4,7 +4,7 @@ from TalentoHumano.models import PermisosFuncionalidad
 
 
 class PermisosFuncionalidadAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'descripcion', 'content_type', 'estado')
+    list_display = ('id', 'nombre', 'descripcion', 'content_type', 'grupo', 'solo_admin', 'estado')
     list_display_links = ('id', 'nombre')
     search_fields = ('id', 'nombre', 'aplicacion')
 
