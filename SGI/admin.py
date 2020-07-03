381eca80db6977aa4336b7e95588db5faad44c58
Alejandro Cepeda <sistemas@arios-ing.com>
2020-07-03 10:06:06 -0500

    EVA-96 Feat: Se agrega la gestión del modelo de Grupos de documento al admin de Django.

--- SGI/admin.py
+++ SGI/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from SGI.models import GrupoDocumento
+
+
+class GrupoDocumentoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion', 'estado', 'empresa', 'es_general')
+    list_display_links = ('id', 'nombre')
+    search_fields = ('id', 'nombre')
+
+
+admin.site.register(GrupoDocumento, GrupoDocumentoAdmin)
