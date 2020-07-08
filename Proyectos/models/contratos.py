9f1145a5efd619499bfbc7db3d46331bec2281a8
Alejandro Cepeda <sistemas@arios-ing.com>
2020-07-08 17:24:05 -0500

    EVA-99 Feat: Se edita el modelo de contrato 1. Se edita el campo de residente, para cambiarlo de Char a Foraneo a la tabla de usuarios. 2. Se configuran los campos de fecha de inicio y terminacion como nulos y blancos. 3. Se configura el campo de periodicidad de informes como nulo y blanco 4. Se agrgan los campos de fecha de suscripción y proceso a cargo 5. Se edita el diccionario para adaptarlo a los cambios.

--- Proyectos/models/contratos.py
+++ Proyectos/models/contratos.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
-from Administracion.models import Tercero, TipoContrato, Empresa
+from Administracion.models import Tercero, TipoContrato, Empresa, Proceso
 
 # Create your models here.
 from EVA.General.conversiones import string_to_date
@@ -15,17 +16,20 @@ class Contrato(models.Model, ModelDjangoExtensiones):
     supervisor_nombre = models.CharField(max_length=100, verbose_name='Nombre del supervisor', null=False, blank=False)
     supervisor_telefono = models.TextField(max_length=15, verbose_name='Teléfono', null=False, blank=False)
     supervisor_correo = models.EmailField(max_length=100, verbose_name='Correo del supervisor', null=False, blank=False,
-                                          error_messages={'invalid':"Ingrese una dirección de correo válida"})
-    residente = models.CharField(max_length=100, verbose_name='Residente', null=False, blank=False)
-    fecha_inicio = models.DateTimeField(verbose_name='Fecha de inicio', null=False, blank=False)
-    fecha_terminacion = models.DateTimeField(verbose_name='Fecha de terminación', null=True, blank=False)
+                                          error_messages={'invalid': "Ingrese una dirección de correo válida"})
+    residente = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Residente', null=False,
+                                  blank=False)
+    fecha_inicio = models.DateTimeField(verbose_name='Fecha de inicio', null=True, blank=True)
+    fecha_terminacion = models.DateTimeField(verbose_name='Fecha de terminación', null=True, blank=True)
+    fecha_suscripcion = models.DateTimeField(verbose_name='Fecha de suscripción', null=True, blank=False)
     valor = models.BigIntegerField(verbose_name="Valor", null=True, blank=False)
-    periodicidad_informes = models.IntegerField(verbose_name='Periodicidad de informes', null=False,
-                                              blank=False)
+    periodicidad_informes = models.IntegerField(verbose_name='Periodicidad de informes', null=True, blank=True)
     tiempo = models.IntegerField(verbose_name='Tiempo', null=False, blank=False)
     tipo_contrato = models.ForeignKey(TipoContrato, on_delete=models.DO_NOTHING, verbose_name='Tipo de contrato',
                                       null=True, blank=False)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, verbose_name='Empresa', null=True, blank=False)
+    proceso_a_cargo = models.ForeignKey(Proceso, on_delete=models.DO_NOTHING, verbose_name='Proceso a cargo',
+                                        null=False, blank=False)
 
     def __str__(self):
         return self.numero_contrato
@@ -48,13 +52,15 @@ class Contrato(models.Model, ModelDjangoExtensiones):
         contrato.supervisor_nombre = datos.get('supervisor_nombre', '')
         contrato.supervisor_correo = datos.get('supervisor_correo', '')
         contrato.supervisor_telefono = datos.get('supervisor_telefono', '')
-        contrato.residente = datos.get('residente', '')
+        contrato.residente_id = datos.get('residente_id', '')
         contrato.fecha_inicio = string_to_date(datos.get('fecha_inicio', ''))
         contrato.fecha_terminacion = string_to_date(datos.get('fecha_terminacion', ''))
+        contrato.fecha_suscripcion = string_to_date(datos.get('fecha_suscripcion', ''))
         contrato.valor = datos.get('valor', '')
-        contrato.periodicidad_informes = datos.get('periodicidad_informes', '')
+        contrato.periodicidad_informes = datos.get('periodicidad_informes', None)
         contrato.tiempo = datos.get('tiempo', '')
         contrato.tipo_contrato_id = datos.get('tipo_contrato_id', '')
         contrato.empresa_id = datos.get('empresa_id', '')
+        contrato.proceso_a_cargo_id = datos.get('proceso_id', '')
 
         return contrato
