3ed3e12e5a80a973c5ef57f9facb8cfb75fbe6e3
Alejandro Cepeda <sistemas@arios-ing.com>
2020-07-16 11:53:28 -0500

    EVA-101 Feat: Se agrega la gestión del modelo de GruposDocumentosProcesos al admin de django

--- SGI/admin.py
+++ SGI/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from SGI.models.documentos import GruposDocumentosProcesos
+
+
+class GruposDocumentosProcesosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'grupo_documento', 'proceso')
+    list_display_links = ('id', 'grupo_documento', 'proceso')
+    search_fields = ('id', 'grupo_documento', 'proceso')
+
+
+admin.site.register(GruposDocumentosProcesos, GruposDocumentosProcesosAdmin)
