cf0ac281bdfda5adef9f51e3584c8b2db38ffc7e
Alejandro Cepeda <sistemas@arios-ing.com>
2020-07-23 07:29:08 -0500

    EVA-102 Feat: Se implementan los condicionales para gestionar SGI desde los resultados de busqueda de documento Tambien se agrega la modificacion para eliminar archivos desde los resultados de busqueda.

--- SGI/views/sgi.py
+++ SGI/views/sgi.py
@@ -3,7 +3,8 @@ from Administracion.models import Proceso
 from Administracion.utils import get_id_empresa_global
 from EVA.views.index import AbstractEvaLoggedView
 from SGI.models import Documento, GrupoDocumento, Archivo
-from SGI.models.documentos import EstadoArchivo, ResultadosAprobacion
+from SGI.models.documentos import EstadoArchivo, ResultadosAprobacion, GruposDocumentosProcesos
+from TalentoHumano.models import Colaborador
 
 
 class Index(AbstractEvaLoggedView):
@@ -14,11 +15,14 @@ class Index(AbstractEvaLoggedView):
 
 class BuscarDocumentos(AbstractEvaLoggedView):
     def post(self, request):
+        empresa_id = get_id_empresa_global(request)
+        colaborador = Colaborador.objects.get(usuario=request.user)
         texto_busqueda = request.POST.get('texto_busqueda', '')
         documentos = Documento.objects.filter(nombre__icontains=texto_busqueda)
         procesos = Proceso.objects.filter(empresa_id=get_id_empresa_global(request)).order_by('nombre')
-        grupos_documentos = GrupoDocumento.objects.filter(es_general=False)
-        grupos_documentos_generales = GrupoDocumento.objects.filter(es_general=True)
+        grupos_documentos = GrupoDocumento.objects.filter(empresa_id=empresa_id, es_general=False).order_by('nombre')
+        grupos_documentos_generales = GrupoDocumento.objects.filter(empresa_id=empresa_id, es_general=True)\
+            .order_by('nombre')
         lista_datos = []
         for pro in procesos:
             lista_grupos = armar_documentos(documentos, grupos_documentos, pro)
@@ -29,26 +33,13 @@ class BuscarDocumentos(AbstractEvaLoggedView):
         if lista_grupos_generales:
             lista_datos.append({'proceso': 'General', 'grupos': lista_grupos_generales})
 
-        # empresa_id = get_id_empresa_global(request)
-
         archivos = Archivo.objects.filter(documento__nombre__icontains=texto_busqueda, estado_id=EstadoArchivo.APROBADO)
         historial = Archivo.objects.filter(documento__nombre__icontains=texto_busqueda).order_by('-version') \
-            .exclude(estado_id=EstadoArchivo.PENDIENTE)
+            .exclude(estado_id=EstadoArchivo.PENDIENTE).exclude(estado_id=EstadoArchivo.ELIMINADO)
         resultados = ResultadosAprobacion.objects.exclude(estado_id=EstadoArchivo.PENDIENTE)
-
-        # Union de documentos de grupos de documento generales.
-
-        # documentos |= Documento.objects.filter(grupo_documento__es_general=True,
-        #                                        grupo_documento__empresa_id=empresa_id)
-        # archivos |= Archivo.objects.filter(documento__grupo_documento__es_general=True,
-        #                                    documento__grupo_documento__empresa_id=empresa_id,
-        #                                    estado_id=EstadoArchivo.APROBADO)
-        # historial |= Archivo.objects.filter(documento__grupo_documento__es_general=True,
-        #                                     documento__grupo_documento__empresa_id=empresa_id).order_by('-version') \
-        #     .exclude(estado_id=EstadoArchivo.PENDIENTE)
-
         return render(request, 'SGI/documentos/resultado-busqueda-documentos.html',
                       {'procesos': procesos,
+                       'colaborador': colaborador,
                        'procesos_grupos_documentos': lista_datos,
                        'texto_busqueda': texto_busqueda,
                        'archivos': archivos,
@@ -62,6 +53,11 @@ class BuscarDocumentos(AbstractEvaLoggedView):
 def armar_documentos(documentos, grupos_documentos, proceso: None):
     lista_grupos = []
     for gru in grupos_documentos:
+        lista_procesos = []
+        for gdp in GruposDocumentosProcesos.objects.all():
+            if gru == gdp.grupo_documento:
+                lista_procesos.append(gdp.proceso)
+
         lista_documentos = []
         for doc in documentos:
             if proceso == doc.proceso and gru == doc.grupo_documento:
@@ -69,5 +65,9 @@ def armar_documentos(documentos, grupos_documentos, proceso: None):
             elif gru == doc.grupo_documento and not proceso:
                 lista_documentos.append({'documento': doc})
         if lista_documentos:
-            lista_grupos.append({'grupo': gru, 'documentos': lista_documentos})
+            if lista_procesos:
+                lista_grupos.append({'grupo': gru, 'documentos': lista_documentos, 'solo_proceso': True,
+                                     'proceso': lista_procesos})
+            else:
+                lista_grupos.append({'grupo': gru, 'documentos': lista_documentos, 'solo_proceso': True})
     return lista_grupos
