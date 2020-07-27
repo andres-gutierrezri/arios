c8cc128dc3630bc2d698617520bb105f1a3a2778
Alejandro Cepeda <sistemas@arios-ing.com>
2020-07-27 13:43:30 -0500

    EVA-105 Feat: Se Agregan los permisos para subtipos de movimientos Adicional, se organizan las urls creadas para subtipos de movimientos.

--- Financiero/permisos.py
+++ Financiero/permisos.py
@@ -8,4 +8,8 @@ permisos = [Permiso('index', None),
             Permiso('factura-editar', ['Financiero.change_facturaencabezado']),
             Permiso('factura-detalle', ['Financiero.view_facturaencabezado']),
             Permiso('factura-imprimir', ['Financiero.view_facturaencabezado']),
+            Permiso('subtipo-movimiento-index', ['Financiero.view_subtipomovimiento']),
+            Permiso('subtipo-movimiento-crear', ['Financiero.add_subtipomovimiento']),
+            Permiso('subtipo-movimiento-editar', ['Financiero.change_subtipomovimiento']),
+            Permiso('subtipo-movimiento-eliminar', ['Financiero.delete_subtipomovimiento']),
             ]
