16a8b7d7024c0a2b9f7a8032ec6ba807b2409587
Alejandro Cepeda <sistemas@arios-ing.com>
2020-08-06 07:52:43 -0500

    EVA-104 Feat: Se editan las vistas de flujo_caja_contratos, para usar las funciones y vistas generales.

---
+++ Financiero/views/flujo_caja_contratos.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from django.shortcuts import render
+
+from EVA.views.index import AbstractEvaLoggedView
+from Financiero.views.flujo_caja_general import flujo_caja_detalle, cargar_modal_crear_editar, guardar_movimiento
+from Proyectos.models import Contrato
+
+
+class FlujoCajaContratosView(AbstractEvaLoggedView):
+    def get(self, request):
+        contratos = Contrato.objects.all()
+        if not request.user.has_perms(['TalentoHumano.can_access_usuarioespecial']):
+            contratos = contratos.filter(colaboradorcontrato__colaborador__usuario=request.user)
+
+        return render(request, 'Financiero/FlujoCaja/FlujoCajaContratos/index.html',
+                      {'contratos': contratos, 'fecha': datetime.now(),
+                       'menu_actual': 'fc_contratos'})
+
+
+class FlujoCajaContratosDetalleView(AbstractEvaLoggedView):
+    def get(self, request, id, tipo):
+        return flujo_caja_detalle(request, tipo, contrato=id)
+
+
+class FlujoCajaContratosCrearView(AbstractEvaLoggedView):
+    def get(self, request, id_contrato, tipo):
+        OPCION = 'crear'
+        return cargar_modal_crear_editar(request, OPCION, tipo=tipo, contrato=id_contrato)
+
+    def post(self, request, id_contrato, tipo):
+        return guardar_movimiento(request, tipo=tipo, contrato=id_contrato)
+
