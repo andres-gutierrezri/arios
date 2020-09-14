6ef4c7f08fd111c91e717098889556e50894055b
Alejandro Cepeda <sistemas@arios-ing.com>
2020-09-14 12:47:24 -0500

    EVA-115 Feat: Se cambia el nombre del campo subtipo en el admin de administración para parametros.

--- Administracion/admin.py
+++ Administracion/admin.py
@@ -29,8 +29,8 @@ class PermisosFuncionalidadAdmin(admin.ModelAdmin):
 
 
 class ParametrosAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'tipo', 'descripcion', 'valor', 'grupo', 'sub_grupo', 'estado')
-    list_display_links = ('id', 'nombre', 'tipo', 'descripcion', 'valor', 'grupo', 'sub_grupo', 'estado')
+    list_display = ('id', 'nombre', 'tipo', 'descripcion', 'valor', 'grupo', 'subgrupo', 'estado')
+    list_display_links = ('id', 'nombre', 'tipo', 'descripcion', 'valor', 'grupo', 'subgrupo', 'estado')
     search_fields = ('id', 'nombre', 'descripcion',)
 
 
