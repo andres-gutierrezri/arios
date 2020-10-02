fdcdaa2e4d3a0d43142c63ca60c9d18cfd27f82c
Alejandro Cepeda <sistemas@arios-ing.com>
2020-10-02 14:12:41 -0500

    EVA-121 Feat: Se crea el js que controla las vigencias

---
+++ Proyectos/static/Proyectos/js/Contratos/FormularioContrato/controlVigencia.js
@@ -0,0 +1,81 @@
+
+'use strict';
+
+let divVigencias = $('#div_vigencias');
+let anho = $('#anho_vigencia_id');
+let vigencia = $('#valor_vigencia_id');
+let eliminarVigencia = $('#eliminar_vigencia');
+let datosVigencias = $('#datos_vigencias');
+let valoresVigencias = [];
+let contadorVigencia = 0;
+
+function agregarVigencia(valores) {
+    let datoAnho = anho.val();
+    let datoVigencia = vigencia.val();
+    if (valores){
+        datoAnho = valores.valor_anho;
+        datoVigencia = valores.valor_vigencia;
+    }
+    if (datoVigencia === '' || datoAnho === ''){
+        EVANotificacion.toast.error('Debes llenar los campos disponibles antes de realizar esta acción.');
+        return false;
+    }
+    eliminarVigencia.show();
+    valoresVigencias.push({'pos': contadorVigencia, 'valor_anho': datoAnho, 'valor_vigencia': datoVigencia});
+    divVigencias.append('<div class="form-group" id="vigencia_'+ contadorVigencia +'" style="margin-bottom: 0">' +
+        '<div class="form-row"><div class="col-md-6">\n' +
+        '<label>Año de vigencia</label>\n' +
+        '<input disabled type="text" id="valor_anho_'+ contadorVigencia +'" value="'+ datoAnho +'" class="form-control"></div>\n' +
+        '<div class="col-md-5">\n' +
+        '<label>Valor de vigencia</label>\n' +
+        '<input disabled type="text" id="valor_vigencia_'+ contadorVigencia +'" value="'+ datoVigencia +'" class="form-control"></div>\n' +
+        '<div class="col-md-1" style="padding-top:30px">\n' +
+        '<a class="far fa-2x far fa-edit" href="#" onclick="editarPosVigencia('+ contadorVigencia +')" title="" data-original-title="Agregar Vigencia" style=""></a>\n' +
+        '<a class="far fa-2x far fa-trash-alt color-danger-900" href="#" onclick="quitarPosVigencia('+ contadorVigencia +')" title="" data-original-title="Eliminar Vigencia"></a>\n' +
+        '</div>' +
+        '</div><br></div>');
+    contadorVigencia += 1;
+    anho.val('');
+    vigencia.val('');
+    datosVigencias.val(JSON.stringify(valoresVigencias));
+}
+
+function editarPosVigencia(contador) {
+    if (anho.val() !== '' && vigencia.val() !== ''){
+        $('#agregar_vigencia').click();
+    }
+    retomarCampoVigencia(contador);
+    valoresVigencias.forEach(function (elemento, pos) {
+       if (elemento.pos === contador){
+            valoresVigencias.splice(pos, 1)
+       }
+    });
+    datosVigencias.val(JSON.stringify(valoresVigencias));
+    $('#vigencia_'+ contador).remove();
+}
+
+function quitarPosVigencia(contador) {
+    valoresVigencias.forEach(function (elemento, pos) {
+       if (elemento.pos === contador){
+            valoresVigencias.splice(pos, 1)
+       }
+    });
+    $('#vigencia_'+ contador).remove();
+    datosVigencias.val(JSON.stringify(valoresVigencias));
+}
+
+function quitarVigencia() {
+    contadorVigencia -= 1;
+    if (contadorVigencia === 0){
+        eliminarVigencia.hide();
+    }
+    valoresVigencias.pop();
+    retomarCampoVigencia(contadorVigencia);
+    $('#vigencia_'+ contadorVigencia).remove();
+    datosVigencias.val(JSON.stringify(valoresVigencias));
+}
+
+function retomarCampoVigencia(contadorVigencia){
+    anho.val($('#valor_anho_' + contadorVigencia).val());
+    vigencia.val($('#valor_vigencia_' + contadorVigencia).val());
+}
