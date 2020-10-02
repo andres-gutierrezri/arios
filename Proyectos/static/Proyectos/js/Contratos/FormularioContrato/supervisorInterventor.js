815f3308f8e4dcf007daf096d3628359846c0d74
Alejandro Cepeda <sistemas@arios-ing.com>
2020-10-02 03:16:06 -0500

    EVA-121 Feat: se crea el js supervisor Interventor para controlar todo lo relacionado con ello

---
+++ Proyectos/static/Proyectos/js/Contratos/FormularioContrato/supervisorInterventor.js
@@ -0,0 +1,26 @@
+
+'use strict';
+
+let selectSupervisorInterventor = $('#supervisor_interventor_id_select_id');
+let divSupervisores = $('#div_supervisores');
+let divInterventores = $('#div_interventores');
+let inputSupervisores = $('#supervisor_id');
+let inputInteventores = $('#interventor_id');
+
+selectSupervisorInterventor.change(function () {
+    ValidarSeleccionSupervisorInterventor();
+});
+
+function ValidarSeleccionSupervisorInterventor() {
+    if (selectSupervisorInterventor.val() === '0'){
+        divSupervisores.show();
+        divInterventores.hide();
+        inputSupervisores.attr('required', true);
+        inputInteventores.removeAttr('required', true);
+    }else{
+        divSupervisores.hide();
+        divInterventores.show();
+        inputSupervisores.removeAttr('required', true);
+        inputInteventores.attr('required', true);
+    }
+}
