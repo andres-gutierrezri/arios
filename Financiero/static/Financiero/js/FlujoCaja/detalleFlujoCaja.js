e7622c96e0f86207052d0eba94f05f308be0c06b
Alejandro Cepeda <sistemas@arios-ing.com>
2020-10-05 07:22:31 -0500

    EVA-122 Feat: Se crea el js que controla los cambios de los filtros por mes y año en el detalle de flujo de caja

---
+++ Financiero/static/Financiero/js/FlujoCaja/detalleFlujoCaja.js
@@ -0,0 +1,29 @@
+
+'use strict';
+
+let selectFiltroAnio = $('#filtro_anio_id_select_id');
+let selectFiltroMes = $('#filtro_mes_id_select_id');
+let idContratoDetalle = $('#id_contrato_detalle');
+let idTipoDetalle = $('#id_tipo_detalle');
+
+ $(document).ready(function () {
+    $('.select2').select2({
+        "language": {
+            noResults: function () {
+              return 'No se encontraron coincidencias';
+            },
+            searching: function () {
+              return 'Buscando…';
+            },
+            removeAllItems: function () {
+              return 'Quitar todas los items';
+            }
+        },
+    });
+    selectFiltroAnio.change(function () {
+        window.location = `/financiero/flujo-caja/procesos/${idContratoDetalle.val()}/detalle/${idTipoDetalle.val()}/anio/${selectFiltroAnio.val()}/mes/${selectFiltroMes.val()}`;
+    });
+    selectFiltroMes.change(function () {
+        window.location = `/financiero/flujo-caja/procesos/${idContratoDetalle.val()}/detalle/${idTipoDetalle.val()}/anio/${selectFiltroAnio.val()}/mes/${selectFiltroMes.val()}`;
+    });
+});
