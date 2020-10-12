164766298820195d78c892fbfcedfef860419c47
Alfonso Ardila <asesorsistemas@arios-ing.com>
2020-10-12 07:31:33 -0500

    EVA-125 feat: Se agrega mejora para que en la validación de formularios se validen los limites min y max de un input  marcado con la calse inputmask.
    Adicional se extienden los alias de Inputmask para dejarlos personalizados a como los vamos a utilizazr y en caso de requerir crear un input desde javascript solo sea necesario especificar el alias y no todas las propiedades personalizadas.

--- EVA/static/EVA/Plantilla/js/custom/validaciones/validar_formulario.js
+++ EVA/static/EVA/Plantilla/js/custom/validaciones/validar_formulario.js
@@ -11,14 +11,32 @@ function agregarValidacionFormularios() {
     let forms = document.getElementsByClassName("needs-validation");
     // Loop over them and prevent submission
     let validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
+        agregarValidacionForm(form);
     });
+}
+
+/**
+ * Agrega los estilos personalizados de validación, en esta validación se incluye la validación de los
+ * valores min y max de los inputmask marcados con la clase 'inputmask'.
+ * @param form Formulario al que se le quiere agregar los estilos personalizados en la validación
+ * @param fnCallback Función que sera ejecutada cuando la validación se completa exitosamente, si esta función retorna
+ * true indica  que se debe evitar el submit del formulario.
+ */
+function agregarValidacionForm(form, fnCallback){
+    form.addEventListener('submit', function(event) {
+        validarMinMaxInputMask(form);
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            form.classList.add('was-validated');
+        } else {
+            if(fnCallback !== undefined)
+                if(fnCallback(event)) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+        }
+    }, false);
 }
 
 function validarLongitud(longitud, elemento){
@@ -31,4 +49,28 @@ function validarLongitud(longitud, elemento){
         str = str.substring(0, str.length - 1);
         elemento.value = str;
     }
+}
+
+function validarMinMaxInputMask(form) {
+    let valido = true;
+    const inputs = form.getElementsByClassName('inputmask');
+    for(let i = 0; i < inputs.length; i++) {
+        const valor = Number(inputs[i].inputmask.unmaskedvalue());
+        if(valor < inputs[i].min || valor > inputs[i].max) {
+            if(valor < inputs[i].min)
+                inputs[i].nextElementSibling.innerText = `Debe ser mayor o igual a ${inputs[i].min}`;
+            else
+                inputs[i].nextElementSibling.innerText = `Debe ser menor o igual a ${inputs[i].max}`;
+            inputs[i].setCustomValidity('.');
+            valido = false;
+        }
+        else
+            inputs[i].setCustomValidity('');
+    }
+    return valido;
+}
+
+function limpiarFormulario(form) {
+    form.reset();
+    form.classList.remove('was-validated');
 }
