38efc11c5292d06b21ed94d62d47669f1d7f1518
Alejandro Cepeda <sistemas@arios-ing.com>
2020-10-16 10:54:29 -0500

    EVA-128 Feat: Se crea el archivo para el uso del redondeo de decimales.

---
+++ EVA/static/EVA/Plantilla/js/mathround/mathround.js
@@ -0,0 +1,31 @@
+
+'use strict';
+
+// Aplicación de la funcion Math.round() para el redondeo de decimales.
+// Recurso: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Math/round#Redondeo_decimal
+
+function decimalAdjust(type, value, exp) {
+    // Si el exp no está definido o es cero...
+    if (typeof exp === 'undefined' || +exp === 0) {
+        return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // Si el valor no es un número o el exp no es un entero...
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+        return NaN;
+    }
+    // Shift
+    value = value.toString().split('e');
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+}
+
+// Decimal round
+if (!Math.round10) {
+    Math.round10 = function (value, exp) {
+        return decimalAdjust('round', value, exp);
+    };
+}
