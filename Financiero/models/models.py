a9b4a8b73f3e19d905314283fb0929f57fd706e6
Alejandro Cepeda <sistemas@arios-ing.com>
2020-10-28 16:40:51 -0500

    EVA-131 Feat: Se crean los modelos en el modulo financiero

--- Financiero/models/models.py
+++ Financiero/models/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+from EVA.General.modelmanagers import ManagerGeneral
+
+
+class EntidadBancaria(models.Model):
+    objects = ManagerGeneral()
+    nombre = models.CharField(verbose_name='Nombre', max_length=100, null=False, blank=False)
+    codigo_banco = models.CharField(verbose_name='Código del Banco', max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Entidad Bancaria'
+        verbose_name_plural = 'Entidades Bancarias'
+
+
+class ActividadEconomica(models.Model):
+    objects = ManagerGeneral()
+    nombre = models.CharField(verbose_name='Nombre', max_length=100, null=False, blank=False)
+    codigo_ciiu = models.CharField(verbose_name='Codigo CIIU', max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Actividad Económica'
+        verbose_name_plural = 'Actividades Económicas'
+
+
+class TipoPersona(models.Model):
+    objects = ManagerGeneral()
+    nombre = models.CharField(verbose_name='Nombre', max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Tipo de Persona'
+        verbose_name_plural = 'Tipos de Personas'
+
+
+class TipoContribuyente(models.Model):
+    objects = ManagerGeneral()
+    nombre = models.CharField(verbose_name='Nombre', max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Tipo de Contribuyente'
+        verbose_name_plural = 'Tipos de Contribuyentes'
+
+
+class Regimen(models.Model):
+    objects = ManagerGeneral()
+    nombre = models.CharField(verbose_name='Nombre', max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Régimen'
+        verbose_name_plural = 'Regímenes'
