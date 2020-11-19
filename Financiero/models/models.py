ab590e52586e3e78617aa1bf9122fb066d2d0c11
Sistemas <sistemas@arios-ing.com>
2020-11-19 13:14:31 -0500

    EVA-130 Feat: Se crea el modelo ProveedorActividadEconomica

--- Financiero/models/models.py
+++ Financiero/models/models.py
@@ -63,3 +63,52 @@ class Regimen(models.Model):
     class Meta:
         verbose_name = 'Régimen'
         verbose_name_plural = 'Regímenes'
+
+
+class ProveedorActividadEconomica(models.Model):
+    actividad_principal = models.ForeignKey(ActividadEconomica, on_delete=models.DO_NOTHING, null=False, blank=False,
+                                            verbose_name='Actividad Principal',
+                                            related_name='proveedor_actividad_principal')
+    actividad_secundaria = models.ForeignKey(ActividadEconomica, on_delete=models.DO_NOTHING, null=True, blank=True,
+                                             verbose_name='Actividad Secundiaria',
+                                             related_name='proveedor_actividad_secundaria')
+    otra_actividad = models.ForeignKey(ActividadEconomica, on_delete=models.DO_NOTHING, null=True, blank=True,
+                                       verbose_name='Otra Actividad', related_name='proveedor_otra_atividad')
+    regimen = models.ForeignKey(Regimen, on_delete=models.DO_NOTHING, null=False, blank=False,
+                                verbose_name='Régimen')
+    tipo_contribuyente = models.ForeignKey(TipoContribuyente, on_delete=models.DO_NOTHING, null=False, blank=False,
+                                           verbose_name='Tipo Contribuyente')
+    numero_resolucion = models.CharField(verbose_name='Número de Resolución', max_length=100, null=True, blank=True)
+    contribuyente_iyc = models.CharField(verbose_name='Contribuyente de Industria y Comercio', max_length=100,
+                                         null=True, blank=True)
+    entidad_publica = models.CharField(verbose_name='Número de Resolución', max_length=100, null=True, blank=True)
+    proveedor = models.OneToOneField(Tercero, on_delete=models.DO_NOTHING, verbose_name='Usuario',
+                                     null=False, blank=False)
+    bienes_servicios = models.TextField(verbose_name='Bienes y Servicios que Ofrece', null=False, blank=False)
+
+    def __str__(self):
+        return 'Información de la Actividad Economica del usuario {0}'.format(self.proveedor.usuario.get_full_name())
+
+    class Meta:
+        verbose_name = 'Actividad Económica del Proveedor'
+        verbose_name_plural = 'Actividades Económicas de Los Proveedores'
+
+    @staticmethod
+    def from_dictionary(datos: dict) -> 'ProveedorActividadEconomica':
+        """
+        Crea una instancia de Proveedor Actividad Económica con los datos pasados en el diccionario.
+        :param datos: Diccionario con los datos para crear el registro de Proveedor Actividad Económica.
+        :return: Instacia de entidad Proveedor Actividad Económica con la información especificada en el diccionario.
+        """
+        proveedor_ae = ProveedorActividadEconomica()
+        proveedor_ae.actividad_principal_id = datos.get('actividad_principal', '')
+        proveedor_ae.actividad_secundaria_id = datos.get('actividad_secundaria', '')
+        proveedor_ae.otra_actividad_id = datos.get('otra_actividad', '')
+        proveedor_ae.regimen_id = datos.get('regimen', '')
+        proveedor_ae.tipo_contribuyente_id = datos.get('tipo_contribuyente', '')
+        proveedor_ae.numero_resolucion = datos.get('resolucion', '')
+        proveedor_ae.contribuyente_iyc = datos.get('contribuyente_iyc', '')
+        proveedor_ae.entidad_publica = datos.get('entidad_publica', '')
+        proveedor_ae.bienes_servicios = datos.get('bienes_servicios', '')
+
+        return proveedor_ae
