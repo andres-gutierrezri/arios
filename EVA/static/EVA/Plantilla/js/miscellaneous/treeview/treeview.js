769622b63da0959c42b26001fd219c860e6c242c
Alfonso Ardila <asesorsistemas@arios-ing.com>
2021-01-17 18:32:15 -0500

    EVA-151 fix: Se soluciona problema con el icono del treeview utilizado en el Index y resultado busqueda de documentos de SGI.
    Adicional se elimina archivo js no utilizado y se traducen los textos del plugin.

--- EVA/static/EVA/Plantilla/js/miscellaneous/treeview/treeview.js
+++ EVA/static/EVA/Plantilla/js/miscellaneous/treeview/treeview.js
@@ -43,18 +43,18 @@
 	function init() {
 	  // Add any initialization logic here...
 
-var mytreebranch = $('.tree').find('li:has(ul)').addClass('parent_li').attr('role', 'treeitem').find(' > span').attr('title', 'Collapse this branch');
+var mytreebranch = $('.tree').find('li:has(ul)').addClass('parent_li').attr('role', 'treeitem').find(' > span').attr('title', 'Colapsar esta rama');
 
 	$('.tree > ul').attr('role', 'tree').find('ul').attr('role', 'group');
 	mytreebranch.on('click', function (e) {
 		var children = $(this).parent('li.parent_li').find(' > ul > li');
 		if (children.is(':visible')) {
 			children.hide('fast');
-			$(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+			$(this).attr('title', 'Expandir esta rama').find(' > i'); //.addClass('icon-plus-sign').removeClass('icon-minus-sign');
 		}
 		else {
 			children.show('fast');
-			$(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+			$(this).attr('title', 'Colapsar esta rama').find(' > i'); //.addClass('icon-minus-sign').removeClass('icon-plus-sign');
 		}
 		e.stopPropagation();
 	});
