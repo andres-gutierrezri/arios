d61295a3ef63a1a2a427c270bc9d3b57632f5f8a
Alfonso Ardila <asesorsistemas@arios-ing.com>
2021-01-19 05:20:16 -0500

    EVA-155 feat: Se reliza modificación en la apertura de la vista previa pra asegurar que no se esta mostrando el documento anterior.

--- EVA/static/EVA/Plantilla/js/custom/funcionesGenerales.js
+++ EVA/static/EVA/Plantilla/js/custom/funcionesGenerales.js
@@ -1,6 +1,10 @@
 'use strict';
 
 function abrirModalVistaPrevia(urlDocumento) {
-    $("#if_vista_previa").attr('src', urlDocumento);
+    const ifVista =  $("#if_vista_previa");
+
+    ifVista.attr('src', '');
+    ifVista.attr('src', urlDocumento);
+
     $("#vista_previa_modal").modal('show');
 }
