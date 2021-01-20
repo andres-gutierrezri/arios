3ce5bae7e3ede0be35e239d25d4979ba28504a83
sistemas <sistemas@arios-ing.com>
2021-01-20 16:51:30 -0500

    EVA-154 Feat: Se añaden la configuración de permisos para las vistas de eliminar consecutivos Adicional, se corrige el formato de la fecha de creación.

--- GestionDocumental/permisos.py
+++ GestionDocumental/permisos.py
@@ -5,6 +5,8 @@ app_name = 'GestionDocumental'
 permisos = [Permiso('index', None),
             Permiso('consecutivo-oficios-index', ['GestionDocumental.view_consecutivooficio']),
             Permiso('consecutivo-oficios-crear', ['GestionDocumental.add_consecutivooficio']),
+            Permiso('consecutivo-oficios-eliminar', ['GestionDocumental.delete_consecutivooficio']),
             Permiso('consecutivo-contratos-index', ['GestionDocumental.view_consecutivocontrato']),
             Permiso('consecutivo-contratos-crear', ['GestionDocumental.add_consecutivocontrato']),
+            Permiso('consecutivo-contratos-eliminar', ['GestionDocumental.delete_consecutivocontrato']),
             ]
