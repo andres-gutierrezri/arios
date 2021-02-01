38ea797b98f85c7c4362e1c1e1a4d0dc28dedaf2
Alfonso Ardila <asesorsistemas@arios-ing.com>
2021-02-01 07:00:01 -0500

    EVA-159 fix: Se soluciona problema que no permitia acceder a los procesos para poder ver los flujos de caja.
    Se aprovecho y se hizo un refactor: se optimiza obteniendo la información con solo una consulta a la BD.

--- Administracion/views/procesos.py
+++ Administracion/views/procesos.py
@@ -1,33 +1,26 @@
 from datetime import datetime
 
+from django.db.models import OuterRef, Exists, F
 from django.shortcuts import render
 
 from Administracion.models import Proceso
 from Administracion.utils import get_id_empresa_global
 from EVA.views.index import AbstractEvaLoggedView
-from Financiero.models import FlujoCajaEncabezado
 from TalentoHumano.models.colaboradores import ColaboradorProceso
 
 
 class ProcesosView(AbstractEvaLoggedView):
     def get(self, request):
-        procesos = Proceso.objects.filter(empresa_id=get_id_empresa_global(request))
-        procesos_usuario = ColaboradorProceso.objects.filter(colaborador__usuario=request.user)
 
-        procesos_lista = []
-        for pro in procesos:
-            proceso_usuario = False
-            for pro_us in procesos_usuario:
-                if pro == pro_us.proceso:
-                    proceso_usuario = True
-            procesos_lista.append({'id': pro.id,
-                                   'nombre': pro.nombre,
-                                   'objeto': pro.objeto,
-                                   'proceso_usuario': proceso_usuario,
-                                   'flujocajaencabezado_set':
-                                       {'first': {'estado':
-                                                  FlujoCajaEncabezado.objects.filter(proceso=pro).first().estado}}})
+        procesos_usuario = ColaboradorProceso.objects\
+            .filter(colaborador__usuario=request.user, proceso_id=OuterRef('pk'))
+
+        procesos = Proceso.objects\
+            .values('id', 'nombre', 'objeto', estado_fc=F('flujocajaencabezado__estado__nombre'))\
+            .annotate(proceso_usuario=Exists(procesos_usuario))\
+            .filter(empresa_id=get_id_empresa_global(request))
+
         return render(request, 'Financiero/FlujoCaja/FlujoCajaProcesos/index.html',
-                      {'procesos': procesos_lista, 'fecha': datetime.now(),
+                      {'procesos': procesos, 'fecha': datetime.now(),
                        'menu_actual': ['procesos', 'flujos_de_caja'],
                        'menu_extendido': 'Administracion/_common/base_administracion.html'})
