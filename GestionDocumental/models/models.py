d24899dae32c8bddcf8e702e6ce2807c0b297863
Nicolas_Aponte <sistemas3@arios-ing.com>
2021-06-04 03:31:27 -0500

    EVA  - 200 feat: Se añade vista para editar y se agregan los campos necesarios para guardar la fecha de modificación.

--- GestionDocumental/models/models.py
+++ GestionDocumental/models/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 import GestionDocumental
 from Administracion.models import Tercero, TipoContrato, Empresa
+from EVA.General.modeljson import ModelDjangoExtensiones
 from EVA.General.modelmanagers import ManagerGeneral
 from Proyectos.models import Contrato
 from EVA import settings
@@ -50,16 +51,16 @@ def custom_upload_to(instance, filename):
                                                settings.EVA_PRIVATE_MEDIA)
 
 
-class ConsecutivoContrato(models.Model):
-    objects = ManagerGeneral()
+class ConsecutivoContrato(models.Model, ModelDjangoExtensiones):
+    objects = ManagerGeneral('codigo')
     numero_contrato = models.IntegerField(verbose_name='Número de Contrato', null=False, blank=False)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuario', null=True, blank=True,
                                 related_name='consecutivo_contrato_usuario')
     tercero = models.ForeignKey(Tercero, on_delete=models.CASCADE, verbose_name='Tercero', null=True, blank=True)
     tipo_contrato = models.ForeignKey(TipoContrato, on_delete=models.CASCADE, verbose_name='Tipo de Contrato',
                                       null=True, blank=True)
-    fecha_inicio = models.DateTimeField(verbose_name='Fecha de Inicio', null=False, blank=False)
-    fecha_final = models.DateTimeField(verbose_name='Fecha Final', null=True, blank=True)
+    fecha_inicio = models.DateField(verbose_name='Fecha de Inicio', null=False, blank=False)
+    fecha_final = models.DateField(verbose_name='Fecha Final', null=True, blank=True)
     codigo = models.CharField(max_length=50, verbose_name='Código', null=False, blank=False)
     fecha_crea = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación', null=False, blank=False)
     empresa = models.ForeignKey(Empresa, on_delete=models.DO_NOTHING, verbose_name='Empresa', null=False, blank=False)
@@ -67,8 +68,9 @@ class ConsecutivoContrato(models.Model):
                                      null=True, blank=True, related_name='consecutivo_contrato_usuario_crea')
     justificacion = models.CharField(max_length=100, verbose_name='Justificacion', blank=True, null=True)
     estado = models.BooleanField(verbose_name='Estado', blank=False, null=False)
-    ruta_archivo = models.FileField(blank=True, max_length=250, upload_to=custom_upload_to),
-    fecha_modificacion = models.DateTimeField(auto_now_add=False, verbose_name='Fecha de Modificacion', null=False, blank=False)
+    ruta_archivo = models.FileField(blank=True, max_length=250, upload_to=custom_upload_to)
+    fecha_modificacion = models.DateTimeField(auto_now=True, verbose_name='Fecha de Modificación', null=False,
+                                              blank=False)
 
     def __str__(self):
         return self.codigo
@@ -86,7 +88,9 @@ class ConsecutivoContrato(models.Model):
         :return: Instacia de consecutivo de contratos con la información especificada en el diccionario.
         """
         consecutivo = ConsecutivoContrato()
-        consecutivo.usuario_id = datos.get('colaborador', '')
+        consecutivo.usuario_id = datos.get('colaborador', None)
+        if consecutivo.usuario_id == '':
+            consecutivo.usuario_id = None
         consecutivo.tercero_id = datos.get('tercero', '')
         consecutivo.tipo_contrato_id = datos.get('tipo_contrato', '')
         consecutivo.fecha_inicio = datos.get('fecha_inicio', '')
--- GestionDocumental/views/consecutivo_contrato.py
+++ GestionDocumental/views/consecutivo_contrato.py
@@ -4,6 +4,7 @@ import os
 from sqlite3 import IntegrityError
 
 from django.contrib import messages
+from django.core.exceptions import ValidationError
 from django.db.models import Q
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
@@ -73,23 +74,36 @@ class ConsecutivoContratoCrearView(AbstractEvaLoggedView):
 
 class ConsecutivoContratoEditarView(AbstractEvaLoggedView):
     def get(self, request, id_contrato):
-        consecutivo = ConsecutivoContrato.objects.get(id=id_contrato)
         return render(request, 'GestionDocumental/ConsecutivoContratos/_modal_crear_editar_consecutivo.html',
-                      datos_xa_render(request))
+                      datos_xa_render_editar(request, id_contrato))
 
-    def post(self, request):
+    def post(self, request, id_contrato):
+        update_fields = ['fecha_inicio', 'fecha_final', 'codigo', 'tercero_id',
+                         'tipo_contrato_id', 'usuario_id']
         consecutivo = ConsecutivoContrato.from_dictionary(request.POST)
-        consecutivo.empresa_id = get_id_empresa_global(request)
-        consecutivo.numero_contrato = ConsecutivoDocumento \
-            .get_consecutivo_por_anho(tipo_documento_id=TipoDocumento.CONTRATOS,
-                                      empresa_id=get_id_empresa_global(request))
+        consecutivo_db = ConsecutivoContrato.objects.get(id=id_contrato)
+
+        consecutivo.id = consecutivo_db.id
+        consecutivo.ruta_archivo = consecutivo_db.ruta_archivo
+        consecutivo.numero_contrato = consecutivo_db.numero_contrato
+        consecutivo.usuario_crea = consecutivo_db.usuario_crea
+        consecutivo.empresa = consecutivo_db.empresa
         sigla = TipoContrato.objects.get(id=consecutivo.tipo_contrato_id).sigla
-        consecutivo.codigo = 'CTO_{0:03d}_{1}_{2}'.format(consecutivo.numero_contrato, sigla,
-                                                          app_datetime_now().year)
-        consecutivo.usuario_crea = request.user
-        consecutivo.save()
-        messages.success(request, 'Se ha creado el consecutivo {0}'.format(consecutivo.codigo))
-        return redirect(reverse('GestionDocumental:consecutivo-contratos-index', args=[0]))
+        consecutivo.codigo = 'CTO_{0:03d}_{1}_{2}'.format(consecutivo.numero_contrato, sigla, app_datetime_now().year)
+
+        try:
+            consecutivo.full_clean(validate_unique=False)
+        except ValidationError as errores:
+            messages.error(request, 'Falló editar. Valide los datos ingresados al editar el consecutivo')
+            return redirect(reverse('GestionDocumental:consecutivo-contratos-index', args=[0]))
+
+        if consecutivo_db.comparar(consecutivo, excluir=['fecha_modificacion', 'ruta_archivo']):
+            messages.success(request, 'No se hicieron cambios en la consecutivo {0}'.format(consecutivo.codigo))
+            return redirect(reverse('GestionDocumental:consecutivo-contratos-index', args=[0]))
+        else:
+            consecutivo.save(update_fields=update_fields)
+            messages.success(request, 'Se ha editado el consecutivo {0}'.format(consecutivo.codigo))
+            return redirect(reverse('GestionDocumental:consecutivo-contratos-index', args=[0]))
 
 
 class ConsecutivoContratoEliminarView(AbstractEvaLoggedView):
@@ -168,6 +182,36 @@ def datos_xa_render(request) -> dict:
              'tipo_contratos': tipo_contratos.get_xa_select_activos().exclude(id=0),
              'extra_tipos_contrato': json.dumps(extra_tipos_contrato),
              'menu_actual': 'consecutivos-contrato'}
+
+    return datos
+
+
+def datos_xa_render_editar(request, id_contrato) -> dict:
+    consecutivo = ConsecutivoContrato.objects.get(id=id_contrato)
+    tipo_contratos = TipoContrato.objects
+    colaboradores = Colaborador.objects.get_xa_select_usuarios_activos_x_empresa(request)
+    terceros = Tercero.objects.get_xa_select_activos()
+    extra_tipos_contrato = []
+    for tipo_contrato in tipo_contratos.all():
+        extra_tipos_contrato.append({'id': tipo_contrato.id, 'laboral': tipo_contrato.laboral,
+                                     'fecha_fin': tipo_contrato.tiene_fecha_fin})
+
+    datos = {'fecha': datetime.datetime.now(),
+             'tipo_terminacion': request.POST.get('tipo_terminacion', ''),
+             'colaboradores': colaboradores,
+             'terceros': terceros,
+             'tipo_contratos': tipo_contratos.get_xa_select_activos().exclude(id=0),
+             'extra_tipos_contrato': json.dumps(extra_tipos_contrato),
+             'contrato_actual': id_contrato,
+             'menu_actual': 'consecutivos-contrato',
+             'colaborador_actual': consecutivo.usuario_id,
+             'tercero_actual': consecutivo.tercero_id,
+             'tipo_contrato_actual': consecutivo.tipo_contrato_id,
+             'fecha_inicial':consecutivo.fecha_inicio,
+             'fecha_final':consecutivo.fecha_final,
+             'tipo_contrato_actu': consecutivo.tipo_contrato,
+             'editar':True
+             }
 
     return datos
 
