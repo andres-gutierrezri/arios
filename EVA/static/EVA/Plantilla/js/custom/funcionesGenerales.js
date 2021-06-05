4c220de47ebb0ff187a370e3cd063bd8f75bc34c
Nicolas_Aponte <sistemas3@arios-ing.com>
2021-06-05 02:45:02 -0500

    EVA-202 feat: Se crea función para copiar elementos de una tabla.
    Recibe como parámetro el id del botón y la columna del elemento que se requiere copiar.
    Se puede copiar un solo campo de la tabla o toda la fila.

--- EVA/static/EVA/Plantilla/js/custom/funcionesGenerales.js
+++ EVA/static/EVA/Plantilla/js/custom/funcionesGenerales.js
@@ -22,3 +22,41 @@ function abrirModalVistaPrevia(urlDocumento) {
 
     $("#vista_previa_modal").modal('show');
 }
+
+
+function fcopiarElemento(idElementoCopiar ,columna)
+{
+    let codigoConsecutivo;
+    let idElemento = "copiar_"+idElementoCopiar
+    let range = document.createRange();
+    if(typeof columna !== 'undefined' || !$.isEmptyObject(columna)){
+        codigoConsecutivo = document.getElementById(idElemento).parentNode.parentNode.cells[columna];
+    }
+    else {
+        codigoConsecutivo = document.getElementById(idElemento).parentNode.parentNode
+    }
+    range.selectNode(codigoConsecutivo);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+    alert(codigoConsecutivo.textContent);
+}
+
+/*
+let elementos = $('.copiarCodigo');
+for (let i = 0; i < elementos.length; i++) {
+        let codigoConsecutivo;
+        elementos[i].addEventListener('click', function() {
+        let range = document.createRange();
+        codigoConsecutivo = this.parentNode.parentNode.cells[0];
+        range.selectNode(codigoConsecutivo); //changed here
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();
+        alert(codigoConsecutivo.textContent);
+    });
+}
+
+*/
