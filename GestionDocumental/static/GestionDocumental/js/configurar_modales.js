60396925df278a1ad3e049af76710352529261a6
Victor Alfonso <sistemas@arios-ing.com>
2021-06-08 12:06:30 -0500

    EVA-204 refactor: se modifica la funcionalidad del JavaScript en configurar_modales para poder mostrar el modal de consecutivo de oficio

--- GestionDocumental/static/GestionDocumental/js/configurar_modales.js
+++ GestionDocumental/static/GestionDocumental/js/configurar_modales.js
@@ -1,5 +1,6 @@
 
 const modalCrear = $('#crear');
+const modalCrearOficio = $('#crear-oficio');
 
 function abrirModalCargar(url) {
     $('#cargar').load(url, function (responseText, textStatus, req) {
@@ -40,6 +41,22 @@ function abrirModalCrear(url) {
             }
             $(this).modal('show');
             configurarModalCrear ();
+        } catch (err) {
+            console.log(err);
+            EVANotificacion.toast.error('Ha ocurrido un error al cargar el archivo');
+        }
+    });
+}
+
+function abrirModalCrearOficio(url) {
+    modalCrearOficio.load(url, function (responseText, textStatus, req) {
+        try {
+            if (responseText.includes("<!DOCTYPE html>")) {
+                EVANotificacion.toast.error('No tiene permisos para acceder a esta funcionalidad');
+                return false;
+            }
+            $(this).modal('show');
+            configurarModalCrearOficio ();
         } catch (err) {
             console.log(err);
             EVANotificacion.toast.error('Ha ocurrido un error al cargar el archivo');
@@ -125,6 +142,16 @@ function configurarModalCrear() {
 
     agregarValidacionFormularios();
 }
+
+
+function configurarModalCrearOficio() {
+
+    $('#contrato_id_select_id').select2({
+            dropdownParent: modalCrearOficio
+        }
+    );
+}
+
 
 function CambiarSelect (actual){
     $.each(jQuery.parseJSON(extraTiposContrato.val()), function(key, value) {
