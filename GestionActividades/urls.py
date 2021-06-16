1f8e9f341522f824948d304fd8eb131d2818f99a
Victor Alfonso <sistemas@arios-ing.com>
2021-06-16 17:13:16 -0500

    EVA-210 feat: Creación de permiso y url del modulo de Gestión Actividades

---
+++ GestionActividades/urls.py
@@ -0,0 +1,24 @@
+"""EVA URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from GestionActividades.views import views
+
+app_name = 'GestionActividades'
+
+urlpatterns = [
+    path('index/', views.PrincipalView.as_view(), name='index'),
+]
