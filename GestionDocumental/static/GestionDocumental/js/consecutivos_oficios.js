baf49b89c979eed1010534093631fa685c232e21
Alfonso Ardila <asesorsistemas@arios-ing.com>
2021-06-18 04:03:12 -0500

    EVA-207 feat: se mejora la visualización de la tabla de consecutivos de oficios para que se adapte mejor a los diferentes tamaños de pantallas.

--- GestionDocumental/static/GestionDocumental/js/consecutivos_oficios.js
+++ GestionDocumental/static/GestionDocumental/js/consecutivos_oficios.js
@@ -5,7 +5,16 @@ const modalCrearOficio = $('#crear-oficio');
 $(document).ready(function () {
     activarSelect2();
     configurarFiltroConsecutivos();
-    iniciarDataTableN({buscar: false, paginar: false, ordenar: false});
+    const columnDefs = [
+        { "targets": [0], "width": '12%' },
+        { "targets": [1], "width": '10%' },
+        { "targets": [2], "width": '28%' },
+        { "targets": [3], "width": '28%' },
+        { "targets": [4], "width": '11%' },
+        { "targets": [7], "width": '5%' },
+        { "targets": [8], "width": '6%' }
+    ]
+    iniciarDataTableN({buscar: false, paginar: false, ordenar: false, detallesColumnas: columnDefs});
 });
 
 function abrirModalCrearOficio(url) {
