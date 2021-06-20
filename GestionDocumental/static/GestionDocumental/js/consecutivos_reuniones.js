c42a628026b67acf6ebc29606ac92a8fe139bfbe
Alfonso Ardila <asesorsistemas@arios-ing.com>
2021-06-20 14:40:26 -0500

    EVA-209 feat: se mejora la visualización de la tabla de consecutivos de actas de reuniones  para que se adapte mejor a los diferentes tamaños de pantallas.

--- GestionDocumental/static/GestionDocumental/js/consecutivos_reuniones.js
+++ GestionDocumental/static/GestionDocumental/js/consecutivos_reuniones.js
@@ -5,7 +5,17 @@ const modalCrearReunion = $('#crear-reunion');
 $(document).ready(function () {
     activarSelect2();
     configurarFiltroConsecutivos();
-    iniciarDataTableN({buscar: false, paginar: false, ordenar: false});
+    const columnDefs = [
+        { "targets": [0], "width": '10%' },
+        { "targets": [1], "width": '7%' },
+        { "targets": [2], "width": '26%' },
+        { "targets": [3], "width": '25%' },
+        { "targets": [4], "width": '11%' },
+        { "targets": [5], "width": '10%' },
+        { "targets": [8], "width": '5%' },
+        { "targets": [9], "width": '6%' }
+    ]
+    iniciarDataTableN({buscar: false, paginar: false, ordenar: false, detallesColumnas: columnDefs});
 });
 
 function abrirModalCrearReunion(url) {
