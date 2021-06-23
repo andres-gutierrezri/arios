5ab1eb4de77df939084ffce39c2328843930aa7a
Victor Alfonso <sistemas@arios-ing.com>
2021-06-23 14:37:01 -0500

    EVA-212 feat: Creación del método post en el View Crear y Editar GrupoActividades para la recepción de los datos del modal crear-editar y guardar en la BD.

--- GestionActividades/views/grupo_actividades.py
+++ GestionActividades/views/grupo_actividades.py
@@ -3,12 +3,14 @@ from datetime import datetime
 from django.db import IntegrityError
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.defaultfilters import lower
 from django.urls import reverse
 from django.contrib import messages
 from django.core.exceptions import ValidationError
 
 from Administracion.models import Proceso, TipoContrato
 from Administracion.utils import get_id_empresa_global
+from EVA.General import app_datetime_now
 from EVA.views.index import AbstractEvaLoggedView
 from GestionActividades.Enumeraciones import PertenenciaGrupoActividades
 from GestionActividades.models import GrupoActividad
@@ -26,6 +28,7 @@ class GruposActividadesIndexView(AbstractEvaLoggedView):
 
         return render(request, 'GestionActividades/GrupoActividades/index.html',
                       {'grupos_actividades': grupos_actividades,
+                       'fecha': app_datetime_now(),
                        'contratos': contratos,
                        'colaboradores': colaboradores,
                        'procesos': procesos})
@@ -36,6 +39,28 @@ class GruposActividadesCrearView(AbstractEvaLoggedView):
         return render(request, 'GestionActividades/GrupoActividades/modal_crear_editar_grupos_actividades.html',
                       datos_xa_render(request))
 
+    def post(self, request):
+        grupo_actividad = GrupoActividad.from_dictionary(request.POST)
+        grupo_actividad.usuario_crea = request.user
+        grupo_actividad.usuario_modifica = request.user
+        grupo_actividad.fecha_crea = app_datetime_now()
+
+        if grupo_actividad.grupo_actividad_id == '':
+            grupo_actividad.grupo_actividad_id = None
+        if grupo_actividad.tipo_pertenencia == '1':
+            grupo_actividad.proceso_id = None
+        if grupo_actividad.tipo_pertenencia == '2':
+            grupo_actividad.contrato_id = None
+
+        if GrupoActividad.objects.filter(nombre__iexact=grupo_actividad.nombre,
+                                         grupo_actividad_id__exact=grupo_actividad.grupo_actividad_id).exists():
+            messages.error(request, 'Falló crear. Ya existe un grupo con el mismo nombre')
+            return redirect(reverse('GestionActividades:grupo-actividades-index'))
+        else:
+            grupo_actividad.save()
+            messages.success(request, 'Se ha creado el grupo de actividades <br> {0}'.format(grupo_actividad.nombre))
+            return redirect(reverse('GestionActividades:grupo-actividades-index'))
+
 
 class GruposActividadesEditarView(AbstractEvaLoggedView):
     def get(self, request, id_grupo):
@@ -44,17 +69,55 @@ class GruposActividadesEditarView(AbstractEvaLoggedView):
         return render(request, 'GestionActividades/GrupoActividades/modal_crear_editar_grupos_actividades.html',
                       datos_xa_render(request, grupo_actividad))
 
+    def post(self, request, id_grupo):
+        update_fields = ['fecha_modificacion', 'contrato_id', 'proceso_id', 'nombre', 'descripcion', 'fecha_crea',
+                         'tipo_pertenencia', 'motivo', 'usuario_modifica', 'usuario_crea', 'grupo_actividad_id']
+
+        grupo_actividad = GrupoActividad.from_dictionary(request.POST)
+        grupo_actividad_db = GrupoActividad.objects.get(id=id_grupo)
+        grupo_actividad.fecha_crea = grupo_actividad_db.fecha_crea
+        grupo_actividad.fecha_modificacion = app_datetime_now()
+        grupo_actividad.id = grupo_actividad_db.id
+        grupo_actividad.usuario_modifica = request.user
+        grupo_actividad.usuario_crea = grupo_actividad_db.usuario_crea
+
+        if grupo_actividad.grupo_actividad_id == '':
+            grupo_actividad.grupo_actividad_id = None
+        if grupo_actividad.tipo_pertenencia == '1':
+            grupo_actividad.proceso_id = None
+        if grupo_actividad.tipo_pertenencia == '2':
+            grupo_actividad.contrato_id = None
+
+        if GrupoActividad.objects.filter(nombre__iexact=grupo_actividad.nombre,
+                                         grupo_actividad_id__exact=grupo_actividad.grupo_actividad_id).exists():
+            messages.error(request, 'Falló Editar. Ya existe un grupo con el mismo nombre')
+            return redirect(reverse('GestionActividades:grupo-actividades-index'))
+
+        try:
+            grupo_actividad.full_clean(validate_unique=False)
+        except ValidationError as errores:
+            messages.error(request, 'Falló editar. Valide los datos ingresados al editar el grupo de actividades')
+            return redirect(reverse('GestionActividades:grupo-actividades-index'))
+
+        if grupo_actividad_db.comparar(grupo_actividad, excluir=['fecha_modificacion', 'motivo']):
+            messages.success(request, 'No se hicieron cambios en el grupo de actividades {0}'
+                             .format(grupo_actividad.nombre))
+            return redirect(reverse('GestionActividades:grupo-actividades-index'))
+        else:
+            grupo_actividad.save(update_fields=update_fields)
+            messages.success(request, 'Se ha editado el grupo de actividades <br> {0}'.format(grupo_actividad.nombre))
+            return redirect(reverse('GestionActividades:grupo-actividades-index'))
+
 
 def datos_xa_render(request, grupo_actividad: GrupoActividad = None) -> dict:
     tipo_pertenencia = [{'campo_valor': 1, 'campo_texto': 'Contrato'},
                         {'campo_valor': 2, 'campo_texto': 'Proceso'}]
 
-    grupo_actividades = GrupoActividad.objects
-    extra_tipos_pertenencia = []
-    for tipo_grupo in grupo_actividades.all():
-        extra_tipos_pertenencia.append({'id': tipo_grupo.id, 'tipo_pertenencia': tipo_grupo.tipo_pertenencia})
+    grupos = GrupoActividad.objects.values('id', 'nombre')
+    lista_grupos = []
+    for grupo in grupos:
+        lista_grupos.append({'campo_valor': grupo['id'], 'campo_texto': grupo['nombre']})
 
-    print(extra_tipos_pertenencia)
     contratos = Contrato.objects \
         .filter(empresa_id=get_id_empresa_global(request)) \
         .values('id', 'numero_contrato', 'cliente__nombre')
@@ -72,14 +135,12 @@ def datos_xa_render(request, grupo_actividad: GrupoActividad = None) -> dict:
     for proceso in procesos:
         lista_procesos.append({'campo_valor': proceso['id'], 'campo_texto': proceso['nombre']})
 
-    datos = {
-        'contratos': lista_contratos,
-        'procesos': lista_procesos,
-        'lista_procesos': lista_procesos,
-        'tipo_pertenencia': tipo_pertenencia,
-        'extra_tipos_pertenencia': json.dumps(extra_tipos_pertenencia),
-        'grupo_actividades': grupo_actividades,
-        'menu_actual': 'grupo-actividades'}
+    datos = {'fecha': app_datetime_now(),
+             'contratos': lista_contratos,
+             'procesos': lista_procesos,
+             'tipo_pertenencia': tipo_pertenencia,
+             'grupos': lista_grupos,
+             'menu_actual': 'grupo-actividades'}
 
     if grupo_actividad:
         datos['grupo_actividad'] = grupo_actividad
