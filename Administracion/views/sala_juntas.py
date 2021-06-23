c2ae9a79bf98e5c40402165196462ae05536e9f5
Andres Felipe Gutierrez Rivera <sistemas4@arios-ing.com>
2021-06-23 10:36:39 -0500

    EVA-214 feat: Se modifica la vista para el calendario, creando un diccionario de todos los objetos almacenados en el modelo de Reserva para la Sala de Juntas
    La finalidad de este diccionario, es poder retornar la respuesta de los datos en formato JSON para la correspondiente visualización de los eventos en el calendario, adicionalmente se crea una función estática, donde se retorne tres colores (gris, verde y azul) para distinguir los eventos en fechas pasadas, presentes y futuras

--- Administracion/views/sala_juntas.py
+++ Administracion/views/sala_juntas.py
@@ -1,11 +1,37 @@
+from datetime import datetime
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from Administracion.models.models import ReservaSalaJuntas
+from EVA.General import app_datetime_now
+from EVA.General.conversiones import datetime_to_isostring
 from EVA.views.index import AbstractEvaLoggedView
 
 
 class ReservaSalaJuntasView(AbstractEvaLoggedView):
     def get(self, request):
-        reserva_sala_juntas = ReservaSalaJuntas.objects.all()
-        return render(request, 'Administracion/SalaJuntas/calendario.html',
-                      {'reserva_sala_juntas': reserva_sala_juntas, 'menu_actual': 'reserva-sala-juntas'})
+        if request.resolver_match.url_name == 'reserva-sala-juntas-json':
+            reservas = ReservaSalaJuntas.objects.all()
+            reservas_dict = []
+
+            for reserva in reservas:
+                reservas_dict.append({'id': reserva.id, 'title': f'{reserva.tema} \n {reserva.responsable.username}',
+                                      'start': datetime_to_isostring(reserva.fecha_inicio),
+                                      'end': datetime_to_isostring(reserva.fecha_fin),
+                                      'color': self.get_color_reserva(reserva.fecha_inicio, reserva.fecha_fin)})
+
+            return JsonResponse(reservas_dict, safe=False)
+        else:
+            return render(request, 'Administracion/SalaJuntas/calendario.html',
+                          {'menu_actual': 'reserva-sala-juntas'})
+
+    @staticmethod
+    def get_color_reserva(fecha_inicio: datetime, fecha_fin: datetime):
+        actual = app_datetime_now()
+        if fecha_fin < actual:
+            return 'gray'
+        elif fecha_inicio > actual:
+            return 'blue'
+        else:
+            return 'green'
