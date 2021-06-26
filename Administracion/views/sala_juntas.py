b901c7a94edd3cf3a50e6a16745760935a9ed409
Andres Felipe Gutierrez Rivera <sistemas4@arios-ing.com>
2021-06-26 17:27:02 -0500

    EVA-215 feat: Se crea la vista para la adición de los eventos correspondientes a la reserva de la sala de juntas
    Adicionalmente se crea la función "datos_xa_render"  para la visualización de los colaboradores en el campo de selección de responsable para la reserva en el cuadro modal

--- Administracion/views/sala_juntas.py
+++ Administracion/views/sala_juntas.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 
+from django.contrib import messages
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from Administracion.models.models import ReservaSalaJuntas
 from EVA.General import app_datetime_now
 from EVA.General.conversiones import datetime_to_isostring
 from EVA.views.index import AbstractEvaLoggedView
+from TalentoHumano.models import Colaborador
 
 
 class ReservaSalaJuntasView(AbstractEvaLoggedView):
@@ -35,3 +38,26 @@ class ReservaSalaJuntasView(AbstractEvaLoggedView):
             return 'blue'
         else:
             return 'green'
+
+
+class ReservaSalaJuntasCrearView(AbstractEvaLoggedView):
+    def get(self, request):
+        return render(request, 'Administracion/SalaJuntas/modal_crear_editar_reserva.html', datos_xa_render(request))
+
+    def post(self, request):
+        reserva = ReservaSalaJuntas.from_dictionary(request.POST)
+        reserva.usuario_crea = request.user
+        reserva.fecha_creacion = app_datetime_now()
+
+        reserva.save()
+        messages.success(request, 'Se ha creado la reserva en la sala de juntas')
+        return redirect(reverse('Administracion:reserva-sala-juntas'))
+
+
+def datos_xa_render(request) -> dict:
+    colaboradores = Colaborador.objects.get_xa_select_usuarios_activos_x_empresa(request)
+
+    datos = {'colaboradores': colaboradores,
+             'menu_actual': 'reserva-sala-juntas'}
+
+    return datos
