7051c90e76da83dc4b90353be79ea3e95da7f5c1
Victor Alfonso <sistemas@arios-ing.com>
2021-06-26 19:23:52 -0500

    EVA-219 feat: Creación de los Views para poder realizar el envió y recepción de los datos del modal crear-editar actividades y del index de actividades

---
+++ GestionActividades/views/actividades.py
@@ -0,0 +1,172 @@
+import json
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.template.defaultfilters import lower
+from django.urls import reverse
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+
+from Administracion.models import Proceso, TipoContrato
+from Administracion.utils import get_id_empresa_global
+from EVA.General import app_datetime_now
+from EVA.views.index import AbstractEvaLoggedView
+from GestionActividades.Enumeraciones import PertenenciaGrupoActividades, EstadosActividades
+from GestionActividades.models.models import Actividad, GrupoActividad, ResponsableActividad
+from Proyectos.models import Contrato
+from TalentoHumano.models import Colaborador
+from TalentoHumano.models.colaboradores import ColaboradorProceso
+
+
+class ActividadesIndexView(AbstractEvaLoggedView):
+    def get(self, request):
+        actividades = Actividad.objects.values('id', 'nombre', 'grupo_actividad_id', 'descripcion',
+                                               'estado', 'porcentaje_avance')
+        responsable_actividad = ResponsableActividad.objects.values('responsable_id', 'actividad_id')
+        colaboradores = User.objects.values('id', 'first_name', 'last_name')
+        grupos = GrupoActividad.objects.values('id', 'nombre')
+        opciones_estado = [1, 2, 3, 4, 5]
+
+        return render(request, 'GestionActividades/Actividades/index.html',
+                      {'actividades': actividades,
+                       'grupos': grupos,
+                       'responsable_actividad': responsable_actividad,
+                       'colaboradores': colaboradores,
+                       'estados': EstadosActividades.labels,
+                       'opciones_estado': opciones_estado,
+                       'fecha': app_datetime_now()})
+
+
+class ActividadesCrearView(AbstractEvaLoggedView):
+    def get(self, request):
+        return render(request, 'GestionActividades/Actividades/modal_crear_editar_actividades.html',
+                      datos_xa_render(request))
+
+    def post(self, request):
+        grupo_actividad = GrupoActividad.from_dictionary(request.POST)
+        actividad = Actividad.from_dictionary(request.POST)
+        actividad.usuario_crea = request.user
+        actividad.usuario_modifica = request.user
+        actividad.fecha_crea = app_datetime_now()
+        responsables = request.POST.getlist('responsables_id[]', None)
+
+        if actividad.grupo_actividad_id == '':
+            if GrupoActividad.objects.filter(nombre__iexact='generales').exists():
+                grupo_generales = list(GrupoActividad.objects.values('id').filter(nombre__iexact='generales'))
+                actividad.grupo_actividad_id = grupo_generales[0].get('id')
+            else:
+                messages.error(request, 'Falló editar. El grupo Generales no existe')
+                return redirect(reverse('GestionActividades:actividades-index'))
+
+        if Actividad.objects.filter(nombre__iexact=actividad.nombre,
+                                    grupo_actividad_id__exact=actividad.grupo_actividad_id).exists():
+            messages.error(request, 'Falló crear. Ya existe una actividad con el mismo nombre dentro del grupo')
+            return redirect(reverse('GestionActividades:actividades-index'))
+        elif GrupoActividad.objects.filter(nombre__iexact=actividad.nombre).exists():
+            messages.error(request, 'Falló crear. No puede colocar el mismo nombre del grupo '
+                                    'que va a contener la actividad')
+            return redirect(reverse('GestionActividades:actividades-index'))
+        else:
+            actividad.save()
+            for responsable in responsables:
+                ResponsableActividad.objects.create(responsable_id=responsable, actividad=actividad)
+            messages.success(request, 'Se ha creado la actividad <br> {0}'.format(grupo_actividad.nombre))
+            return redirect(reverse('GestionActividades:actividades-index'))
+
+
+class ActividadesEditarView(AbstractEvaLoggedView):
+    def get(self, request, id_actividad):
+        actividad = Actividad.objects.get(id=id_actividad)
+        return render(request, 'GestionActividades/Actividades/modal_crear_editar_actividades.html',
+                      datos_xa_render(request, actividad))
+
+    def post(self, request, id_actividad):
+        update_fields = ['fecha_modificacion', 'supervisor_id', 'fecha_inicio', 'fecha_fin', 'nombre', 'descripcion',
+                         'fecha_crea', 'motivo', 'usuario_modifica', 'usuario_crea',
+                         'grupo_actividad_id', 'estado', 'soporte_requerido']
+        grupo_actividad = GrupoActividad.from_dictionary(request.POST)
+        actividad = Actividad.from_dictionary(request.POST)
+        actividad_db = Actividad.objects.get(id=id_actividad)
+        actividad.fecha_crea = actividad_db.fecha_crea
+        actividad.fecha_modificacion = app_datetime_now()
+        actividad.id = actividad_db.id
+        actividad.usuario_modifica = request.user
+        actividad.usuario_crea = actividad_db.usuario_crea
+        responsables = request.POST.getlist('responsables_id[]', None)
+        actividad.soporte_requerido = request.POST.get('soporte_requerido', 'False') == 'True'
+
+        if actividad.grupo_actividad_id == '':
+            if GrupoActividad.objects.filter(nombre__iexact='generales').exists():
+                grupo_generales = list(GrupoActividad.objects.values('id').filter(nombre__iexact='generales'))
+                actividad.grupo_actividad_id = grupo_generales[0].get('id')
+            else:
+                messages.error(request, 'Falló editar. El grupo Generales no existe')
+                return redirect(reverse('GestionActividades:actividades-index'))
+
+        responsables_actividad_db = ResponsableActividad.objects.filter(actividad_id=id_actividad)
+        cantidad_responsables = responsables_actividad_db.count()
+        conteo_responsables = 0
+        if cantidad_responsables == len(responsables):
+            for clb in responsables_actividad_db:
+                for ctr in responsables:
+                    if clb.responsable.id == int(ctr):
+                        conteo_responsables += 1
+        else:
+            conteo_responsables = len(responsables)
+
+        try:
+            actividad.full_clean(validate_unique=False)
+        except ValidationError as errores:
+            messages.error(request, 'Falló editar. Valide los datos ingresados al editar la actividad')
+            return redirect(reverse('GestionActividades:actividades-index'))
+
+        if GrupoActividad.objects.filter(nombre__iexact=actividad.nombre).exists():
+            messages.error(request, 'Falló editar. No puede colocar el mismo nombre del grupo '
+                                    'que va a contener la actividad')
+            return redirect(reverse('GestionActividades:actividades-index'))
+
+        if actividad_db.comparar(actividad, excluir=['fecha_modificacion', 'motivo', 'calificacion', 'codigo',
+                                                     'porcentaje_avance']) \
+                and conteo_responsables == cantidad_responsables:
+            messages.success(request, 'No se hicieron cambios en la actividad {0}'
+                             .format(actividad.nombre))
+            return redirect(reverse('GestionActividades:actividades-index'))
+        else:
+            actividad.save(update_fields=update_fields)
+            ResponsableActividad.objects.filter(actividad_id=id_actividad).delete()
+            for responsable in responsables:
+                ResponsableActividad.objects.create(responsable_id=responsable, actividad_id=id_actividad)
+            messages.success(request, 'Se ha editado la actividad <br> {0}'.format(actividad.nombre))
+            return redirect(reverse('GestionActividades:actividades-index'))
+
+
+def datos_xa_render(request, actividad: Actividad = None) -> dict:
+    grupos = GrupoActividad.objects.values('id', 'nombre')
+    lista_grupos = []
+    for grupo in grupos:
+        lista_grupos.append({'campo_valor': grupo['id'], 'campo_texto': grupo['nombre']})
+
+    colaboradores = User.objects.values('id', 'first_name', 'last_name')
+    lista_colaboradores = []
+    for colaborador in colaboradores:
+        lista_colaboradores.append({'campo_valor': colaborador['id'],
+                                   'campo_texto': colaborador['first_name']+' '+colaborador['last_name']})
+
+    responsable_actividad = ResponsableActividad.objects.get_ids_responsables_list(actividad)
+
+    datos = {'fecha': app_datetime_now(),
+             'grupos': lista_grupos,
+             'colaboradores': lista_colaboradores,
+             'menu_actual': 'actividades',
+             'responsable_actividad': responsable_actividad,
+             'estado': EstadosActividades.choices}
+
+    if actividad:
+        datos['actividad'] = actividad
+        datos['editar'] = True
+
+    return datos
+
