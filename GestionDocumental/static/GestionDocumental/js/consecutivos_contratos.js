4f4a62cb863225255e6c375eecdce7ea4a8b285c
Nicolas_Aponte <sistemas3@arios-ing.com>
2021-07-21 00:35:00 -0500

    EVA-235 refactor: Se ajusta el ancho de las columnas para la tabla de consecutivos de contratos. - Se deja la fecha de creación para que se muestre con el + de la tabla.

--- GestionDocumental/static/GestionDocumental/js/consecutivos_contratos.js
+++ GestionDocumental/static/GestionDocumental/js/consecutivos_contratos.js
@@ -17,7 +17,17 @@ $(document).ready(function () {
         window.location = '/gestion-documental/consecutivo-contratos/' + this.value + '/index';
     });
 
-    iniciarDataTableN({buscar: false, paginar:false, ordenar: false});
+    const columnDefs = [
+        { "targets": [0], "width": '18%' },
+        { "targets": [1], "width": '17%' },
+        { "targets": [3], "width": '10%' },
+        { "targets": [4], "width": '10%' },
+        { "targets": [5], "width": '25%' },
+        { "targets": [6], "width": '12%' },
+        { "targets": [7], "width": '4%' },
+        { "targets": [8], "width": '4%' }
+    ]
+    iniciarDataTableN({buscar: false, paginar: false, ordenar: false, detallesColumnas: columnDefs});
 });
 
 function abrirModalCargar(url) {
