579e5bbd0ee74a28230dc14f0bb9a567535f0d1a
Victor Alfonso <sistemas@arios-ing.com>
2021-07-29 20:15:10 -0500

    EVA-236 feat: Se agregaron estilos y condiciones en los templates del modal editar actividad, index de actividades, index de aprobaciones de actividades y en el Js de la acción de la actividad.
    - Se agregaron condiciones en el modal de editar actividad para comparar los campos que se hayan modificado y mostrarlos al supervisor.
    - Se agregaron nuevos campos en la tabla de index de aprobación de actividades.
    -Se agrego el DataTable en el Js accion_actividad_modal para la tabla del index de aprobación de actividades.
    -Se elimino el icono del segundo modal en el index de actividades, con el Js, permiso y Url del mismo.

--- GestionActividades/static/GestionActividades/js/accion_actividad_modal.js
+++ GestionActividades/static/GestionActividades/js/accion_actividad_modal.js
@@ -5,6 +5,15 @@ const modalAccionActividad = $('#accion-actividad');
 $(document).ready(function () {
     activarSelect2();
     configurarFiltroConsecutivos();
+    const columnDefs = [
+        { "targets": [0], "width": '12%' },
+        { "targets": [1], "width": '10%' },
+        { "targets": [2], "width": '28%' },
+        { "targets": [4], "width": '28%' },
+        { "targets": [6], "width": '11%' },
+        { "targets": [7], "width": '11%' },
+    ]
+    iniciarDataTableN({buscar: false, paginar: false, ordenar: false, detallesColumnas: columnDefs});
 });
 
 function abrirModalAccionActividad(url) {
