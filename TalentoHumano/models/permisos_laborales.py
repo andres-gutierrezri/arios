9b68cbcb8f3dd315625401469797acb20af78a6e
Andres Felipe Gutierrez Rivera <sistemas4@arios-ing.com>
2021-07-31 00:10:13 -0500

    EVA-240 refactor: Se modificó el modelo "PermisoLaboral" con el propósito de usar la nueva configuración del "ModeloBase"
    Se quitaron las fechas y los usuarios de creación y modificación porque se heredan del modelo base. También se modificó los nombres asociados a talento humano, de TH por RRHH, y se cambió el nombre del campo de tipo de permiso por simplemente nombre.

--- TalentoHumano/models/permisos_laborales.py
+++ TalentoHumano/models/permisos_laborales.py
@@ -5,16 +5,16 @@ from EVA import settings
 from EVA.General import app_date_now
 from EVA.General.conversiones import string_to_datetime
 from EVA.General.modeljson import ModelDjangoExtensiones
-from EVA.General.modelmanagers import ManagerGeneral
+from EVA.General.modelmanagers import ManagerGeneral, ModeloBase
 
 
 class TipoPermiso (models.Model):
     objects = ManagerGeneral()
-    tipo = models.CharField(max_length=100, verbose_name='Tipo de Permiso', null=False, blank=False)
+    nombre = models.CharField(max_length=100, verbose_name='Tipo de Permiso', null=False, blank=False)
     estado = models.BooleanField(verbose_name='Estado', null=False, blank=False, default=True)
 
     def __str__(self):
-        return self.tipo
+        return self.nombre
 
     class Meta:
         verbose_name = 'Tipo de Permiso'
@@ -27,27 +27,21 @@ class TipoPermiso (models.Model):
         :param datos: Diccionario con los datos para crear el Tipo de Permiso.
         :return: Instancia de Tipo de Permiso con la información especificada en el diccionario.
         """
-        permiso = TipoPermiso()
-        permiso.tipo = datos.get('tipo', '')
+        tipo = TipoPermiso()
+        tipo.nombre = datos.get('nombre', '')
 
-        return permiso
+        return tipo
 
 
 def custom_upload_to(instance, filename):
-    return '{3}/Permisos/Documentos/{0}/{1}/{2}'.format(app_date_now().year, instance.usuario_crea.username, filename,
-                                                        settings.EVA_PRIVATE_MEDIA)
+    return '{3}/Permisos/Soportes/{0}/{1}/{2}'.format(app_date_now().year, instance.usuario_crea.username, filename,
+                                                      settings.EVA_PRIVATE_MEDIA)
 
 
-class PermisoLaboral (models.Model, ModelDjangoExtensiones):
+class PermisoLaboral (ModeloBase, ModelDjangoExtensiones):
     objects = ManagerGeneral()
-    usuario_crea = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="Usuario Crea", null=False,
-                                     blank=False, related_name='PermisoLaboralCrea')
-    usuario_modifica = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="Usuario Modifica", null=True,
-                                         blank=False, related_name='PermisoLaboralModifica')
     fecha_inicio = models.DateTimeField(verbose_name='Fecha Inicio', null=False, blank=False)
     fecha_fin = models.DateTimeField(verbose_name='Fecha Fin', null=False, blank=False)
-    fecha_crea = models.DateTimeField(auto_now_add=True, verbose_name='Fecha Crea', null=False, blank=False)
-    fecha_modifica = models.DateTimeField(auto_now=True, verbose_name='Fecha Modifica', null=True, blank=False)
     tipo_permiso = models.ForeignKey(TipoPermiso, on_delete=models.DO_NOTHING, verbose_name='Tipo de Permiso',
                                      null=False, blank=False)
     jefe_inmediato = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Jefe Inmediato', null=True,
@@ -56,10 +50,10 @@ class PermisoLaboral (models.Model, ModelDjangoExtensiones):
     motivo_editar = models.TextField(max_length=100, verbose_name='Motivo Editar', null=False, blank=False)
     soporte = models.FileField(upload_to=custom_upload_to, verbose_name='Documento Soporte', null=False, blank=False,
                                max_length=250)
-    estado_th = models.BooleanField(verbose_name='Estado TH', null=False, blank=False)
+    estado_rrhh = models.BooleanField(verbose_name='Estado RRHH', null=False, blank=False)
     estado_jefe = models.BooleanField(verbose_name='Estado Jefe', null=False, blank=False)
     estado_gerencia = models.BooleanField(verbose_name='Estado Gerencia', null=False, blank=False)
-    motivo_th = models.CharField(max_length=300, verbose_name='Motivo TH', null=True, blank=False)
+    motivo_rrhh = models.CharField(max_length=300, verbose_name='Motivo RRHH', null=True, blank=False)
     motivo_jefe = models.CharField(max_length=300, verbose_name='Motivo Jefe', null=True, blank=False)
     motivo_gerencia = models.CharField(max_length=300, verbose_name='Motivo Gerencia', null=True, blank=False)
     remuneracion = models.BooleanField(verbose_name='Remuneración', null=False, blank=False)
