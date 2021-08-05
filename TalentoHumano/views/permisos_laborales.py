0ee8b7f5489279ad0dc151339a014ea726ac32a0
Andres Felipe Gutierrez Rivera <sistemas4@arios-ing.com>
2021-08-05 04:17:08 -0500

    EVA-241 feat: Se crea en la vista el filtro correspondiente para la búsqueda de la selección del tipo de permiso laboral

--- TalentoHumano/views/permisos_laborales.py
+++ TalentoHumano/views/permisos_laborales.py
@@ -3,18 +3,32 @@ import datetime
 from django.shortcuts import render
 from EVA.General.utilidades import paginar
 from EVA.views.index import AbstractEvaLoggedView
-from TalentoHumano.models import PermisoLaboral
+from TalentoHumano.models import PermisoLaboral, TipoPermiso
 
 
 class PermisosLaboralesIndexView(AbstractEvaLoggedView):
-    def get(self, request):
-        permisos = PermisoLaboral.objects.filter(usuario_crea_id=request.user.id)
+    def get(self, request, id_tipo_permiso):
+        if id_tipo_permiso == 0:
+            permisos = PermisoLaboral.objects.filter(usuario_crea_id=request.user.id)
+        else:
+            permisos = PermisoLaboral.objects.filter(tipo_permiso_id=id_tipo_permiso, usuario_crea_id=request.user.id)
+
         page = request.GET.get('page', 1)
         permisos = paginar(permisos.order_by('-id'), page, 10)
         return render(request, 'TalentoHumano/PermisosLaborales/index.html',
                       {'permisos': permisos,
                        'fecha': datetime.datetime.now(),
+                       'tipos_permiso': self.tipos_permiso_filtro(),
+                       'id_tipo_permiso': id_tipo_permiso,
                        'menu_actual': 'permisos-laborales'})
+
+    @staticmethod
+    def tipos_permiso_filtro() -> list:
+        tipos_permiso = TipoPermiso.objects.filter(estado=True)
+        lista_tipo_permiso = [{'campo_valor': 0, 'campo_texto': 'Todos'}]
+        for tipo_permiso in tipos_permiso:
+            lista_tipo_permiso.append({'campo_valor': tipo_permiso.id, 'campo_texto': tipo_permiso.nombre})
+        return lista_tipo_permiso
 
 
 class PermisoLaboralCrearView(AbstractEvaLoggedView):
