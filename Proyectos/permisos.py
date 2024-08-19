10dc3298392225d91fa379bc933e319f374a0b37
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2024-08-19 18:38:20 -0500

    EVA-250 refactor: Se da formato al archivo "permisos.py" en la aplicación de proyectos.
    - Se realiza un refactor al archivo "permisos.py" en la aplicación de proyectos.

--- Proyectos/permisos.py
+++ Proyectos/permisos.py
@@ -1,11 +1,12 @@
 from EVA.permisos import Permiso
 
-app_name = 'Proyectos'
+app_name = "Proyectos"
 
-permisos = [Permiso('index', None),
-            Permiso('contratos', ['Proyectos.view_contrato']),
-            Permiso('contratos-crear', ['Proyectos.add_contrato']),
-            Permiso('contratos-editar', ['Proyectos.change_contrato']),
-            Permiso('contratos-eliminar', ['Proyectos.delete_contrato']),
-            Permiso('contratos-detalle', ['Proyectos.view_contrato']),
-            ]
+permisos = [
+    Permiso("index", None),
+    Permiso("contratos", ["Proyectos.view_contrato"]),
+    Permiso("contratos-crear", ["Proyectos.add_contrato"]),
+    Permiso("contratos-editar", ["Proyectos.change_contrato"]),
+    Permiso("contratos-eliminar", ["Proyectos.delete_contrato"]),
+    Permiso("contratos-detalle", ["Proyectos.view_contrato"]),
+]
