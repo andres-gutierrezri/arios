508ab19b4dc124c8f3a0fa4862ead6735511b827
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2024-08-21 06:03:27 -0500

    EVA-main feat: Se agregan las variables de entorno al proyecto para la configuración de la base de datos.

--- EVA/local_settings.py
+++ EVA/local_settings.py
@@ -7,19 +7,19 @@ from dotenv import load_dotenv
 # Carga las variables del archivo .env
 load_dotenv()
 
-IS_DEPLOYED = bool(os.getenv("IS_DEPLOYED", ""))
+IS_DEPLOYED = os.getenv('IS_DEPLOYED', 'False') == 'True'
 
 if IS_DEPLOYED:
-    DATABASE_DICT = dj_database_url.config(default=os.getenv("DATABASE_URL"))
-else:
+    DATABASE_DICT = dj_database_url.config(default=os.getenv("DATABASE_PUBLIC_URL"))
+else: 
     DATABASE_DICT = {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "EVA",
-        "USER": "postgres",
-        "PASSWORD": "postgresql",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-        "TEST": {
-            "NAME": "test_EVA",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),   
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': 'test_EVA',
         },
     }
