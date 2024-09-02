61abd1e4d20c02e3faeddc9998afd18d387ca1ab
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2024-09-01 23:26:28 -0500

    EVA-predesarrollo chore: Se actualiza la configuración de la base de datos en "local_settings.py".
    Se actualiza el archivo "local_settings.py" para mejorar la configuración de la base de datos. Ahora se permite seleccionar entre PostgreSQL y MySQL mediante la variable de entorno DATABASE_SELECTOR. Además, se actualizan las variables de entorno en el archivo ".env" relacionadas con la base de datos para reflejar los cambios.

--- EVA/local_settings.py
+++ EVA/local_settings.py
@@ -4,22 +4,63 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
-# Carga las variables del archivo .env
+# Carga las variables de entorno del archivo .env
 load_dotenv()
 
-IS_DEPLOYED = os.getenv('IS_DEPLOYED', 'False') == 'True'
+# Variable para escoger la base de datos local o de producción
+# Si IS_DEPLOYED es 'True', se escoge la base de datos de producción, de lo contrario, se escoge la base de datos local.
+IS_DEPLOYED = os.getenv("IS_DEPLOYED", "False") == "True"
 
-if IS_DEPLOYED:
-    DATABASE_DICT = dj_database_url.config(default=os.getenv("DATABASE_PUBLIC_URL"))
-else: 
-    DATABASE_DICT = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),   
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-        'TEST': {
-            'NAME': 'test_EVA',
-        },
-    }
+# Variable para escoger la base de datos a utilizar
+# Si DATABASE_SELECTOR es 'postgresql', se escoge la base de datos PostgreSQL
+# Si DATABASE_SELECTOR es 'mysql', se escoge la base de datos MySQL
+DATABASE_SELECTOR = os.getenv("DATABASE_SELECTOR", "postgresql")
+
+# Si DATABASE_SELECTOR no es 'postgresql' o 'mysql', se establece en 'postgresql'
+DATABASE_SELECTOR != "postgresql" and DATABASE_SELECTOR != "mysql" = "postgresql"
+
+# Configuración de la base de datos
+if DATABASE_SELECTOR == "postgresql":
+    if IS_DEPLOYED:
+        DATABASE_DICT = dj_database_url.config(default=os.getenv("POSTGRESQL_DATABASE_URL"))
+    else:
+        DATABASE_DICT = {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.getenv("POSTGRESQL_DB_NAME", "postgres"),
+            "USER": os.getenv("POSTGRESQL_DB_USER", "postgres"),
+            "PASSWORD": os.getenv("POSTGRESQL_DB_PASSWORD", ""),
+            "HOST": os.getenv("POSTGRESQL_DB_HOST", "localhost"),
+            "PORT": os.getenv("POSTGRESQL_DB_PORT", "5432"),
+            "OPTIONS": {
+                "client_encoding": "UTF8",
+            },
+            "TEST": {
+                "NAME": "test_EVA",
+            },
+        }
+elif DATABASE_SELECTOR == "mysql":
+    if IS_DEPLOYED:
+        DATABASE_DICT = dj_database_url.config(default=os.getenv("MYSQL_DATABASE_URL"))
+    else:
+        DATABASE_DICT = {
+            "ENGINE": "django.db.backends.mysql_pymysql",
+            "NAME": os.getenv("MYSQL_DB_NAME", "mysql"),
+            "USER": os.getenv("MYSQL_DB_USER", "root"),
+            "PASSWORD": os.getenv("MYSQL_DB_PASSWORD", ""),
+            "HOST": os.getenv("MYSQL_DB_HOST", "localhost"),
+            "PORT": os.getenv("MYSQL_DB_PORT", "3306"),
+            "OPTIONS": {
+                "sql_mode": "traditional",
+                "charset": "utf8mb4",
+                "init_command": "SET default_storage_engine=INNODB",
+            },
+            "TEST": {
+                "NAME": "test_EVA",
+            },
+        }
+else:
+    print(
+        "Error: DATABASE SELECTOR debe ser 'postgresql' o 'mysql'.\n"
+        + "Se utiliza el valor predeterminado 'postgresql'.\n"
+        + "Establezca la variable de entorno DATABASE SELECTOR en 'postgresql' o 'mysql' antes de ejecutar el servidor."
+    )
