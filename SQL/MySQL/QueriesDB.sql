f06e00d5e14da2c688606a9f0d2ddd023af44cd1
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2024-09-02 10:50:22 -0500

    EVA-main feat: Se agregan Scripts para realizar consultas de SQL, con el propósito de obtener información de las bases de datos de MySQL y PostgreSQL.

---
+++ SQL/MySQL/QueriesDB.sql
@@ -0,0 +1,10 @@
+-- -------------------
+-- Base de Datos MySQL
+-- -------------------
+
+-- Consultas en la Base de Datos de MySQL
+
+-- Consultar los plugins instalados
+SHOW plugins;
+-- Consultar la zona horaria
+SELECT @@system_time_zone;
---
+++ SQL/PostgreSQL/QueriesDB.sql
@@ -0,0 +1,10 @@
+-- ------------------------
+-- Base de Datos PostgreSQL
+-- ------------------------
+
+-- Consultas en la Base de Datos de PostgreSQL
+
+-- Consultar las extensiones instaladas
+SELECT * FROM pg_extension;
+-- Consultar la zona horaria
+SHOW timezone;
