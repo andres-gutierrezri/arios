3b88fbe1de0169a65a65abb6276e57288ef342e8
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2024-09-15 19:37:20 -0500

    EVA-predesarrollo feat: Se agregan más consultas para la configuración de las bases de datos.
    - Se agregan más queries a los archivos de SQL para la configuración de las bases de datos de MySQL y PostgreSQL.

--- SQL/PostgreSQL/QueriesDB.sql
+++ SQL/PostgreSQL/QueriesDB.sql
@@ -4,7 +4,31 @@
 
 -- Consultas en la Base de Datos de PostgreSQL
 
+-- Consultar la versión de PostgreSQL
+SELECT version();
+
+-- Consultar las bases de datos
+\l -- \list
+SELECT datname FROM pg_database;
+
+-- Recuperación de información detallada de la base de datos
+SELECT datname AS database_name, 
+       pg_size_pretty(pg_database_size(datname)) AS size,
+       datallowconn AS allow_connections,
+       datconnlimit AS connection_limit,
+       encoding, 
+       datcollate, 
+       datctype 
+FROM pg_catalog.pg_database;
+
+-- Listado de roles y permisos
+\du
+
+-- Listado de información de espacios de tabla
+\db
+
 -- Consultar las extensiones instaladas
 SELECT * FROM pg_extension;
+
 -- Consultar la zona horaria
 SHOW timezone;
